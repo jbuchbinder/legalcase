9663335e5efa1c96e6a19fa3aa0c7cceb3ea422e
mlutfy <mlutfy>
2004-08-19 11:28:43 +0000

    rather minimal but functionnal login page

---
+++ lcm_login.php
@@ -0,0 +1,26 @@
+<?php
+
+include('inc/inc_version.php');
+include_lcm('inc_presentation');
+include_lcm('inc_login');
+
+debut_html(_T('login_acces_prive'));
+
+global $spip_lang_right;
+$lcm_site_name = lire_meta('nom_site');
+
+echo "<p>&nbsp;</p>\n";
+echo "<center><table width='400'><tr><td width='400'>\n";
+echo "<div align='center'>\n";
+echo "<h3 class='spip'>" . $lcm_site_name . "<br>\n";
+echo "<small>" . _T('login_acces_prive') . "</small></h3>\n";
+echo "<div align='" . $spip_lang_right . "'>" . menu_languages() . "</div>\n";
+echo "</div>\n";
+
+login('');
+
+echo "</td></tr></table></center>\n";
+
+fin_html();
+
+?>
