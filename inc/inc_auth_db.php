74b233ed40aeaa25740c8f74df13cd8119c56a02
mlutfy <mlutfy>
2004-09-14 16:22:33 +0000

    comment translations

--- inc/inc_auth_db.php
+++ inc/inc_auth_db.php
@@ -12,15 +12,15 @@ class Auth_db {
 		return true;
 	}
 
-	// Verification du mot passe crypte (javascript)
+	// Check the encrypted password (javascript)
 	function verifier_challenge_md5($login, $mdpass_actuel, $mdpass_futur) {
-		// Interdire mot de passe vide
+		// Do not allow empty passwords
 		if ($mdpass_actuel == '') return false;
 
 		$query = "SELECT * FROM lcm_author WHERE username='".addslashes($login)."' AND password='".addslashes($mdpass_actuel)."' AND status<>'5poubelle'";
-		$result = spip_query($query);
+		$result = lcm_query($query);
 
-		if ($row = spip_fetch_array($result)) {
+		if ($row = lcm_fetch_array($result)) {
 			$this->nom = $row['nom'];
 			$this->login = $row['username'];
 			$this->email = $row['email'];
@@ -32,14 +32,14 @@ class Auth_db {
 		return false;
 	}
 
-	// Verification du mot passe en clair (sans javascript)
+	// Check the non-encrypted password (no javascript support)
 	function verifier($login, $pass) {
-		// Interdire mot de passe vide
+		// Do not allow empty passwords
 		if ($pass == '') return false;
 
 		$query = "SELECT alea_actuel, alea_futur FROM lcm_author WHERE username='".addslashes($login)."'";
-		$result = spip_query($query);
-		if ($row = spip_fetch_array($result)) {
+		$result = lcm_query($query);
+		if ($row = lcm_fetch_array($result)) {
 			$md5pass = md5($row['alea_actuel'] . $pass);
 			$md5next = md5($row['alea_futur'] . $pass);
 			return $this->verifier_challenge_md5($login, $md5pass, $md5next);
@@ -47,23 +47,29 @@ class Auth_db {
 		return false;
 	}
 
+	// lire == read. See lcm_cookie.php. This function is important for LDAP auth.
 	function lire() {
 		return true;
 	}
 
-	function activer() {
-		if ($this->statut == 'nouveau') { // nouvel inscrit
-			spip_query("UPDATE lcm_author SET status='1comite' WHERE username='".addslashes($this->login)."'");
+	function activate() {
+		if ($this->statut == 'nouveau') { // new author
+			lcm_query("UPDATE lcm_author SET status='1comite' WHERE username='".addslashes($this->login)."'");
 		}
 		if ($this->md5next) {
 			include_lcm('inc_session');
-			// fait tourner le codage du pass dans la base
+			// creates a new salt for password encoding in the database
 			$nouvel_alea_futur = creer_uniqid();
 			$query = "UPDATE lcm_author SET alea_actuel = alea_futur, ".
 				"password = '".addslashes($this->md5next)."', alea_futur = '$nouvel_alea_futur' ".
 				"WHERE username='".$this->login."'";
 			@spip_query($query);
 		}
+	}
+
+	function activer() {
+		lcm_log("use of deprecated function: activer, use activate instead");
+		return activate();
 	}
 }
 
