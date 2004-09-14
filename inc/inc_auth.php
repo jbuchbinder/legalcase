93520f5fa358e077521d88948be174392be20f6d
mlutfy <mlutfy>
2004-09-14 15:36:45 +0000

    lcm-ification + alot of useless code removed

--- inc/inc_auth.php
+++ inc/inc_auth.php
@@ -8,10 +8,9 @@ define('_INC_AUTH', '1');
 include_lcm('inc_meta'); // initiates the database connection
 include_lcm('inc_session');
 
-//
-// Fonctions de gestion de l'acces restreint aux rubriques
-//
 
+// Management function to restrictive access to sections
+// [ML] At the moment, we have no reason to use this
 function acces_rubrique($id_rubrique) {
 	global $connect_toutes_rubriques;
 	global $connect_id_rubrique;
@@ -27,7 +26,7 @@ function acces_restreint_rubrique($id_rubrique) {
 }
 
 
-// [ML] Alot of things to adapt...
+// [ML] Alot of things to adapt... XXX/TODO
 function auth() {
 	global $INSECURE, $HTTP_POST_VARS, $HTTP_GET_VARS, $HTTP_COOKIE_VARS, $REMOTE_USER, $PHP_AUTH_USER, $PHP_AUTH_PW;
 	global $auth_can_disconnect, $ignore_auth_http, $ignore_remote_user;
@@ -43,7 +42,7 @@ function auth() {
 	// If there is not SQL connection, quit.
 	if (!$GLOBALS['db_ok']) {
 		echo "<P><H4>"._T('titre_probleme_technique')."</H4><P><P>\n".
-		"<tt>".spip_sql_errno()." ".spip_sql_error()."</tt>";
+		"<tt>".lcm_sql_errno()." ".lcm_sql_error()."</tt>";
 		return false;
 	}
 
@@ -55,31 +54,7 @@ function auth() {
 	$auth_can_disconnect = false;
 	$auth_htaccess = false;
 
-	//
-	// Recuperer les donnees d'identification
-	//
-
-	// Peut-etre sommes-nous en auth http?
-	if ($PHP_AUTH_USER && $PHP_AUTH_PW && !$ignore_auth_http) {
-		if (verifier_php_auth()) {
-			$auth_login = $PHP_AUTH_USER;
-			$auth_pass_ok = true;
-			$auth_can_disconnect = true;
-		}
-		else // normalement on n'arrive pas la sauf changement de mot de passe dans la base...
-		if ($PHP_AUTH_USER != 'root') // ... mais quelques serveurs forcent cette valeur
-		{
-			$auth_login = '';
-			echo "<p><b>"._T('info_connexion_refusee')."</b></p>";
-			echo "[<a href='lcm_cookie.php?essai_auth_http=oui'>"._T('lien_reessayer')."</a>]";
-			exit;
-		}
-		$PHP_AUTH_PW = '';
-		$_SERVER['PHP_AUTH_PW'] = '';
-		$HTTP_SERVER_VARS['PHP_AUTH_PW'] = '';
-	}
-
-	// Authentification session
+	// Fetch identification data from authentication session
 	else if ($cookie_session = $HTTP_COOKIE_VARS['lcm_session']) {
 		if (verifier_session($cookie_session)) {
 			if ($auteur_session['status'] == 'admin' OR $auteur_session['status'] == '1comite') {
@@ -91,13 +66,15 @@ function auth() {
 	}
 
 	// Authentification .htaccess
+	// [ML] should be removed (TODO)
 	else if ($REMOTE_USER && !$INSECURE['REMOTE_USER'] && !$ignore_remote_user) {
 		$auth_login = $REMOTE_USER;
 		$auth_pass_ok = true;
 		$auth_htaccess = true;
 	}
 
-	// Tentative de login echec
+	// Failed login attempt
+	// [ML] this is rather obscure to me, I will try to clean it later (TODO)
 	else if ($GLOBALS['bonjour'] == 'oui') {
 		$link = new Link("lcm_cookie.php?test_echec_cookie=oui");
 		$clean_link->delVar('bonjour');
@@ -107,23 +84,22 @@ function auth() {
 		exit;
 	}
 
-	// Si pas authentifie, demander login / mdp
+	// If not authenticated, ask for login / password
 	if (!$auth_login) {
 		$url = /* str_replace('/./', '/', 'ecrire/'. */ $clean_link->getUrl(); //);
-		// $url = str_replace('/./', '/', 'ecrire/'. $clean_link->getUrl());
 		@header("Location: lcm_login.php?var_url=".urlencode($url));
 		exit;
 	}
 
 	//
-	// Chercher le login dans la table auteurs
+	// Search for the login in the authors' table
 	//
 
 	$auth_login = addslashes($auth_login);
 	$query = "SELECT * FROM lcm_author WHERE username='$auth_login' AND status !='external' AND status !='6forum'";
-	$result = @spip_query($query);
+	$result = @lcm_query($query);
 
-	if ($row = spip_fetch_array($result)) {
+	if ($row = lcm_fetch_array($result)) {
 		$connect_id_auteur = $row['id_author'];
 		$connect_nom = $row['name_first'];
 		$connect_bio = $row['bio'];
@@ -136,19 +112,10 @@ function auth() {
 		$connect_activer_messagerie = "non"; //$row["messagerie"];
 		$connect_activer_imessage = "non "; //$row["imessage"];
 
-		// Special : si dans la fiche auteur on modifie les valeurs
-		// de messagerie, utiliser ces valeurs plutot que celle de la base.
-		// D'ou leger bug si on modifie la fiche de quelqu'un d'autre.
-		/*if ($GLOBALS['perso_activer_messagerie']) {
-			$connect_activer_messagerie = $GLOBALS['perso_activer_messagerie'];
-			$connect_activer_imessage = $GLOBALS['perso_activer_imessage'];
-		}*/
-		// (1.8: La messagerie est toujours active)
-
-		// regler les preferences de l'auteur
+		// set the users' preferences
 		$prefs = unserialize($row['prefs']);
 
-		// vieux ! on pourra supprimer post 1.6 finale...
+		// [ML] I will clean this later, for not it is not used much (TODO)
 		if (! isset($prefs['display'])) { // recuperer les cookies ou creer defaut
 			if ($GLOBALS['set_disp'] = $GLOBALS['HTTP_COOKIE_VARS']['spip_display']) {}
 			else $GLOBALS['set_disp'] = 2;
@@ -157,51 +124,13 @@ function auth() {
 			if ($GLOBALS['set_options'] = $GLOBALS['HTTP_COOKIE_VARS']['spip_options']) {}
 			else $GLOBALS['set_options'] = 'basiques';
 		}
-
-		// Indiquer connexion
-		/* [ML] NOT USED
-		if ($connect_activer_messagerie != "non") {
-			@spip_query("UPDATE lcm_author SET en_ligne=NOW() WHERE id_author = '$connect_id_auteur'");
-		}
-		*/
-
-		// Si administrateur, recuperer les rubriques gerees par l'admin
-		if ($connect_statut == 'admin') {
-			// [ML] $query_admin = "SELECT id_rubrique FROM spip_auteurs_rubriques WHERE id_auteur=$connect_id_auteur AND id_rubrique!='0'";
-			// [ML] $result_admin = spip_query($query_admin);
-
-			/* [ML]
-			$connect_toutes_rubriques = (@spip_num_rows($result_admin) == 0);
-			if ($connect_toutes_rubriques) {
-				$connect_id_rubrique = array();
-			}
-			else {
-				for (;;) {
-					$r = '';
-					while ($row_admin = spip_fetch_array($result_admin)) {
-						$id_rubrique = $row_admin['id_rubrique'];
-						$r[] = $id_rubrique;
-						$connect_id_rubrique[$id_rubrique] = $id_rubrique;
-					}
-					if (!$r) break;
-					$r = join(',', $r);
-					$query_admin = "SELECT id_rubrique FROM spip_rubriques WHERE id_parent IN ($r) AND id_rubrique NOT IN ($r)";
-				 	$result_admin = spip_query($query_admin);
-				 }
-			}
-			*/
-		}
-		// Si pas admin, acces egal a toutes rubriques
-		else {
-			$connect_toutes_rubriques = false;
-			$connect_id_rubrique = array();
-		}
 	}
 	else {
-		// ici on est dans un cas limite : l'auteur a ete identifie OK
-		// mais il n'existe pas dans la table auteur. Cause possible,
-		// notamment, une restauration de base de donnees dans laquelle
-		// il n'existe pas.
+		// This case is a strange possibility: the author is authentified
+		// OK, but he does not exist in the authors table. Possible cause:
+		// the database was restaured and the author does not exist.
+		// [ML] I am not sure this could really happen if we use cookie-only
+		// authentication (not php_auth). I will clean it later (TODO)
 		include_lcm('inc_presentation');
 		include_lcm('inc_text');
 
@@ -216,11 +145,14 @@ function auth() {
 		exit;
 	}
 
+	// [ML] Again, not sure how this is used, but we can ignore it for now
+	// TODO (note: nouveau == new)
 	if ($connect_statut == 'nouveau') {
 		$query = "UPDATE lcm_author SET status = '1comite' WHERE id_author = $connect_id_auteur";
 		$result = spip_query($query);
 		$connect_statut = '1comite';
 	}
+
 	return true;
 }
 
