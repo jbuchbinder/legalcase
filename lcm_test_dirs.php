bd03977988d0123c64d0afef98dc1afd7f368890
mlutfy <mlutfy>
2004-09-15 11:09:48 +0000

    translation of code and comments

--- lcm_test_dirs.php
+++ lcm_test_dirs.php
@@ -10,26 +10,26 @@ function bad_dirs($bad_dirs, $test_dir, $install) {
 
 	if ($install) {
 		$titre = _T('dirs_preliminaire');
-		$continuer = _T('dirs_commencer');
+		$continue = _T('dirs_commencer');
 	} else
 		$titre = _T('dirs_probleme_droits');
 
 	$bad_url = "spip_test_dirs.php3";
 	if ($test_dir) $bad_url .= '?test_dir='.$test_dir;
 
-	echo "<FONT FACE=\"Verdana,Arial,Helvetica,sans-serif\" SIZE=3>$titre</FONT>\n<p>";
+	echo "<font face=\"Verdana,Arial,Helvetica,sans-serif\" SIZE='3'>$titre</font>\n<p>";
 	echo "<div align='right'>". menu_languages('var_lang_lcm')."</div><p>";
 
 	echo _T('dirs_repertoires_suivants', array('bad_dirs' => $bad_dirs));
-	echo "<B>". _T('login_recharger')." $continuer.";
+	echo "<b>". _T('login_recharger')." $continue.";
 
 	if ($install)
 		echo aide ("install0");
 	echo "<p>";
 
-	echo "<FORM ACTION='$bad_urls' METHOD='GET'>\n";
-	echo "<DIV align='right'><INPUT TYPE='submit' CLASS='fondl' NAME='Valider' VALUE='". _T('login_recharger')."'></DIV>";
-	echo "</FORM>";
+	echo "<form action='$bad_urls' method='get'>\n";
+	echo "<div align='right'><input type='submit' class='fondl' name='Valider' value='". _T('login_recharger')."'></div>";
+	echo "</form>";
 
 	install_html_end();
 }
@@ -39,7 +39,7 @@ function absent_dirs($bad_dirs, $test_dir) {
 	install_html_start();
 
 	$titre = _T('dirs_preliminaire');
-	$continuer = _T('dirs_commencer');
+	$continue = _T('dirs_commencer');
 
 	$bad_url = "spip_test_dirs.php3";
 	if ($test_dir) $bad_url .= '?test_dir='.$test_dir;
@@ -48,7 +48,7 @@ function absent_dirs($bad_dirs, $test_dir) {
 	echo "<div align='right'>". menu_languages('var_lang_lcm')."</div><p>";
 
 	echo _T('dirs_repertoires_absents', array('bad_dirs' => $bad_dirs));
-	echo "<B>". _T('login_recharger')." $continuer.";
+	echo "<B>". _T('login_recharger')." $continue.";
 
 	if ($install)
 		echo aide ("install0");
@@ -61,21 +61,21 @@ function absent_dirs($bad_dirs, $test_dir) {
 	install_html_end();
 }
 
-//
-// Tente d'ecrire
-//
-function test_ecrire($my_dir) {
+// Try to write in a directory
+function test_write($my_dir) {
 	$ok = true;
-	$nom_fich = "$my_dir/test.txt";
-	$f = @fopen($nom_fich, "w");
+	$file_name = "$my_dir/test.txt";
+	$f = @fopen($file_name, "w");
+
 	if (!$f) $ok = false;
 	else if (!@fclose($f)) $ok = false;
-	else if (!@unlink($nom_fich)) $ok = false;
+	else if (!@unlink($file_name)) $ok = false;
+
 	return $ok;
 }
 
 //
-// teste les droits sur les repertoires
+// Test rights on directories
 //
 
 $install = !@file_exists("inc/config/inc_connect.php");
@@ -83,23 +83,23 @@ $install = !@file_exists("inc/config/inc_connect.php");
 if ($test_dir)
 	$test_dirs[] = $test_dir;
 else {
-	$test_dirs = array("inc/config", "log", "inc/data");
+	$test_dirs = array("log", "inc/config", "inc/data");
 }
 
 unset($bad_dirs);
 unset($absent_dirs);
 
 while (list(, $my_dir) = each($test_dirs)) {
-	if (!test_ecrire($my_dir)) {
+	if (!test_write($my_dir)) {
 		@umask(0);
 		if (@file_exists($my_dir)) {
 			@chmod($my_dir, 0777);
 			// ???
-			if (!test_ecrire($my_dir))
+			if (!test_write($my_dir))
 				@chmod($my_dir, 0775);
-			if (!test_ecrire($my_dir))
+			if (!test_write($my_dir))
 				@chmod($my_dir, 0755);
-			if (!test_ecrire($my_dir))
+			if (!test_write($my_dir))
 				$bad_dirs[] = "<li>".$my_dir;
 		} else
 			$absent_dirs[] = "<li>".$my_dir;
