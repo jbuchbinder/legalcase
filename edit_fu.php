752f0908e0e047034e59fd87b1fb580a9c025a3d
antzi <antzi>
2004-09-21 14:12:19 +0000

    Incorporated error checking and mechanism into the followup form/update scripts couple.
    Now a session is used to detect errors in the upd_fu.php script and return data along with error mesage for the respective field back to the edit_fu.php.

--- edit_fu.php
+++ edit_fu.php
@@ -1,46 +1,64 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-  <title>Case details</title>
-</head>
-<body>
-
 <?php
 
-// Set connection parameters
+// Error display function
+function f_err($fn, $errors)
+{
+    if (isset($errors[$fn]))
+		echo "<font color=RED>$errors[$fn]</font><br>";
+}
 
-// Connect to the database
-$db=mysql_connect('localhost','lcm','lcmpass');
+// Initiate session
+session_start();
 
-// Select lcm database
-mysql_select_db('lcm',$db);
+if (empty($errors)) {
+    // Clear form data
+    $fu_data=array('referer'=>$HTTP_REFERER);
 
-$fu_data=array();
+	if (isset($followup)) {
+		// Register followup as session variable
+	    if (!session_is_registered("followup"))
+			session_register("followup");
 
-if (isset($followup)) {
-    // Prepare query
-    $q='SELECT * FROM lcm_followup WHERE id_followup=' . $followup;
+		// Connect to the database
+		$db=mysql_connect('localhost','lcm','lcmpass');
 
-    // Do the query
-    $result=mysql_query($q,$db);
+		// Select lcm database
+		mysql_select_db('lcm',$db);
 
-    // Process the output of the query
-    if ($row = mysql_fetch_assoc($result)) {
-	// Get followup details
-	foreach($row as $key=>$value) {
-	    $fu_data[$key]=$value;
+		// Prepare query
+		$q='SELECT * FROM lcm_followup WHERE id_followup=' . $followup;
+
+		// Do the query
+		$result=mysql_query($q,$db);
+
+		// Process the output of the query
+		if ($row = mysql_fetch_assoc($result)) {
+			// Get followup details
+			foreach($row as $key=>$value) {
+				$fu_data[$key]=$value;
+			}
+		}
+
+		// Close connection
+		mysql_close($db);
+	} else {
+		// Setup default values
+		$fu_data['id_case']=$case; // Link to the case
+		$fu_data['date_start']=date('Y-m-d H:i:s'); // '2004-09-16 16:32:37'
 	}
-    }
-} else {
-    // Link to the case
-    $fu_data['id_case']=$case;
-    //$fu_data['date_start']='2004-09-16 16:32:37';
 }
 
 $types=array("assignment","suspension","delay","conclusion","consultation","correspondance","travel","other");
 
 // Edit followup details form
 ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+  <title>Follow-up details</title>
+</head>
+<body>
+
 <h1>Edit follow-up information:</h1>
 
 <form action="upd_fu.php" method="POST">
@@ -48,7 +66,8 @@ $types=array("assignment","suspension","delay","conclusion","consultation","corr
 <tr><th>Parameter</th><th>Value</th></tr>
 <INPUT type="hidden" name="id_followup" value="<?php echo $fu_data['id_followup']; ?>">
 <INPUT type="hidden" name="id_case" value="<?php echo $fu_data['id_case']; ?>">
-<tr><td>Start date:</td><td><INPUT name="date_start" value="<?php echo $fu_data['date_start']; ?>"></td></tr>
+<tr><td>Start date:</td><td><INPUT name="date_start" value="<?php echo $fu_data['date_start']; ?>">
+<?php echo f_err('date_start',$errors); ?></td></tr>
 <tr><td>End date:</td><td><INPUT name="date_end" value="<?php echo $fu_data['date_end']; ?>"></td></tr>
 <tr><td>Type:</td><td><SELECT name="type" size="1"><OPTION selected><?php echo $fu_data['type']; ?></OPTION>
 <?php
@@ -65,14 +84,8 @@ foreach($types as $item) {
 </table>
 <BUTTON name="submit" type="submit" value="submit">Save</BUTTON>
 <BUTTON name="reset" type="reset">Reset</BUTTON>
-<INPUT type="hidden" name="referer" value="<?php echo $HTTP_REFERER ?>">
+<INPUT type="hidden" name="referer" value="<?php echo $fu_data['referer']; ?>">
 </form>
 
-<?php
-//} else die("There's no such followup!");
-
-// Close connection
-mysql_close($db);
-?>
 </body>
 </html>
--- upd_fu.php
+++ upd_fu.php
@@ -1,26 +1,57 @@
 <?php
-// Connect to the database
-$db=mysql_connect('localhost','lcm','lcmpass');
 
-// Select lcm database
-mysql_select_db('lcm',$db);
+// Start session
+session_start();
 
-$fl="date_start='$date_start',date_end='$date_end',type='$type',description='$description'";
-$fl.=",sumbilled='$sumbilled'";
+// Register $errors array - just in case
+if (!session_is_registered("errors"))
+    session_register("errors");
 
-if ($id_followup>0) {
-   // Prepare query
-   $q="UPDATE lcm_followup SET $fl WHERE id_followup=$id_followup";
+// Clear all previous errors
+$errors=array();
+
+// Register form data in the session
+if(!session_is_registered("fu_data"))
+    session_register("fu_data");
+
+// Get form data from POST fields
+foreach($_POST as $key => $value)
+    $fu_data[$key]=$value;
+
+// Check submitted information
+if (strtotime($fu_data['date_start'])<0) { $errors['date_start']='Invalid start date!'; }
+
+if (count($errors)) {
+    header("Location: $HTTP_REFERER");
+    exit;
 } else {
-   $q="INSERT INTO lcm_followup SET id_followup=0,id_case=$id_case,$fl";
+    // Connect to the database
+    $db=mysql_connect('localhost','lcm','lcmpass');
+
+    // Select lcm database
+    mysql_select_db('lcm',$db);
+
+    $fl="date_start=\"$date_start\",date_end='$date_end',type='$type',description='$description'";
+    $fl.=",sumbilled='$sumbilled'";
+
+    if ($id_followup>0) {
+	// Prepare query
+	$q="UPDATE lcm_followup SET $fl WHERE id_followup=$id_followup";
+    } else {
+	$q="INSERT INTO lcm_followup SET id_followup=0,id_case=$id_case,$fl";
+    }
+
+    // Do the query
+    if (!($result=mysql_query($q,$db))) die("$q<br>\nError ".mysql_errno().": ".mysql_error());
+    //echo $q;
+
+    // Close connection
+    mysql_close($db);
+
+    // Clear the session
+    session_destroy();
+
+    // Send user back to add/edit page's referer
+    header("Location: $fu_data['referer']");
 }
-
-// Do the query
-if (!($result=mysql_query($q,$db))) die("$q<br>\nError ".mysql_errno().": ".mysql_error());
-//echo $q;
-
-// Close connection
-mysql_close($db);
-
-header("Location: $referer");
 ?>
