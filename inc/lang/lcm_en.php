5c790534c69c98c41e86f462d3994585d23f5615
mlutfy <mlutfy>
2004-10-04 09:23:19 +0000

    Added translation strings for login

--- inc/lang/lcm_en.php
+++ inc/lang/lcm_en.php
@@ -13,43 +13,28 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 
 // B
-'bouton_acces_ldap' => 'Add an access to LDAP >>',
-'bouton_suivant' => 'Next',
+'button_validate' => 'Validate',
 
 
-// E
-'entree_adresse_email' => 'Your e-mail address',
-'entree_identifiants_connexion' => 'Your connection identifiers...',
-'entree_login' => 'Your login',
-'entree_mot_passe' => 'Your password',
-'entree_nom_pseudo_1' => '(Your name or alias)',
-'entree_signature' => 'Signature',
-
-
-// I
-'info_authentification_externe' => 'External authentication',
-'info_code_acces' => 'Do not forget your own access codes!',
-'info_creation_tables' => 'Fourth step: <B>Creation of the database tables</b>',
-'info_derniere_etape' => 'Last step: <B>It is completed!',
-'info_etape_suivante_1' => 'You can move to the next step.',
-'info_identification_publique' => 'Your public identity...',
-'info_informations_personnelles' => 'Fifth step: <b>Personal information</b>',
-'info_installation_legal_case_management' => 'Legal case management installation ...',
-'info_laisser_champs_vides' => 'leave these fields empty)',
-'info_mon_site_spip' => 'My Legal Case Management',
-'info_plus_cinq_car' => 'more than 5 characters',
-'info_plus_cinq_car_2' => '(More than 5 characters)',
-'info_plus_trois_car' => '(More than 3 characters)',
-'info_utilisation_spip' => 'LCM is now ready to be used...',
-'install_select_langue' => 'Select a language then click on the button "next" to start the installation procedure.',
+// L
+'login_access_denied' => 'Access denied',
+'login_identifier_unknown' => 'The identifier "@login@" is unknown.',
+'login_info_login' => 'identifier for connection to the site',
+'login_login' => 'Login',
+'login_other_identifier' => 'connect with another identifier',
+'login_password' => 'Password',
+'login_password_forgotten' => 'password forgotten?',
+'login_password_incorrect' => 'Incorrect login or password.',
+'login_prefer_no_cookie' => '<b>If you prefer to refuse the cookies</b>, there is another connection method (less secure) at your disposal:',
+'login_register' => 'register',
+'login_remain_logged_on' => 'Remain logged-on for a few days',
+'login_without_cookie' => 'Identification without cookie',
 
 
 // T
-'texte_annuaire_ldap_1' => 'If you have access to a (LDAP) directory, you can use it to automatically import users under SPIP.',
-'texte_informations_personnelles_1' => 'The system will provide you now with a custom access to the site.',
-'texte_informations_personnelles_2' => '(Note: if it is a reinstallation, and your access is still working, you can',
 'title_software' => 'Legal Case Management System',
 'title_software_article' => 'The Legal Case Management Software',
+'typo_column' => ':',
 
 
 // W
