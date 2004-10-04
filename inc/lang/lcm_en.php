a32c8521b10fd258f6dc0ef1f456f2e1686bab54
mlutfy <mlutfy>
2004-10-04 16:10:16 +0000

    Added strings for installation steps 1-3

--- inc/lang/lcm_en.php
+++ inc/lang/lcm_en.php
@@ -13,7 +13,34 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 
 // B
+'button_next' => 'Next',
 'button_validate' => 'Validate',
+
+
+// I
+'info_or' => 'or',
+'install_connection_login' => 'Connection login',
+'install_connection_password' => 'Connection password',
+'install_connection_succeeded' => 'Connection succeeded.',
+'install_database_address' => 'Database address',
+'install_info_connection_login' => 'Sometimes matches your FTP access login and sometimes left empty.',
+'install_info_connection_password' => 'Sometimes matches your FTP access password and sometimes left empty.',
+'install_info_database_address' => '(Often, this address matches the address of your site, sometimes it corresponds to the name "localhost", and sometimes it is left completely empty.)',
+'install_info_go_back_verify_data' => 'Go back to the previous page, and verify the information you have provided.',
+'install_info_sql_connection' => 'Refer to the information provided to you by your system administrator: if your host supports SQL, it should give you the connection codes to SQL server.',
+'install_info_sql_connection_failed' => '<b>N.B.</b> On a number of servers, you must request the activation of your access to the SQL database before you can use it. If you are unable to connect, make sure that you actually made this request.',
+'install_next_step' => 'You can proceed to the next step.',
+'install_select_database' => 'Select the database that your host has attributed to you',
+'install_select_language' => 'Select a language then click on the button "next" to start the installation procedure.',
+'install_step_four' => 'Fourth step',
+'install_step_last' => 'Last step',
+'install_step_one' => 'First step',
+'install_step_three' => 'Third step',
+'install_step_two' => 'Second step',
+'install_title_connection_attempt' => 'Attempting to connect to the SQL database',
+'install_title_installation_start' => 'Legal Case Management Software installation ...',
+'install_title_select_database' => 'Choosing your database',
+'install_title_sql_connection' => 'Your SQL database connection',
 
 
 // L
@@ -39,7 +66,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // W
 'warning_already_installed' => 'is already installed.',
-'warning_forbidden_area' => 'Forbidden area'
+'warning_forbidden_area' => 'Forbidden area',
+'warning_sql_connection_failed' => 'Connection to SQL database server failed.'
 
 );
 
