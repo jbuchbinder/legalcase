42a59660e83714bdc54c51a6b2ea9167be52c098
mlutfy <mlutfy>
2004-10-05 08:41:11 +0000

    Renamed inc_dbmgnt.php to inc_db_create.php + minor SQL bug fix.

--- inc/inc_db_create.php
+++ inc/inc_db_create.php
@@ -1,14 +1,30 @@
 <?php
 
-if (defined('_INC_DBMGNT')) return;
-define('_INC_DBMGNT', '1');
+if (defined('_INC_DB_CREATE')) return;
+define('_INC_DB_CREATE', '1');
 
 include_lcm('inc_access');
+
+function log_if_not_duplicate_table($errno) {
+	if ($errno) {
+		$error = lcm_sql_error();
+		// XXX 1- If MySQL set by default in non-English, may not catch the error 
+		//        (needs testing, and we can simply add the regexp in _T())
+		// XXX 2- PostgreSQL may have different error format.
+		if (! preg_match("/.*Table.*already exists.*/", $error)) {
+			return lcm_sql_error() . "\n";
+		}
+	}
+
+	return "";
+}
 
 // For details on the various fields, see:
 // http://www.lcm.ngo-bg.org/article2.html
 
 function create_database() {
+	$log = "";
+
 	//
 	// Main objects
 	//
@@ -27,7 +43,7 @@ function create_database() {
 	// - DONE lcm_case_client_org
 	// - DONE lcm_case_author
 
-	lcm_log("creating the tables for objects", 'install');
+	lcm_log("creating the SQL tables", 'install');
 
 	$query = "CREATE TABLE lcm_case (
 		id_case bigint(21) NOT NULL auto_increment,
@@ -38,9 +54,11 @@ function create_database() {
 		legal_reason text NOT NULL,
 		alledged_crime text NOT NULL,
 		status text NOT NULL,
-		public tinyint(1) default '0' NOT NULL,
+		public tinyint(1) DEFAULT '0' NOT NULL,
 		PRIMARY KEY (id_case))";
 	$result = lcm_query($query);
+	
+	$log .= log_if_not_duplicate_table(lcm_sql_errno());
 
 	$query = "CREATE TABLE lcm_followup (
 		id_followup bigint(21) NOT NULL auto_increment,
@@ -53,6 +71,8 @@ function create_database() {
 		PRIMARY KEY (id_followup),
 		KEY id_case (id_case))";
 	$result = lcm_query($query);
+
+	$log .= log_if_not_duplicate_table(lcm_sql_errno());
 
 	// [ML] XXX too many extra fields
 	$query = "CREATE TABLE lcm_author (
@@ -86,6 +106,8 @@ function create_database() {
 		KEY lang (lang))";
 	$result = lcm_query($query);
 
+	$log .= log_if_not_duplicate_table(lcm_sql_errno());
+
 	$query = "CREATE TABLE lcm_client (
 		id_client bigint(21) NOT NULL auto_increment,
 		name_first text NOT NULL,
@@ -100,6 +122,8 @@ function create_database() {
 		PRIMARY KEY id_client (id_client))";
 	$result = lcm_query($query);
 
+	$log .= log_if_not_duplicate_table(lcm_sql_errno());
+
 	$query = "CREATE TABLE lcm_org (
 		id_org bigint(21) NOT NULL auto_increment,
 		name text NOT NULL,
@@ -108,6 +132,8 @@ function create_database() {
 		address text NOT NULL,
 		PRIMARY KEY id_org (id_org))";
 	$result = lcm_query($query);
+
+	$log .= log_if_not_duplicate_table(lcm_sql_errno());
 
 
 	//
@@ -125,17 +151,19 @@ function create_database() {
 		KEY id_org (id_org))";
 	$result = lcm_query($query);
 
-	// + Access control permissions
+	$log .= log_if_not_duplicate_table(lcm_sql_errno());
+
 	$query = "CREATE TABLE lcm_case_author (
 		id_case bigint(21) DEFAULT '0' NOT NULL,
 		id_author bigint(21) DEFAULT '0' NOT NULL,
-		read tinyint(1) NOT NULL default '1',
-		write tinyint(1) NOT NULL default '0',
-		admin tinyint(1) NOT NULL default '0',
+		ac_read tinyint(1) DEFAULT '1' NOT NULL,
+		ac_write tinyint(1) DEFAULT '0' NOT NULL,
+		ac_admin tinyint(1) DEFAULT '0' NOT NULL,
 		KEY id_case (id_case),
 		KEY id_author (id_author))";
 	$result = lcm_query($query);
 
+	$log .= log_if_not_duplicate_table(lcm_sql_errno());
 
 	$query = "CREATE TABLE lcm_client_org (
 		id_client bigint(21) DEFAULT '0' NOT NULL,
@@ -143,6 +171,8 @@ function create_database() {
 		KEY id_client (id_client),
 		KEY id_org (id_org))";
 	$result = lcm_query($query);
+
+	$log .= log_if_not_duplicate_table(lcm_sql_errno());
 
 	//
 	// Management of the application
@@ -155,7 +185,11 @@ function create_database() {
 		PRIMARY KEY (name))";
 	$result = lcm_query($query);
 
+	$log .= log_if_not_duplicate_table(lcm_sql_errno());
+
 	lcm_log("LCM database initialisation complete", 'install');
+
+	return $log;
 }
 
 ?>
