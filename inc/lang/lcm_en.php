87e9da8d778cc690b5982db539ea24da89937b3d
mlutfy <mlutfy>
2004-10-05 13:28:18 +0000

    A few new strings for installer

--- inc/lang/lcm_en.php
+++ inc/lang/lcm_en.php
@@ -17,30 +17,57 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'button_validate' => 'Validate',
 
 
+// E
+'enter_name_first' => 'First',
+'enter_name_last' => 'Last',
+'enter_name_middle' => 'Middle',
+
+
 // I
+'info_more_than_five' => '(More than 5 characters)',
+'info_more_than_three' => '(More than 3 characters)',
+'info_name_of_person' => 'Name',
 'info_or' => 'or',
+'info_your_contact_information' => 'Your contact information ...',
+'input_connection_identifiers' => 'Your connection identifiers ...',
+'input_email' => 'E-mail address',
 'install_connection_login' => 'Connection login',
 'install_connection_password' => 'Connection password',
 'install_connection_succeeded' => 'Connection succeeded.',
+'install_create_new_database' => '<B>Create</b> a new database',
 'install_database_address' => 'Database address',
+'install_database_install_failed' => 'Go back to the previous page, select another database or create a new one. Verify the information provided by your host.',
+'install_database_installed_ok' => 'Your database is now ready to use.',
+'install_info_application_ready' => 'The system is now ready to be used...',
 'install_info_connection_login' => 'Sometimes matches your FTP access login and sometimes left empty.',
 'install_info_connection_password' => 'Sometimes matches your FTP access password and sometimes left empty.',
 'install_info_database_address' => '(Often, this address matches the address of your site, sometimes it corresponds to the name "localhost", and sometimes it is left completely empty.)',
+'install_info_do_not_forget' => 'Do not forget your own access codes!',
 'install_info_go_back_verify_data' => 'Go back to the previous page, and verify the information you have provided.',
+'install_info_new_account_1' => 'The system will now provide you with a custom access to the site.',
+'install_info_new_account_2' => '(Note: If this is a re-installation and your administrator access is still working, you can leave these fields empty)',
 'install_info_sql_connection' => 'Refer to the information provided to you by your system administrator: if your host supports SQL, it should give you the connection codes to SQL server.',
 'install_info_sql_connection_failed' => '<b>N.B.</b> On a number of servers, you must request the activation of your access to the SQL database before you can use it. If you are unable to connect, make sure that you actually made this request.',
 'install_next_step' => 'You can proceed to the next step.',
 'install_select_database' => 'Select the database that your host has attributed to you',
 'install_select_language' => 'Select a language then click on the button "next" to start the installation procedure.',
+'install_step_five' => 'Fifth step',
 'install_step_four' => 'Fourth step',
 'install_step_last' => 'Last step',
 'install_step_one' => 'First step',
 'install_step_three' => 'Third step',
 'install_step_two' => 'Second step',
+'install_title_admin_account' => 'Personal information for the administrator access',
 'install_title_connection_attempt' => 'Attempting to connect to the SQL database',
+'install_title_creating_database' => 'Creation of the database tables',
 'install_title_installation_start' => 'Legal Case Management Software installation ...',
 'install_title_select_database' => 'Choosing your database',
 'install_title_sql_connection' => 'Your SQL database connection',
+'install_warning_no_databases_1' => 'The installer could not read the names of the available databases.',
+'install_warning_no_databases_2' => 'Either there are no databases available, or the feature allowing the listing of the databases was disabled for security reasons (which is the case with a lot of hosts).',
+'install_warning_no_databases_3' => 'In case the second alternative was true, it is possible that a database named after your login could be usable:',
+'install_warning_nonfatal_sql_errors' => 'There were warnings while installing or upgrading the database, but they may not be fatal. Errors such as \'Table already exists\' can safely be ignored. Errors such as \'Syntax errors\' are most probably bugs and should be reported to the LCM development team ([lcm-devel@lists.sourceforge.net->mailto:lcm-devel@lists.sourceforge.net]).',
+'install_warning_update_impossible' => '<b>Warning!</b> Failed to update the SQL database from version @old_version@ to version @version@, maybe due to a permissions problem on the database. Please contact your administrator. A full technical listing of the possible errors is provided below.',
 
 
 // L
@@ -67,6 +94,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 // W
 'warning_already_installed' => 'is already installed.',
 'warning_forbidden_area' => 'Forbidden area',
+'warning_operation_failed' => 'The operation failed.',
 'warning_sql_connection_failed' => 'Connection to SQL database server failed.'
 
 );
