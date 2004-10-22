391c0e0e868b4d16a40fbee54bec8ba1f4487f32
mlutfy <mlutfy>
2004-10-22 23:40:22 +0000

    fixed translation program to output utf8

--- inc/lang/lcm_bg.php
+++ inc/lang/lcm_bg.php
@@ -7,54 +7,178 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // 0
 '0_URL' => 'http://www.lcm.ngo-bg.org',
-'0_language' => '&ETH;&plusmn;&Ntilde;&#352;&ETH;&raquo;&ETH;&sup3;&ETH;&deg;&Ntilde;&euro;&Ntilde;�&ETH;&ordm;&ETH;&#382; [bg]',
+'0_language' => 'български [bg]',
 '0_list' => 'legalcase-devel@lists.sourceforge.net',
 '0_maintainer' => 'Mathieu Lutfy (mathieu@bidon.ca)',
 
 
+// A
+'add' => '<NEW>Add',
+'add_and_go_to_details' => '<NEW>Add &amp; go to details',
+'add_and_open_new' => '<NEW>Add &amp; open new',
+'add_client_s' => '<NEW>Add client(s)',
+'add_organisation_s' => '<NEW>Add organisation(s)',
+'add_user_case' => '<NEW>Add user to the case',
+'alledged_crime' => '<NEW>Alledged crime',
+'assignment_date' => '<NEW>Assignment date',
+'author_id' => '<NEW>Author ID',
+
+
 // B
-'bouton_acces_ldap' => '<NEW>Add an access to LDAP >>',
-'bouton_suivant' => '<NEW>Next',
+'button_next' => '<NEW>Next',
+'button_reload_page' => '<NEW>Reload this page',
+'button_validate' => '<NEW>Validate',
+
+
+// C
+'case_clients' => '<NEW>Clients in this case',
+'case_details' => '<NEW>Case details',
+'case_followups' => '<NEW>Follow-ups to this case',
+'case_id' => '<NEW>Case ID',
+'case_status' => '<NEW>Case status',
+'case_title' => '<NEW>Case title',
+'case_user_s' => '<NEW>Case user(s)',
+'clients' => 'Клиенти',
+'court_archive_id' => '<NEW>Court archive ID',
+'creation_date' => '<NEW>Creation date',
+
+
+// D
+'date' => '<NEW>Date',
+'date_format' => '<NEW>l, dS F Y',
+'date_time_format' => '<NEW>Y-m-d H:i:s',
+'description' => '<NEW>Description',
+'directories_bad_rights' => '<NEW>The following directories do not have write permission',
+'directories_bad_rights_cause' => '<NEW>To change this, use your SSH/SFTP client in order to set access permissions for each of these directories. The procedure is detailed in the install guide.',
+'directories_continue' => '<NEW>Once you are ready, you can reload this page.',
+'directories_missing' => '<NEW>The following directories have not been found',
+'directories_missing_possible_cause' => '<NEW>The possible cause is a bad lower or upper case handling. Please make sure that the lower and upper case letters of these directories match what is displayed above; if they don\'t, rename the directories using your SSH/SFTP client in order to correct the error.',
+'directories_setup_problem' => '<NEW>Problem in access permissions',
+'directories_setup_start' => '<NEW>Preliminary: Setting up access permissions',
 
 
 // E
-'entree_adresse_email' => '<NEW>Your e-mail address',
-'entree_identifiants_connexion' => '<NEW>Your connection identifiers...',
-'entree_login' => '<NEW>Your login',
-'entree_mot_passe' => '<NEW>Your password',
-'entree_nom_pseudo_1' => '<NEW>(Your name or alias)',
-'entree_signature' => '<NEW>Signature',
+'edit' => '<NEW>Edit',
+'edit_case_details' => '<NEW>Edit case details',
+'edit_case_information' => '<NEW>Edit case information',
+'enter_name_first' => '<NEW>First',
+'enter_name_last' => '<NEW>Last',
+'enter_name_middle' => '<NEW>Middle',
+'error_add_auth_no_rights' => '<NEW>You don\'t have permission to add access rights to this case!',
+'error_no_case_specified' => '<NEW>No case was specified',
+'error_no_edit_permission' => '<NEW>You don\'t have permission to edit this case!',
+'error_no_read_permission' => '<NEW>You don\'t have permission to view this case!',
+'error_no_such_case' => '<NEW>There\'s no such case!',
 
 
 // I
-'info_authentification_externe' => '<NEW>External authentication',
-'info_code_acces' => '<NEW>Do not forget your own access codes!',
-'info_creation_tables' => '<NEW>Fourth step: <B>Creation of the database tables</b>',
-'info_derniere_etape' => '<NEW>Last step: <B>It is completed!',
-'info_etape_suivante_1' => '<NEW>You can move to the next step.',
-'info_identification_publique' => '<NEW>Your public identity...',
-'info_informations_personnelles' => '<NEW>Fifth step: <b>Personal information</b>',
-'info_installation_legal_case_management' => 'Legal case management installation ...',
-'info_laisser_champs_vides' => '<NEW>leave these fields empty)',
-'info_mon_site_spip' => 'My Legal Case Management',
-'info_plus_cinq_car' => '<NEW>more than 5 characters',
-'info_plus_cinq_car_2' => '<NEW>(More than 5 characters)',
-'info_plus_trois_car' => '<NEW>(More than 3 characters)',
-'info_utilisation_spip' => '<NEW>LCM is now ready to be used...',
-'install_select_langue' => '<NEW>Select a language then click on the button "next" to start the installation procedure.',
+'info_free_software' => '<NEW>Free software <a href=\'http://www.lcm.ngo-bg.org/en\'>distributed</a> under the <a href=\'http://www.gnu.org/licenses/gpl.html\'>GPL licence</a>.',
+'info_more_than_five' => '<NEW>(More than 5 characters)',
+'info_more_than_three' => '<NEW>(More than 3 characters)',
+'info_name_of_person' => '<NEW>Name',
+'info_or' => 'или',
+'info_your_contact_information' => '<NEW>Your contact information ...',
+'input_connection_identifiers' => '<NEW>Your connection identifiers ...',
+'input_email' => '<NEW>E-mail address',
+'install_connection_login' => '<NEW>Connection login',
+'install_connection_password' => '<NEW>Connection password',
+'install_connection_succeeded' => '<NEW>Connection succeeded.',
+'install_create_new_database' => '<NEW><B>Create</b> a new database',
+'install_database_address' => '<NEW>Database address',
+'install_database_install_failed' => '<NEW>Go back to the previous page, select another database or create a new one. Verify the information provided by your host.',
+'install_database_installed_ok' => '<NEW>Your database is now ready to use.',
+'install_info_application_ready' => '<NEW>The system is now ready to be used...',
+'install_info_connection_login' => '<NEW>Sometimes matches your FTP access login and sometimes left empty.',
+'install_info_connection_password' => '<NEW>Sometimes matches your FTP access password and sometimes left empty.',
+'install_info_database_address' => '<NEW>(Often, this address matches the address of your site, sometimes it corresponds to the name "localhost", and sometimes it is left completely empty.)',
+'install_info_do_not_forget' => '<NEW>Do not forget your own access codes!',
+'install_info_go_back_verify_data' => '<NEW>Go back to the previous page, and verify the information you have provided.',
+'install_info_new_account_1' => '<NEW>The system will now provide you with a custom access to the site.',
+'install_info_new_account_2' => '<NEW>(Note: If this is a re-installation and your administrator access is still working, you can leave these fields empty)',
+'install_info_sql_connection' => '<NEW>Refer to the information provided to you by your system administrator: if your host supports SQL, it should give you the connection codes to SQL server.',
+'install_info_sql_connection_failed' => '<NEW><b>N.B.</b> On a number of servers, you must request the activation of your access to the SQL database before you can use it. If you are unable to connect, make sure that you actually made this request.',
+'install_next_step' => '<NEW>You can proceed to the next step.',
+'install_select_database' => '<NEW>Select the database that your host has attributed to you',
+'install_select_language' => '<NEW>Select a language then click on the button "next" to start the installation procedure.',
+'install_step_five' => '<NEW>Fifth step',
+'install_step_four' => '<NEW>Fourth step',
+'install_step_last' => '<NEW>Last step',
+'install_step_one' => '<NEW>First step',
+'install_step_three' => '<NEW>Third step',
+'install_step_two' => '<NEW>Second step',
+'install_title_admin_account' => '<NEW>Personal information for the administrator access',
+'install_title_connection_attempt' => '<NEW>Attempting to connect to the SQL database',
+'install_title_creating_database' => '<NEW>Creation of the database tables',
+'install_title_installation_start' => '<NEW>Legal Case Management Software installation ...',
+'install_title_select_database' => '<NEW>Choosing your database',
+'install_title_sql_connection' => '<NEW>Your SQL database connection',
+'install_warning_no_databases_1' => '<NEW>The installer could not read the names of the available databases.',
+'install_warning_no_databases_2' => '<NEW>Either there are no databases available, or the feature allowing the listing of the databases was disabled for security reasons (which is the case with a lot of hosts).',
+'install_warning_no_databases_3' => '<NEW>In case the second alternative was true, it is possible that a database named after your login could be usable:',
+'install_warning_nonfatal_sql_errors' => '<NEW>There were warnings while installing or upgrading the database, but they may not be fatal. Errors such as \'Table already exists\' can safely be ignored. Errors such as \'Syntax errors\' are most probably bugs and should be reported to the LCM development team ([lcm-devel@lists.sourceforge.net->mailto:lcm-devel@lists.sourceforge.net]).',
+'install_warning_update_impossible' => '<NEW><b>Warning!</b> Failed to update the SQL database from version @old_version@ to version @version@, maybe due to a permissions problem on the database. Please contact your administrator. A full technical listing of the possible errors is provided below.',
+
+
+// L
+'legal_reason' => '<NEW>Legal reason',
+'login_access_denied' => '<NEW>Access denied',
+'login_identifier_unknown' => '<NEW>The identifier "@login@" is unknown.',
+'login_info_login' => '<NEW>identifier for connection to the site',
+'login_login' => '<NEW>Login',
+'login_other_identifier' => '<NEW>connect with another identifier',
+'login_password' => '<NEW>Password',
+'login_password_forgotten' => '<NEW>password forgotten?',
+'login_password_incorrect' => '<NEW>Incorrect login or password.',
+'login_prefer_no_cookie' => '<NEW><b>If you prefer to refuse the cookies</b>, there is another connection method (less secure) at your disposal:',
+'login_register' => '<NEW>register',
+'login_remain_logged_on' => '<NEW>Remain logged-on for a few days',
+'login_without_cookie' => '<NEW>Identification without cookie',
+
+
+// N
+'new_case' => '<NEW>New case',
+'new_followup' => '<NEW>New followup',
+
+
+// O
+'organisations' => '<NEW>Organisations',
+
+
+// P
+'parameter' => '<NEW>Parameter',
+'public' => '<NEW>Public',
+
+
+// R
+'read' => '<NEW>Read',
+'reset' => '<NEW>Reset',
+
+
+// S
+'save' => '<NEW>Save',
+'status' => '<NEW>Status',
 
 
 // T
-'texte_annuaire_ldap_1' => '<NEW>If you have access to a (LDAP) directory, you can use it to automatically import users under SPIP.',
-'texte_informations_personnelles_1' => '<NEW>The system will provide you now with a custom access to the site.',
-'texte_informations_personnelles_2' => '<NEW>(Note: if it is a reinstallation, and your access is still working, you can',
-'title_software' => '&ETH;�&ETH;&frac34;&ETH;&#381;&ETH;&sup3;&ETH;&frac34;&Ntilde;�&ETH;&frac34;&ETH;&sup2;&ETH;&ordm;&ETH;&deg; &ETH;&frac12;&ETH;&deg; &ETH;&iquest;&Ntilde;&euro;&ETH;&frac34;&ETH;&sup3;&Ntilde;&euro;&ETH;&deg;&ETH;&frac14;&ETH;&micro;&ETH;&frac12; &ETH;&iquest;&Ntilde;&euro;&ETH;&frac34;&ETH;&#381;&Ntilde;�&ETH;&ordm;&Ntilde;� &ETH;&middot;&ETH;&deg; &Ntilde;�&ETH;&iquest;&Ntilde;&euro;&ETH;&deg;&ETH;&sup2;&ETH;&raquo;&ETH;&micro;&ETH;&frac12;&ETH;&#382;&ETH;&micro; &ETH;&frac12;&ETH;&deg; &Ntilde;&euro;&ETH;&micro;&ETH;&sup3;&ETH;&#382;&Ntilde;�&Ntilde;�&Ntilde;&euro;&ETH;&deg;&Ntilde;�&ETH;&#382;&Ntilde;� &ETH;&frac12;&ETH;&deg; &Ntilde;�&Ntilde;&euro;&ETH;&#382;&ETH;&#381;&ETH;&#382;&Ntilde;�&ETH;&micro;&Ntilde;�&ETH;&ordm;&ETH;&deg; &ETH;&iquest;&Ntilde;&euro;&ETH;&deg;&ETH;&ordm;&Ntilde;�&ETH;&#382;&ETH;&ordm;&ETH;&deg;',
+'title_error' => 'Грешка',
+'title_software' => 'Подготовка на програмен продукт за управление на регистрация на юридическа практика',
 'title_software_article' => '&ETH;�&ETH;&frac34;&ETH;&#381;&ETH;&sup3;&ETH;&frac34;&Ntilde;�&ETH;&frac34;&ETH;&sup2;&ETH;&ordm;&ETH;&deg; &ETH;&frac12;&ETH;&deg; &ETH;&iquest;&Ntilde;&euro;&ETH;&frac34;&ETH;&sup3;&Ntilde;&euro;&ETH;&deg;&ETH;&frac14;&ETH;&micro;&ETH;&frac12; &ETH;&iquest;&Ntilde;&euro;&ETH;&frac34;&ETH;&#381;&Ntilde;�&ETH;&ordm;&Ntilde;� &ETH;&middot;&ETH;&deg; &Ntilde;�&ETH;&iquest;&Ntilde;&euro;&ETH;&deg;&ETH;&sup2;&ETH;&raquo;&ETH;&micro;&ETH;&frac12;&ETH;&#382;&ETH;&micro; &ETH;&frac12;&ETH;&deg; &Ntilde;&euro;&ETH;&micro;&ETH;&sup3;&ETH;&#382;&Ntilde;�&Ntilde;�&Ntilde;&euro;&ETH;&deg;&Ntilde;�&ETH;&#382;&Ntilde;� &ETH;&frac12;&ETH;&deg; &Ntilde;�&Ntilde;&euro;&ETH;&#382;&ETH;&#381;&ETH;&#382;&Ntilde;�&ETH;&micro;&Ntilde;�&ETH;&ordm;&ETH;&deg; &ETH;&iquest;&Ntilde;&euro;&ETH;&deg;&ETH;&ordm;&Ntilde;�&ETH;&#382;&ETH;&ordm;&ETH;&deg;',
+'title_technical_message' => '<NEW>Technical message',
+'type' => '<NEW>Type',
+'typo_column' => ':',
+
+
+// V
+'value' => '<NEW>Value',
 
 
 // W
 'warning_already_installed' => '&ETH;&micro; &ETH;&sup2;&ETH;&micro;&Ntilde;�&ETH;&micro; &ETH;&#382;&ETH;&frac12;&Ntilde;�&Ntilde;�&ETH;&deg;&ETH;&raquo;&ETH;&#382;&Ntilde;&euro;&ETH;&deg;&ETH;&frac12;.',
-'warning_forbidden_area' => '&ETH;�&ETH;&deg;&ETH;&plusmn;&Ntilde;&euro;&ETH;&deg;&ETH;&frac12;&ETH;&micro;&ETH;&frac12;&ETH;&deg; &ETH;&frac34;&ETH;&plusmn;&ETH;&raquo;&ETH;&deg;&Ntilde;�&Ntilde;�'
+'warning_forbidden_area' => '&ETH;�&ETH;&deg;&ETH;&plusmn;&Ntilde;&euro;&ETH;&deg;&ETH;&frac12;&ETH;&micro;&ETH;&frac12;&ETH;&deg; &ETH;&frac34;&ETH;&plusmn;&ETH;&raquo;&ETH;&deg;&Ntilde;�&Ntilde;�',
+'warning_operation_failed' => '<NEW>The operation failed.',
+'warning_sql_connection_failed' => '<NEW>Connection to SQL database server failed.',
+'which_case' => '<NEW>Which case?',
+'write' => '<NEW>Write'
 
 );
 
