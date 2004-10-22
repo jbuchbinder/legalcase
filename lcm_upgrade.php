618174d92401bd7c9b6248ab88e6a0a1d8db47a2
mlutfy <mlutfy>
2004-10-22 07:27:41 +0000

    added test to see if upgrade necessary and added a bit of error checking, albeit not very useful

--- lcm_upgrade.php
+++ lcm_upgrade.php
@@ -1,20 +1,42 @@
 <?php
-	include('inc/inc_version.php');
-	include_lcm('inc_presentation');
 
-	// if upgraded needed
+include('inc/inc_version.php');
+include_lcm('inc_presentation');
+include_lcm('inc_meta');
 
+$current_version = lire_meta('lcm_db_version');
+if (!$current_version) $current_version = 0;
+
+// test if upgraded necessary
+if ($lcm_db_version <> $current_version) {
 	include_lcm('inc_db_upgrade');
-
+	
 	lcm_page_start("Database upgrade", "install");
+	
+	echo "\n<!-- Hide possibly confusing error messages: \n";
+	echo "\t** Upgrading from $current_version to $lcm_db_version **\n";
+	$log = upgrade_database($installed_db_version);
+	echo "-->\n";
 
-	upgrade_database($installed_db_version);
+	if ($log) {
+		echo "<div class='box_error'>\n";
+		echo "<p>An error occured while upgrading the database: <br/>$log<br/>
+			<a href='index.php'>Click here to go back to the main page.</a></p>\n";
+		echo "</div>\n";
+	} else {
+		echo "<div class='box_success'>\n";
+		echo "<p>The database upgrade was a success.
+			<a href='index.php'>Click here to go back to the main page.</a></p>\n";
+		echo "</div>\n";
+	}
+	
+	lcm_page_end();
+} else {
+	lcm_page_start("No database upgrade needed", "install");
 
-	echo "<div class='box_sucess'>\n";
-	echo "<p>Should be ok, but we should maybe test it with inc_db_test.php.
-		<a href='index.php'>Click here to go back to the main page</a></p>\n";
-	echo "</div>\n";
+	echo "<p><a href='index.php'>Click here to go back to the main page.</a></p>\n";
 
 	lcm_page_end();
+}
 
 ?>
