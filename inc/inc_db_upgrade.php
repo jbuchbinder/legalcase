23a24da88895f23ade93a70b4823eb54fbf0fd32
mlutfy <mlutfy>
2004-10-22 07:08:17 +0000

    - Moved alter-table-test into inc_db_test.php - Cleanup a bit the code - Removed some of the commented out code (left only the most interesting)

--- inc/inc_db_upgrade.php
+++ inc/inc_db_upgrade.php
@@ -8,18 +8,6 @@ include('inc/inc_version.php');
 include_lcm('inc_meta');
 include_lcm('inc_db');
 
-// [AG] Maybe not needed anymore
-//function upgrade_version ($version, $test = true) {
-//	if ($test) {
-//		lcm_query("REPLACE lcm_meta (name, value) VALUES ('version_lcm', '$version')");
-//		lcm_log("upgrading database to version: $version");
-//	} else {
-//		include_lcm('inc_lang');
-//		echo _T('install_warning_update_impossible', array('version' => $version));
-//		exit;
-//	}
-//}
-
 function upgrade_db_version ($version, $test = true) {
 	if ($test) {
 		lcm_query("REPLACE lcm_meta (name, value) VALUES ('lcm_db_version', '$version')");
@@ -43,6 +31,9 @@ function upgrade_database($old_db_version) {
 	//$lcm_db_version_current = read_meta('lcm_db_version');
 	// echo "VERSION = $version \n";
 
+	// [ML] I think we still need this
+	$lcm_db_version_current = read_meta('lcm_db_version');
+
 	// If there is no version mentioned in lcm_meta, then it is a new installation
 	// and therefore there is no need to upgrade.
 	// [AG] No need to doublecheck it. upgrade_database is invoked only when necessary
@@ -56,17 +47,13 @@ function upgrade_database($old_db_version) {
 	// Verify the rights to modify the database
 	//
 
-	lcm_query("DROP TABLE IF EXISTS lcm_test");
-	lcm_query("CREATE TABLE lcm_test (a INT)");
-	lcm_query("ALTER TABLE lcm_test ADD b INT");
-	lcm_query("INSERT INTO lcm_test (b) VALUES (1)");
-	$result = lcm_query("SELECT b FROM lcm_test");
-	lcm_query("ALTER TABLE lcm_test DROP b");
+	echo "\n<!-- Hide possibly confusing error messages: \n";
+	include_lcm('inc_db_test');
+	$alter_test_log = lcm_test_alter_table();
+	echo "-->\n";
 
-	if (!$result) {
-		$log .= "User does not have the right to modify the database\n";
-		return $log;
-	}
+	if ($alter_test_log)
+		return $alter_test_log;
 
 	//
 	// Upgrade the database accordingly to the current version
@@ -99,24 +86,6 @@ function upgrade_database($old_db_version) {
 
 /* [ML] I'm leaving this because it can provide us with interesting ideas
 	if ($lcm_version_current < 0.98) {
-		lcm_query("ALTER TABLE spip_articles ADD maj TIMESTAMP");
-		lcm_query("ALTER TABLE spip_articles ADD export VARCHAR(10) DEFAULT 'oui'");
-		lcm_query("ALTER TABLE spip_articles ADD images TEXT DEFAULT ''");
-		lcm_query("ALTER TABLE spip_articles ADD date_redac datetime DEFAULT '0000-00-00 00:00:00' NOT NULL");
-		lcm_query("ALTER TABLE spip_articles DROP INDEX id_article");
-		lcm_query("ALTER TABLE spip_articles ADD INDEX id_rubrique (id_rubrique)");
-		lcm_query("ALTER TABLE spip_articles ADD visites INTEGER DEFAULT '0' NOT NULL");
-		lcm_query("ALTER TABLE spip_articles ADD referers BLOB NOT NULL");
-
-		lcm_query("ALTER TABLE spip_auteurs ADD maj TIMESTAMP");
-		lcm_query("ALTER TABLE spip_auteurs ADD pgp BLOB NOT NULL");
-
-		lcm_query("ALTER TABLE spip_auteurs_articles ADD INDEX id_auteur (id_auteur), ADD INDEX id_article (id_article)");
-
-		lcm_query("ALTER TABLE spip_rubriques ADD maj TIMESTAMP");
-		lcm_query("ALTER TABLE spip_rubriques ADD export VARCHAR(10) DEFAULT 'oui', ADD id_import BIGINT DEFAULT '0'");
-
-		lcm_query("ALTER TABLE spip_breves ADD maj TIMESTAMP");
 		lcm_query("ALTER TABLE spip_breves DROP INDEX id_breve");
 		lcm_query("ALTER TABLE spip_breves DROP INDEX id_breve_2");
 		lcm_query("ALTER TABLE spip_breves ADD INDEX id_rubrique (id_rubrique)");
@@ -277,11 +246,6 @@ function upgrade_database($old_db_version) {
 		upgrade_version (1.414);
 	}
 
-	if ($lcm_version_current < 1.417) {
-		lcm_query("ALTER TABLE spip_syndic_articles DROP date_index");
-		upgrade_version (1.417);
-	}
-
 	if ($lcm_version_current < 1.418) {
 		$query = "SELECT * FROM spip_auteurs WHERE statut = 'admin' AND email != '' ORDER BY id_auteur LIMIT 0,1";
 		$result = lcm_query($query);
@@ -291,91 +255,6 @@ function upgrade_database($old_db_version) {
 			ecrire_metas();
 		}
 		upgrade_version (1.418);
-	}
-
-	if ($lcm_version_current < 1.419) {
-		$query = "ALTER TABLE spip_auteurs ADD alea_actuel TINYTEXT DEFAULT ''";
-		lcm_query($query);
-		$query = "ALTER TABLE spip_auteurs ADD alea_futur TINYTEXT DEFAULT ''";
-		lcm_query($query);
-		$query = "UPDATE spip_auteurs SET alea_futur = FLOOR(32000*RAND())";
-		lcm_query($query);
-		upgrade_version (1.419);
-	}
-
-	if ($lcm_version_current < 1.420) {
-		$query = "UPDATE spip_auteurs SET alea_actuel='' WHERE statut='nouveau'";
-		lcm_query($query);
-		upgrade_version (1.420);
-	}
-	
-	if ($lcm_version_current < 1.421) {
-		$query = "ALTER TABLE spip_articles ADD auteur_modif bigint(21) DEFAULT '0' NOT NULL";
-		lcm_query($query);
-		$query = "ALTER TABLE spip_articles ADD date_modif datetime DEFAULT '0000-00-00 00:00:00' NOT NULL";
-		lcm_query($query);
-		upgrade_version (1.421);
-	}
-
-	if ($lcm_version_current < 1.432) {
-		lcm_query("ALTER TABLE spip_articles DROP referers");
-		$query = "ALTER TABLE spip_articles ADD referers INTEGER DEFAULT '0' NOT NULL";
-		lcm_query($query);
-		$query = "ALTER TABLE spip_articles ADD popularite INTEGER DEFAULT '0' NOT NULL";
-		lcm_query($query);
-		upgrade_version (1.432);
-	}
-
-	if ($lcm_version_current < 1.436) {
-		$query = "ALTER TABLE spip_documents ADD date datetime DEFAULT '0000-00-00 00:00:00' NOT NULL";
-		lcm_query($query);
-		upgrade_version (1.436);
-	}
-
-	if ($lcm_version_current < 1.437) {
-		lcm_query("ALTER TABLE spip_visites ADD maj TIMESTAMP");
-		lcm_query("ALTER TABLE spip_visites_referers ADD maj TIMESTAMP");
-		upgrade_version (1.437);
-	}
-
-	if ($lcm_version_current < 1.438) {
-		lcm_query("ALTER TABLE spip_articles ADD INDEX id_secteur (id_secteur)");
-		lcm_query("ALTER TABLE spip_articles ADD INDEX statut (statut, date)");
-		upgrade_version (1.438);
-	}
-
-	if ($lcm_version_current < 1.439) {
-		lcm_query("ALTER TABLE spip_syndic ADD INDEX statut (statut, date_syndic)");
-		lcm_query("ALTER TABLE spip_syndic_articles ADD INDEX statut (statut)");
-		lcm_query("ALTER TABLE spip_syndic_articles CHANGE url url VARCHAR(255) NOT NULL");
-		lcm_query("ALTER TABLE spip_syndic_articles ADD INDEX url (url)");
-		upgrade_version (1.439);
-	}
-
-	if ($lcm_version_current < 1.440) {
-		lcm_query("ALTER TABLE spip_visites_temp CHANGE ip ip INTEGER UNSIGNED NOT NULL");
-		upgrade_version (1.440);
-	}
-
-	if ($lcm_version_current < 1.441) {
-		lcm_query("ALTER TABLE spip_visites_temp CHANGE date date DATE NOT NULL");
-		lcm_query("ALTER TABLE spip_visites CHANGE date date DATE NOT NULL");
-		lcm_query("ALTER TABLE spip_visites_referers CHANGE date date DATE NOT NULL");
-		upgrade_version (1.441);
-	}
-
-	if ($lcm_version_current < 1.442) {
-		$query = "ALTER TABLE spip_auteurs ADD prefs TINYTEXT NOT NULL";
-		lcm_query($query);
-		upgrade_version (1.442);
-	}
-
-	if ($lcm_version_current < 1.443) {
-		lcm_query("ALTER TABLE spip_auteurs CHANGE login login VARCHAR(255) BINARY NOT NULL");
-		lcm_query("ALTER TABLE spip_auteurs CHANGE statut statut VARCHAR(255) NOT NULL");
-		lcm_query("ALTER TABLE spip_auteurs ADD INDEX login (login)");
-		lcm_query("ALTER TABLE spip_auteurs ADD INDEX statut (statut)");
-		upgrade_version (1.443);
 	}
 
 	if ($lcm_version_current < 1.444) {
@@ -456,78 +335,6 @@ function upgrade_database($old_db_version) {
 		upgrade_version (1.464);
 	}
 
-	if ($lcm_version_current < 1.465) {
-		lcm_query("ALTER TABLE spip_articles CHANGE popularite popularite DOUBLE NOT NULL");
-		upgrade_version (1.465);
-	}
-
-	if ($lcm_version_current < 1.466) {
-		lcm_query("ALTER TABLE spip_auteurs ADD source VARCHAR(10) DEFAULT 'spip' NOT NULL");
-		upgrade_version (1.466);
-	}
-
-	if ($lcm_version_current < 1.468) {
-		lcm_query("ALTER TABLE spip_auteurs ADD INDEX en_ligne (en_ligne)");
-		lcm_query("ALTER TABLE spip_forum ADD INDEX statut (statut, date_heure)");
-		upgrade_version (1.468);
-	}
-
-	if ($lcm_version_current < 1.470) {
-		if ($lcm_version_current >= 1.467) {	// annule les "listes de diff"
-			lcm_query("DROP TABLE spip_listes");
-			lcm_query("ALTER TABLE spip_auteurs DROP abonne");
-			lcm_query("ALTER TABLE spip_auteurs DROP abonne_pass");
-		}
-		upgrade_version (1.470);
-	}
-
-	if ($lcm_version_current < 1.471) {
-		if ($lcm_version_current >= 1.470) {	// annule les "maj"
-			lcm_query("ALTER TABLE spip_auteurs_articles DROP maj TIMESTAMP");
-			lcm_query("ALTER TABLE spip_auteurs_rubriques DROP maj TIMESTAMP");
-			lcm_query("ALTER TABLE spip_auteurs_messages DROP maj TIMESTAMP");
-			lcm_query("ALTER TABLE spip_documents_articles DROP maj TIMESTAMP");
-			lcm_query("ALTER TABLE spip_documents_rubriques DROP maj TIMESTAMP");
-			lcm_query("ALTER TABLE spip_documents_breves DROP maj TIMESTAMP");
-			lcm_query("ALTER TABLE spip_mots_articles DROP maj TIMESTAMP");
-			lcm_query("ALTER TABLE spip_mots_breves DROP maj TIMESTAMP");
-			lcm_query("ALTER TABLE spip_mots_rubriques DROP maj TIMESTAMP");
-			lcm_query("ALTER TABLE spip_mots_syndic DROP maj TIMESTAMP");
-			lcm_query("ALTER TABLE spip_mots_forum DROP maj TIMESTAMP");
-		}
-		upgrade_version (1.471);
-	}
-
-	if ($lcm_version_current < 1.472) {
-		lcm_query("ALTER TABLE spip_referers ADD visites_jour INTEGER UNSIGNED NOT NULL");
-		upgrade_version (1.472);
-	}
-
-	if ($lcm_version_current < 1.473) {
-		lcm_query("UPDATE spip_syndic_articles SET url = REPLACE(url, '&amp;', '&')");
-		lcm_query("UPDATE spip_syndic SET url_site = REPLACE(url_site, '&amp;', '&')");
-		upgrade_version (1.473);
-	}
-
-	if ($lcm_version_current < 1.600) {
-		include_ecrire('inc_index.php3');
-		purger_index();
-		creer_liste_indexation();
-		upgrade_version (1.600);
-	}
-
-	if ($lcm_version_current < 1.601) {
-		lcm_query("ALTER TABLE spip_forum ADD INDEX id_syndic (id_syndic)");
-		upgrade_version (1.601);
-	}
-
-	if ($lcm_version_current < 1.603) {
-		// supprimer les fichiers deplaces
-		@unlink('inc_meta_cache.php3');
-		@unlink('data/engines-list.ini');
-		upgrade_version (1.603);
-	}
-
 	if ($lcm_version_current < 1.604) {
 		lcm_query("ALTER TABLE spip_auteurs ADD lang VARCHAR(10) DEFAULT '' NOT NULL");
 		$u = lcm_query("SELECT * FROM spip_auteurs WHERE prefs LIKE '%spip_lang%'");
@@ -569,145 +376,6 @@ function upgrade_database($old_db_version) {
 			&& lcm_query("SELECT extra FROM spip_rubriques")
 			&& lcm_query("SELECT extra FROM spip_mots")
 			);
-	}
-
-	if ($lcm_version_current < 1.703) {
-		lcm_query("ALTER TABLE spip_articles ADD lang VARCHAR(10) DEFAULT '' NOT NULL");
-		lcm_query("ALTER TABLE spip_rubriques ADD lang VARCHAR(10) DEFAULT '' NOT NULL");
-		upgrade_version (1.703);
-	}
-
-	if ($lcm_version_current < 1.704) {
-		lcm_query("ALTER TABLE spip_articles ADD INDEX lang (lang)");
-		lcm_query("ALTER TABLE spip_auteurs ADD INDEX lang (lang)");
-		lcm_query("ALTER TABLE spip_rubriques ADD INDEX lang (lang)");
-		upgrade_version (1.704);
-	}
-
-	if ($lcm_version_current < 1.705) {
-		lcm_query("ALTER TABLE spip_articles ADD langue_choisie VARCHAR(3) DEFAULT 'non'");
-		lcm_query("ALTER TABLE spip_rubriques ADD langue_choisie VARCHAR(3) DEFAULT 'non'");
-		upgrade_version (1.705);
-	}
-
-	if ($lcm_version_current < 1.707) {
-		lcm_query("UPDATE spip_articles SET langue_choisie='oui' WHERE MID(lang,1,1) != '.' AND lang != ''");
-		lcm_query("UPDATE spip_articles SET lang=MID(lang,2,8) WHERE langue_choisie = 'non'");
-		lcm_query("UPDATE spip_rubriques SET langue_choisie='oui' WHERE MID(lang,1,1) != '.' AND lang != ''");
-		lcm_query("UPDATE spip_rubriques SET lang=MID(lang,2,8) WHERE langue_choisie = 'non'");
-		upgrade_version (1.707);
-	}
-
-	if ($lcm_version_current < 1.708) {
-		lcm_query("ALTER TABLE spip_breves ADD lang VARCHAR(10) DEFAULT '' NOT NULL");
-		lcm_query("ALTER TABLE spip_breves ADD langue_choisie VARCHAR(3) DEFAULT 'non'");
-		upgrade_version (1.708);
-	}
-
-	if ($lcm_version_current < 1.709) {
-		lcm_query("ALTER TABLE spip_articles ADD id_trad bigint(21) DEFAULT '0' NOT NULL");
-		lcm_query("ALTER TABLE spip_articles ADD INDEX id_trad (id_trad)");
-		upgrade_version (1.709);
-	}
-
-	if ($lcm_version_current < 1.717) {
-		lcm_query("ALTER TABLE spip_articles ADD INDEX date_modif (date_modif)");
-		upgrade_version (1.717);
-	}
-
-	if ($lcm_version_current < 1.718) {
-		lcm_query("ALTER TABLE spip_referers DROP domaine");
-		lcm_query("ALTER TABLE spip_referers_articles DROP domaine");
-		lcm_query("ALTER TABLE spip_referers_temp DROP domaine");
-		upgrade_version (1.718);
-	}
-
-	if ($lcm_version_current < 1.722) {
-		lcm_query("ALTER TABLE spip_articles ADD nom_site tinytext NOT NULL");
-		lcm_query("ALTER TABLE spip_articles ADD url_site VARCHAR(255) NOT NULL");
-		lcm_query("ALTER TABLE spip_articles ADD INDEX url_site (url_site)");
-		if ($lcm_version_current >= 1.720) {
-			lcm_query("UPDATE spip_articles SET url_site=url_ref");
-			lcm_query("ALTER TABLE spip_articles DROP INDEX url_ref");
-			lcm_query("ALTER TABLE spip_articles DROP url_ref");
-		}
-		upgrade_version (1.722);
-	}
-
-	if ($lcm_version_current < 1.723) {
-		if ($lcm_version_current == 1.722) {
-			lcm_query("ALTER TABLE spip_articles MODIFY url_site VARCHAR(255) NOT NULL");
-			lcm_query("ALTER TABLE spip_articles DROP INDEX url_site;");
-			lcm_query("ALTER TABLE spip_articles ADD INDEX url_site (url_site);");
-		}
-		upgrade_version (1.723);
-	}
-
-	if ($lcm_version_current < 1.724) {
-		lcm_query("ALTER TABLE spip_messages ADD date_fin datetime DEFAULT '0000-00-00 00:00:00' NOT NULL");
-		upgrade_version (1.724);
-	}
-
-	if ($lcm_version_current < 1.726) {
-		lcm_query("ALTER TABLE spip_auteurs ADD low_sec tinytext NOT NULL");
-		upgrade_version (1.726);
-	}
-
-	if ($lcm_version_current < 1.727) {
-		// occitans : oci_xx -> oc_xx
-		lcm_query("UPDATE spip_auteurs SET lang=REPLACE(lang,'oci_', 'oc_') WHERE lang LIKE 'oci_%'");
-		lcm_query("UPDATE spip_rubriques SET lang=REPLACE(lang,'oci_', 'oc_') WHERE lang LIKE 'oci_%'");
-		lcm_query("UPDATE spip_articles SET lang=REPLACE(lang,'oci_', 'oc_') WHERE lang LIKE 'oci_%'");
-		lcm_query("UPDATE spip_breves SET lang=REPLACE(lang,'oci_', 'oc_') WHERE lang LIKE 'oci_%'");
-		upgrade_version (1.727);
-	}
-
-	// Ici version 1.7 officielle
-
-	if ($lcm_version_current < 1.728) {
-		lcm_query("ALTER TABLE spip_articles ADD id_version int unsigned DEFAULT '0' NOT NULL");
-		upgrade_version (1.728);
-	}
-
-	if ($lcm_version_current < 1.730) {
-		lcm_query("ALTER TABLE spip_articles ADD idx ENUM('', '1', 'non', 'oui', 'idx') DEFAULT '' NOT NULL");
-		lcm_query("ALTER TABLE spip_articles INDEX idx (idx)");
-		lcm_query("ALTER TABLE spip_auteurs ADD idx ENUM('', '1', 'non', 'oui', 'idx') DEFAULT '' NOT NULL");
-		lcm_query("ALTER TABLE spip_auteurs INDEX idx (idx)");
-		lcm_query("ALTER TABLE spip_breves ADD idx ENUM('', '1', 'non', 'oui', 'idx') DEFAULT '' NOT NULL");
-		lcm_query("ALTER TABLE spip_breves INDEX idx (idx)");
-		lcm_query("ALTER TABLE spip_mots ADD idx ENUM('', '1', 'non', 'oui', 'idx') DEFAULT '' NOT NULL");
-		lcm_query("ALTER TABLE spip_mots INDEX idx (idx)");
-		lcm_query("ALTER TABLE spip_rubriques ADD idx ENUM('', '1', 'non', 'oui', 'idx') DEFAULT '' NOT NULL");
-		lcm_query("ALTER TABLE spip_rubriques INDEX idx (idx)");
-		lcm_query("ALTER TABLE spip_syndic ADD idx ENUM('', '1', 'non', 'oui', 'idx') DEFAULT '' NOT NULL");
-		lcm_query("ALTER TABLE spip_syndic INDEX idx (idx)");
-		lcm_query("ALTER TABLE spip_forum ADD idx ENUM('', '1', 'non', 'oui', 'idx') DEFAULT '' NOT NULL");
-		lcm_query("ALTER TABLE spip_forum ADD INDEX idx (idx)");
-		lcm_query("ALTER TABLE spip_signatures ADD idx ENUM('', '1', 'non', 'oui', 'idx') DEFAULT '' NOT NULL");
-		lcm_query("ALTER TABLE spip_signatures INDEX idx (idx)");
-		upgrade_version (1.730);
-	}
-
-	if ($lcm_version_current < 1.731) {	// reindexer les docs allemands et vietnamiens
-		lcm_query("UPDATE spip_articles SET idx='1' where lang IN ('de','vi')");
-		lcm_query("UPDATE spip_rubriques SET idx='1' where lang IN ('de','vi')");
-		lcm_query("UPDATE spip_breves SET idx='1' where lang IN ('de','vi')");
-		lcm_query("UPDATE spip_auteurs SET idx='1' where lang IN ('de','vi')");
-		upgrade_version (1.731);
-	}
-
-	if ($lcm_version_current < 1.732) {	// en correction d'un vieux truc qui avait fait sauter le champ inclus sur les bases version 1.415
-		lcm_query ("ALTER TABLE spip_documents ADD inclus  VARCHAR(3) DEFAULT 'non'");
-		upgrade_version (1.732);
-	}
-
-	if ($lcm_version_current < 1.733) {
-		// lcm_query("ALTER TABLE spip_articles ADD id_version int unsigned DEFAULT '0' NOT NULL");
-		lcm_query("DROP TABLE spip_versions");
-		lcm_query("DROP TABLE spip_versions_fragments");
-		creer_base();
-		upgrade_version(1.733);
 	}
 
 	if ($lcm_version_current < 1.734) {
