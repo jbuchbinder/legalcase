49c99d6735c15d49f073fc7e8773a545fdba24ad
mlutfy <mlutfy>
2004-10-23 20:24:51 +0000

    misc cleanups and functions removed

--- inc/inc_mail.php
+++ inc/inc_mail.php
@@ -1,15 +1,8 @@
 <?php
 
-//
 // Execute this file only once
 if (defined('_INC_MAIL')) return;
 define('_INC_MAIL', '1');
-
-
-//
-// Chez lyconiania, envoyer un mail coupe la connection MySQL (sic)
-// [ML] removed
-//
 
 $GLOBALS['queue_mails'] = '';
 
@@ -33,20 +26,20 @@ function tester_mail() {
 	return $test_mail;
 }
 
-function envoyer_mail($email, $sujet, $texte, $from = "", $headers = "") {
+function send_email($email, $sujet, $texte, $from = "", $headers = "") {
 	global $hebergeur, $queue_mails, $flag_wordwrap, $os_serveur;
 	include_lcm('inc_filters');
 
 	if (!$from) {
 		$email_envoi = lire_meta("email_envoi");
-		$from = email_valide($email_envoi) ? $email_envoi : $email;
+		$from = is_valid_email($email_envoi) ? $email_envoi : $email;
 	}
-	if (!email_valide($email)) return false;
+	if (!is_valid_email($email)) return false;
 	if ($email == _T('info_mail_fournisseur')) return false; // tres fort
 
-	spip_log("mail ($email): $sujet");
+	lcm_log("mail ($email): $sujet");
 
-	$charset = lire_meta('charset');
+	$charset = read_meta('charset');
 
 	$headers = "From: $from\n".
 		"MIME-Version: 1.0\n".
@@ -90,104 +83,11 @@ function envoyer_mail($email, $sujet, $texte, $from = "", $headers = "") {
 	}
 }
 
-function extrait_article($row) {
-	include_lcm('inc_texte');
-	include_lcm('inc_filtres');
-
-	$adresse_site = lire_meta("adresse_site");
-
-	$id_article = $row[0];
-	$titre = $row[2];
-	$chapo = $row[6];
-	$texte = $row[7];
-	$date = $row[9];
-	$statut = $row[10];
-
-	$les_auteurs = "";
- 	$query = "SELECT auteurs.* FROM spip_auteurs AS auteurs, spip_auteurs_articles AS lien WHERE lien.id_article=$id_article AND auteurs.id_auteur=lien.id_auteur";
-	$result_auteurs = spip_query($query);
-
-	while ($row = spip_fetch_array($result_auteurs)) {
-		$nom_auteur = trim(supprimer_tags(typo($row['nom'])));
-
-		if ($les_auteurs) $les_auteurs .= ', ';
-		$les_auteurs .= $nom_auteur;
-	}
-
-	$extrait = "** $titre **\n";
-	if ($les_auteurs) $extrait .= _T('info_les_auteurs_1', array('les_auteurs' => $les_auteurs));
-	if ($statut == 'publie') $extrait .= " "._T('date_fmt_nomjour_date', array('nomjour'=>nom_jour($date), 'date'=>affdate($date)));
-	$extrait .= "\n\n".textebrut(propre(couper_intro("$chapo<p>$texte", 700)))."\n\n";
-	if ($statut == 'publie') $extrait .= "-> ".$adresse_site."/spip_redirect.php3?id_article=$id_article\n\n";
-	return $extrait;
-}
-
-
 function nettoyer_titre_email($titre) {
 	$titre = ereg_replace("\n", ' ', supprimer_tags($titre));
 	return ($titre);
 }
 
-function envoyer_mail_publication($id_article) {
-	global $connect_nom;
-	$adresse_suivi = lire_meta("adresse_suivi");
-	$adresse_site = lire_meta("adresse_site");
-	$nom_site_spip = lire_meta("nom_site");
-	$suivi_edito = lire_meta("suivi_edito");
 
-	if ($suivi_edito == "oui") {
-		$query = "SELECT * FROM spip_articles WHERE id_article = $id_article";
-		$result = spip_query($query);
-
-		if ($row = spip_fetch_array($result)) {
-
-			// selectionne langue du site
-			$lang_utilisateur = $GLOBALS['lcm_lang'];
-			changer_langue($GLOBALS['langue_site']);
-
-			$titre = nettoyer_titre_email($row['titre']);
-
-			$sujet = _T('info_publie_1', array('nom_site_spip' => $nom_site_spip, 'titre' => $titre));
-			$courr = _T('info_publie_2')."\n\n";
-			$nom = trim(supprimer_tags(typo($connect_nom)));
-			$courr .= _T('info_publie_01', array('titre' => $titre, 'connect_nom' => $nom))."\n\n\n";
-			$courr = $courr . extrait_article($row);
-			envoyer_mail($adresse_suivi, $sujet, $courr);
-
-			// reinstalle la langue utilisateur (au cas ou)
-			changer_langue($lang_utilisateur);
-		}
-	}
-}
-
-function envoyer_mail_proposition($id_article) {
-	$adresse_suivi = lire_meta("adresse_suivi");
-	$adresse_site = lire_meta("adresse_site");
-	$nom_site_spip = lire_meta("nom_site");
-	$suivi_edito = lire_meta("suivi_edito");
-
-	if ($suivi_edito == "oui") {
-		$query = "SELECT * FROM spip_articles WHERE id_article = $id_article";
-		$result = spip_query($query);
-
-		if ($row = spip_fetch_array($result)) {
-			$lang_utilisateur = $GLOBALS['lcm_lang'];
-			changer_langue($GLOBALS['langue_site']);
-
-			$titre = nettoyer_titre_email($row['titre']);
-
-			$sujet = _T('info_propose_1', array('nom_site_spip' => $nom_site_spip, 'titre' => $titre));
-			$courr = _T('info_propose_2')."\n\n";
-			$courr .= _T('info_propose_3', array('titre' => $titre))."\n";
-			$courr .= _T('info_propose_4')."\n";
-			$courr .= _T('info_propose_5')."\n";
-			$courr .= $adresse_site."/ecrire/articles.php3?id_article=$id_article\n\n\n";
-			$courr = $courr . extrait_article($row);
-			envoyer_mail($adresse_suivi, $sujet, $courr);
-
-			changer_langue($lang_utilisateur);
-		}
-	}
-}
 
 ?>
