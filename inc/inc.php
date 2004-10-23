3a80d0334474319a41a74c0368181d8bd7341aee
mlutfy <mlutfy>
2004-10-23 10:16:12 +0000

    cleanup of useless code, started adding prefs for color/theme

--- inc/inc.php
+++ inc/inc.php
@@ -20,10 +20,8 @@ if (!@file_exists('data/inc_meta_cache.php'))
 
 //
 // Preferences for presentation
+// Can be done from any screen, but for now most is in config_author.php
 //
-
-lcm_log("1- lang = " . $GLOBALS['HTTP_COOKIE_VARS']['lcm_lang']);
-lcm_log("2- session = " . $author_session['lang']);
 
 // [ML] This is very important (but dirty hack) to change the language
 // from config_author.php, without passing by lcm_cookie.php
@@ -32,121 +30,33 @@ if ($sel_language)
 else
 	$lang = $GLOBALS['HTTP_COOKIE_VARS']['lcm_lang'];
 
-// if ($lang = $GLOBALS['HTTP_COOKIE_VARS']['lcm_lang'] AND $lang <> $author_session['lang'] AND lcm_set_language($lang)) {
 if ($lang AND $lang <> $author_session['lang'] AND lcm_set_language($lang)) {
-	lcm_query("UPDATE lcm_author SET lang = '".addslashes($lang)."' WHERE id_author  = $connect_id_auteur");
+	lcm_query("UPDATE lcm_author 
+				SET   lang = '".addslashes($lang)."' 
+				WHERE id_author  = " .$author_session['id_author']);
 	$author_session['lang'] = $lang;
 	lcm_add_session($author_session, $lcm_session);
-	lcm_log("session re-saved");
-} else {
-	lcm_log("session NOT touched");
 }
 
-if ($set_couleur) {
-	$prefs['couleur'] = floor($set_couleur);
+if ($sel_theme) {
+	$prefs['theme'] = floor($sel_theme);
 	$prefs_mod = true;
-}
-if ($set_disp) {
-	$prefs['display'] = floor($set_disp);
-	$prefs_mod = true;
-}
-if ($set_options == 'avancees' OR $set_options == 'basiques') {
-	$prefs['options'] = $set_options;
-	$prefs_mod = true;
+	lcm_log("Theme = " . $sel_theme);
 }
 
-if ($set_ecran) {
-	// Set a cookie, since this features depends more on the navigator than on the user
-	// ecran == screen/monitor
-	lcm_setcookie('spip_ecran', $set_ecran, time() + 365 * 24 * 3600);
-	$spip_ecran = $set_ecran;
+// Update user preferences if modified
+if ($prefs_mod) {
+	lcm_query("UPDATE lcm_author	
+				SET   prefs = '".addslashes(serialize($prefs))."' 
+				WHERE id_author = " . $author_session['id_author']);
 }
-if (!$spip_ecran) $spip_ecran = "etroit";
 
-// deux globales (compatibilite ascendante)
-$options      = $prefs['options'];
-$spip_display = $prefs['display'];
-
-
-// Green
-if (!$couleurs_spip[1]) $couleurs_spip[1] = array (
-		"couleur_foncee" => "#9DBA00",
-		"couleur_claire" => "#C5E41C",
-		"couleur_lien" => "#657701",
-		"couleur_lien_off" => "#A6C113"
-);
-// Light Violet
-if (!$couleurs_spip[2]) $couleurs_spip[2] = array (
-		"couleur_foncee" => "#eb68b3",
-		"couleur_claire" => "#ffa9e6",
-		"couleur_lien" => "#E95503",
-		"couleur_lien_off" => "#8F004D"
-);
-// Orange
-if (!$couleurs_spip[3]) $couleurs_spip[3] = array (
-		"couleur_foncee" => "#fa9a00",
-		"couleur_claire" => "#ffc000",
-		"couleur_lien" => "#81A0C1",
-		"couleur_lien_off" => "#FF5B00"
-);
-// Salmon
-if (!$couleurs_spip[4]) $couleurs_spip[4] = array (
-		"couleur_foncee" => "#CDA261",
-		"couleur_claire" => "#FFDDAA",
-		"couleur_lien" => "#5E0283",
-		"couleur_lien_off" => "#472854"
-);
-//  Light blue
-if (!$couleurs_spip[5]) $couleurs_spip[5] = array (
-		"couleur_foncee" => "#5da7c5",
-		"couleur_claire" => "#97d2e1",
-		"couleur_lien" => "#869100",
-		"couleur_lien_off" => "#5B55A0"
-);
-//  Grey
-if (!$couleurs_spip[6]) $couleurs_spip[6] = array (
-		"couleur_foncee" => "#727D87",
-		"couleur_claire" => "#C0CAD4",
-		"couleur_lien" => "#854270",
-		"couleur_lien_off" => "#666666"
-);
-
-
-$choix_couleur = $prefs['couleur'];
-if (strlen($couleurs_spip[$choix_couleur]['couleur_foncee']) < 7) $choix_couleur = 1;
-
-$couleur_foncee = $couleurs_spip[$choix_couleur]['couleur_foncee'];
-$couleur_claire = $couleurs_spip[$choix_couleur]['couleur_claire'];
-$couleur_lien = $couleurs_spip[$choix_couleur]['couleur_lien'];
-$couleur_lien_off = $couleurs_spip[$choix_couleur]['couleur_lien_off'];
-
-
-//
-// Version management
-//
-
-write_metas();
-
-/* [ML] Not needed anymore, perhaps, since done with db_version */
-/*
-$installed_version = (double) read_meta('version_lcm');
-if ($installed_version <> $lcm_version) {
-	lcm_page_start();
-	if (!$installed_version)
-		$installed_version = _T('info_anterieur');
-
-	echo "<p>[ML] Installed version = $installed_version ; lcm_version = $lcm_version </p>"; // FIXME
-	echo "<blockquote><blockquote><h4><font color='red'>"._T('title_technical_message')."</font><br> "._T('info_procedure_maj_version')."</h4>
-	"._T('info_administrateur_site_01')." <a href='upgrade.php3'>"._T('info_administrateur_site_02')."</a></blockquote></blockquote><p>";
-
-	lcm_page_end();
-	exit;
-} */
 
 //
 // Database version management
 // 
 
+write_metas();
 $installed_db_version = read_meta('lcm_db_version');
 
 if ($installed_db_version <> $lcm_db_version) {
