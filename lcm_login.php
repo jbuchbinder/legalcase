41ee8ba10f1c988bda7b8ab7a93f01c104fe46c4
mlutfy <mlutfy>
2004-10-23 18:20:40 +0000

    site description is shown if it is non-empty

--- lcm_login.php
+++ lcm_login.php
@@ -13,12 +13,23 @@ include_lcm('inc_login');
 lcm_html_start(_T('login_acces_prive'));
 
 global $spip_lang_right;
-$lcm_site_name = read_meta('site_name');
+
+$site_name = read_meta('site_name');
+if (! $site_name)
+	$site_name = _T('title_software');
+
+// may be empty
+$site_desc = read_meta('site_description');
 
 echo "<p>&nbsp;</p>\n";
 echo "<center><table width='400'><tr><td width='400'>\n";
 echo "<div align='center'>\n";
-echo "<h3>" . $lcm_site_name . "</h3>\n";
+echo "<h3>" . $site_name;
+
+if ($site_desc)
+	echo "<div style='font-size: 80%; font-weight: normal;'>" . $site_desc . "</div>";
+
+echo "</h3>\n";
 echo "<div align='" . $spip_lang_right . "'>" . menu_languages() . "</div>\n";
 echo "</div>\n";
 
