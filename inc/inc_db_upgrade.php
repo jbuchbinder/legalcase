242af9d187c5bb9424bd2ed7695cde5338bc84b4
mlutfy <mlutfy>
2004-10-27 09:36:19 +0000

    fixed borked regexp in site_address meta

--- inc/inc_db_upgrade.php
+++ inc/inc_db_upgrade.php
@@ -110,13 +110,15 @@ function upgrade_database($old_db_version) {
 		$site_address = read_meta('site_address');
 
 		if (! $site_address) {
+			global $HTTP_SERVER_VARS;
+	
 			// Replace www.site.net/foo/name.php -> www.site.net/foo/
-			$site_address = $GLOBALS['HTTP_SERVER_VARS']['REQUEST_URI'];
-			$site_address = preg_replace("/\/.*\.php$/\//", $site_address);
+			$site_address = $HTTP_SERVER_VARS['REQUEST_URI'];
+			if (!$site_address) $site_address = $_ENV['PHP_SELF']; // [ML] unsure
+			$site_address = preg_replace("/\/[^\/]+\.php$/", "/", $site_address);
 			$site_address = 'http://' . $GLOBALS['SERVER_NAME'] . $site_address;
-
+	
 			write_meta('site_address', $site_address);
-			write_metas();
 		}
 
 		// Added 'trash' and 'suspended'
