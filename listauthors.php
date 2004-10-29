98940a7dc31368f42239d9645efd8e6379655cd3
antzi <antzi>
2004-10-29 17:57:07 +0000

    Added 'Edit' link

--- listauthors.php
+++ listauthors.php
@@ -70,7 +70,7 @@ if ($list_pos>0)
 </form>
 
 <table border='1' align='center' class='tbl_data'>
-<tr><th class='tbl_head'>Author name</th><th class='tbl_head'>Status</th></tr>
+<tr><th class='tbl_head'>Name</th><th class='tbl_head'>Status</th></tr><th class='tbl_head'>Action</th></tr>
 <?php
 // Process the output of the query
 for ($i = 0 ; (($i<$GLOBALS['list_len']) && ($row = lcm_fetch_array($result))) ; $i++) {
@@ -82,8 +82,8 @@ for ($i = 0 ; (($i<$GLOBALS['list_len']) && ($row = lcm_fetch_array($result))) ;
 //	if ( ) echo '</a>';
 	echo "</td>\n<td class='tbl_data'>";
 	echo clean_output($row['status']);
-//	if ( )
-//		echo '<a href=".php?author=' . $row['id_author'] . '">Edit author</a>';
+	if ($GLOBALS['author_session']['status'] = 'admin')
+		echo '<a href="edit_author.php?author=' . $row['id_author'] . '">Edit</a>';
 	echo "</td></tr>\n";
 }
 
