4fcc18bb592aa3ea6b9d34f8d693c833aad75243
makaveev <makaveev>
2004-11-05 12:03:34 +0000

    added some classes to html tags

--- listauthors.php
+++ listauthors.php
@@ -64,14 +64,14 @@ if ($list_pos>0)
 ?> />&nbsp;<input type="submit" name="submit" value="Search" class="search_form_btn" />
 </form>
 
-<table border='0' width='99%' align='center' class='tbl_data'>
-<tr><th class='tbl_head'>Name</th><th class='tbl_head'>Status</th><th class='tbl_head'>Action</th></tr>
+<table border='0' width='99%' align='center' class='tbl_usr_dtl'>
+<tr><th class='heading'>Name</th><th class='heading'>Status</th><th class='heading'>Action</th></tr>
 <?php
 // Process the output of the query
 for ($i = 0 ; (($i<$prefs['page_rows']) && ($row = lcm_fetch_array($result))) ; $i++) {
 	// Show author name
 	echo "<tr><td class='tbl_cont_" . ($i % 2 ? "dark" : "light") . "'>";
-//	if ( ) echo '<a href=".php?author=' . $row['id_author'] . '">';
+//	if ( ) echo '<a href=".php?author=' . $row['id_author'] . '" class="content_link">';
 	echo highlight_matches(clean_output($row['name_first'] . ' ' . $row['name_middle'] . ' '
 		. $row['name_last']),$find_author_string);
 //	if ( ) echo '</a>';
@@ -79,14 +79,14 @@ for ($i = 0 ; (($i<$prefs['page_rows']) && ($row = lcm_fetch_array($result))) ; 
 	echo clean_output($row['status']);
 	echo "</td>\n<td class='tbl_cont_" . ($i % 2 ? "dark" : "light") . "'>";
 	if ($GLOBALS['author_session']['status'] = 'admin')
-		echo '<a href="edit_author.php?author=' . $row['id_author'] . '">Edit</a>';
+		echo '<a href="edit_author.php?author=' . $row['id_author'] . '" class="content_link">Edit</a>';
 	echo "</td></tr>\n";
 }
 
 ?>
 </table>
 
-<a href="edit_author.php?author=0">Add author</a>
+<a href="edit_author.php?author=0" class="content_link">Add author</a>
 
 <?php
 
@@ -95,7 +95,7 @@ if ($list_pos>0) {
 	echo '<a href="listauthors.php';
 	if ($list_pos>$prefs['page_rows']) echo '?list_pos=' . ($list_pos - $prefs['page_rows']);
 	if (strlen($find_author_string)>1) echo "&amp;find_author_string=" . rawurlencode($find_author_string);
-	echo '">< Prev</a> ';
+	echo '" class="content_link">< Prev</a> ';
 }
 
 // Show page numbers with direct links
@@ -106,7 +106,7 @@ if ($list_pages>1) {
 		else {
 			echo '<a href="listauthors.php?list_pos=' . ($i*$prefs['page_rows']);
 			if (strlen($find_author_string)>1) echo "&amp;find_author_string=" . rawurlencode($find_author_string);
-			echo '">' . ($i+1) . '</a> ';
+			echo '" class="content_link">' . ($i+1) . '</a> ';
 		}
 	}
 }
@@ -116,7 +116,7 @@ $next_pos = $list_pos + $prefs['page_rows'];
 if ($next_pos<$number_of_rows) {
 	echo "<a href=\"listauthors.php?list_pos=$next_pos";
 	if (strlen($find_author_string)>1) echo "&amp;find_author_string=" . rawurlencode($find_author_string);
-	echo '">Next ></a>';
+	echo '" class="content_link">Next ></a>';
 }
 
 lcm_page_end();
--- sel_cli_org.php
+++ sel_cli_org.php
@@ -7,7 +7,7 @@ if ($org>0) {
 	// Prepare query
 	$q = "SELECT *
 		  FROM lcm_client_org
-		  WHERE id_org=$case";
+		  WHERE id_org='$case'";
 
 	// Do the query
 	$result = lcm_query($q);
@@ -28,12 +28,12 @@ if ($org>0) {
 	$result = lcm_query($q);
 ?>
 <form action="add_cli_org.php" method="post">
-	<table border>
-		<caption>List if persons</caption>
+<h3>List if persons</h3>
+	<table class="tbl_usr_dtl">
 		<tr>
-			<th></th>
-			<th>Person name</th>
-			<th></th>
+			<th class="heading">&nbsp;</th>
+			<th class="heading">Person name</th>
+			<th class="heading">&nbsp;</th>
 		</tr>
 <?php
 	while ($row = lcm_fetch_array($result)) {
@@ -41,21 +41,21 @@ if ($org>0) {
 		<tr>
 			<td><input type="checkbox" name="clients[]" value="<?php echo $row['id_client']; ?>"></td>
 			<td><?php echo $row['name_first'] . ' ' . $row['name_middle'] . ' ' . $row['name_last']; ?></td>
-			<td><a href="edit_client.php?client=<?php echo $row['id_client']; ?>">Edit</a></td>
+			<td><a href="edit_client.php?client=<?php echo $row['id_client']; ?>" class="content_link">Edit</a></td>
 		</tr>
 <?php
 	}
 ?>
 		<tr>
-			<td></td>
-			<td><a href="edit_client.php">Add new person</a></td>
-			<td></td>
+			<td>&nbsp;</td>
+			<td><a href="edit_client.php" class="content_link"><strong>Add new person</strong></a></td>
+			<td>&nbsp;</td>
 		</tr>
 	</table>
 	<input type="hidden" name="org" value="<?php echo $org; ?>">
 	<input type="hidden" name="ref_sel_cli_org" value="<?php echo $HTTP_REFERER ?>">
-	<button name="submit" type="submit" value="submit">Add representative(s)</button>
-	<button name="reset" type="reset" value="reset">Clear selected</button>
+	<button name="submit" type="submit" value="submit" class="simple_form_btn">Add representative(s)</button>
+	<button name="reset" type="reset" value="reset" class="simple_form_btn">Clear selected</button>
 </form>
 <?php
 
