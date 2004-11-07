d809af6fa4f20e09ce612600c2393205e273311e
mlutfy <mlutfy>
2004-11-07 09:20:37 +0000

    small attempt to cleanup html/css

--- lcm_login.php
+++ lcm_login.php
@@ -12,31 +12,34 @@ include_lcm('inc_login');
 
 lcm_html_start(_T('login_title_login'), 'login');
 
-global $spip_lang_right;
+global $lcm_lang_right;
 
+// Site name: mandatory
 $site_name = read_meta('site_name');
 if (! $site_name)
 	$site_name = _T('title_software');
 
-// may be empty
+// Site description: may be empty
 $site_desc = read_meta('site_description');
 
-echo "<p>&nbsp;</p>\n";
-echo "<center><table width='400'><tr><td width='400'>\n";
+echo "\n";
 echo "<div align='center'>\n";
+echo "<div align='center' id='login_screen'>\n\n";
 echo "<h3>" . $site_name;
 
 if ($site_desc)
-	echo "<br/><span style='font-size: 80%; font-weight: normal;'>" . $site_desc .  "</span>";
+	echo "<br /><span style='font-size: 80%; font-weight: normal;'>" . $site_desc .  "</span>";
 
-echo "</h3>\n";
-echo "<div align='" . $spip_lang_right . "'>" . menu_languages() . "</div>\n";
-echo "</div>\n";
+echo "</h3>\n\n";
+echo "<div align='" . $lcm_lang_right . "'>" . menu_languages() . "</div>\n\n";
 
 login('');
 
-echo "</td></tr></table></center>\n";
-echo "</body></html>\n\n";
+echo "\n\n";
+echo "</div>\n";
+echo "</div>\n\n";
+
+echo "</body>\n</html>\n\n";
 
 lcm_html_end();
 
