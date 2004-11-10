67354f1ea66c6bcf3e5adc4b7a8baa0f7fa3239d
mlutfy <mlutfy>
2004-11-10 12:04:01 +0000

    bumped lcm_version up to 0.2.1

--- inc/inc_version.php
+++ inc/inc_version.php
@@ -147,10 +147,10 @@ if (@file_exists('inc/my_options.php'))
 	include('inc/my_options.php');
 
 // Current version of LCM
-$lcm_version = 0.2;
+$lcm_version = 0.21;
 
 // Current version of LCM shown on screen
-$lcm_version_shown = "0.2";
+$lcm_version_shown = "0.2.1";
 
 // Current version of LCM database
 $lcm_db_version = 9;
