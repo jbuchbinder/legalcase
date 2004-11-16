ec4246a351fd64ecfebe925d75e3fe7aaaafc21e
mlutfy <mlutfy>
2004-11-16 08:34:25 +0000

    Added function get_kwg_from_id() + GPL header

--- inc/inc_keywords.php
+++ inc/inc_keywords.php
@@ -1,5 +1,26 @@
 <?php
 
+/*
+	This file is part of the Legal Case Management System (LCM).
+	(C) 2004-2005 Free Software Foundation, Inc.
+
+	This program is free software; you can redistribute it and/or modify it
+	under the terms of the GNU General Public License as published by the 
+	Free Software Foundation; either version 2 of the License, or (at your 
+	option) any later version.
+
+	This program is distributed in the hope that it will be useful, but 
+	WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+	or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+	for more details.
+
+	You should have received a copy of the GNU General Public License along 
+	with this program; if not, write to the Free Software Foundation, Inc.,
+    59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
+
+	$Id: inc_keywords.php,v 1.3 2004/11/16 08:34:25 mlutfy Exp $
+*/
+
 if (defined('_INC_KEYWORDS')) return;
 define('_INC_KEYWORDS', '1');
 
@@ -26,6 +47,19 @@ function get_kwg_all($type) {
 		$ret[$row['name']] = $row;
 	
 	return $ret;
+}
+
+//
+// get_kwg_from_id: Returns the keyword group associated
+// with the provided ID.
+//
+function get_kwg_from_id($group_id) {
+	$query = "SELECT *
+				FROM lcm_keyword_group
+				WHERE id_group = " . intval($group_id);
+	$result = lcm_query($query);
+
+	return lcm_fetch_array($result);
 }
 
 //
