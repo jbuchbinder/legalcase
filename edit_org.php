141c5ad28de3955e3738ab494b584a604b4a8cbc
antzi <antzi>
2004-12-10 10:18:30 +0000

    Fixed $org variable initialisation

--- edit_org.php
+++ edit_org.php
@@ -1,17 +1,39 @@
 <?php
 
+/*
+	This file is part of the Legal Case Management System (LCM).
+	(C) 2004-2005 Free Software Foundation, Inc.
+
+	This program is free software; you can redistribute it and/or modify it
+	under the terms of the GNU General Public License as published by the
+	Free Software Foundation; either version 2 of the License, or (at your
+	option) any later version.
+
+	This program is distributed in the hope that it will be useful, but
+	WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+	or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+	for more details.
+
+	You should have received a copy of the GNU General Public License along
+	with this program; if not, write to the Free Software Foundation, Inc.,
+	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
+
+	$Id: edit_org.php,v 1.14 2004/12/10 10:18:30 antzi Exp $
+*/
+
 include('inc/inc.php');
 include_lcm('inc_filters');
 
-// Create empty org data
-$org_data=array();
+// Initialise variables
+$org = intval($_GET['org']);
 
 // Initiate session
 session_start();
 
 if (empty($errors)) {
     // Clear form data
-    $org_data=array('referer'=>$HTTP_REFERER);
+    $org_data=array();
+	$org_data['referer'] = $HTTP_REFERER;
 
 	if (isset($org)) {
 		// Register org as session variable
