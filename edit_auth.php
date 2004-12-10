8a47e8f1e414697351db45f525328e910d2f2d19
antzi <antzi>
2004-12-10 10:52:04 +0000

    Added GPL and variables initialisation

--- edit_auth.php
+++ edit_auth.php
@@ -1,8 +1,32 @@
 <?php
 
+/*
+	This file is part of the Legal Case Management System (LCM).
+	(C) 2004-2005 Free Software Foundation, Inc.
+
+	This program is free software; you can redistribute it and/or modify it
+	under the terms of the GNU General Public License as published by the
+	Free Software Foundation; either version 2 of the License, or (at your
+	option) any later version.
+
+	This program is distributed in the hope that it will be useful, but
+	WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+	or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+	for more details.
+
+	You should have received a copy of the GNU General Public License along
+	with this program; if not, write to the Free Software Foundation, Inc.,
+	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
+
+	$Id: edit_auth.php,v 1.11 2004/12/10 10:52:04 antzi Exp $
+*/
+
 include('inc/inc.php');
 include_lcm('inc_acc');
 include_lcm('inc_filters');
+
+// Initialise variables
+$case = intval($_GET['case']);
 
 if ($case > 0) {
 	if (allowed($case,'a')) {
