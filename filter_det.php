bcc2171cf280a568c968a691c44143b764671d9c
antzi <antzi>
2004-12-11 11:04:10 +0000

    Added 'remove' link text.

--- filter_det.php
+++ filter_det.php
@@ -18,7 +18,7 @@
 	with this program; if not, write to the Free Software Foundation, Inc.,
     59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
 
-	$Id: filter_det.php,v 1.5 2004/12/11 11:02:07 antzi Exp $
+	$Id: filter_det.php,v 1.6 2004/12/11 11:04:10 antzi Exp $
 */
 
 include('inc/inc.php');
@@ -87,7 +87,7 @@ if ($filter > 0) {
 			// Condition description
 			echo '<td>' . $GLOBALS['condition_types'][$condition['type']] . '</td>';
 			echo '<td>' . $condition['value'] . '</td>';
-			if ($edit) echo "<td><a href='rem_filter_cond.php?filter=$filter&amp;cond=" . $condition['cond_order'] . "' class='content_link'></td>\n";
+			if ($edit) echo "<td><a href='rem_filter_cond.php?filter=$filter&amp;cond=" . $condition['cond_order'] . "' class='content_link'>Remove</a></td>\n";
 			echo "</tr>\n";
 			$last_order = $condition['cond_order']+1;
 		}
