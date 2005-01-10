7552b8eaf30e39f313934d96224ec33a02a97c9c
mlutfy <mlutfy>
2005-01-10 14:46:29 +0000

    added 'newpass', 'is_newpass_allowed', 'newlogin', 'is_newlogin_allowed', .. and made some cleanups which can really break login stuff..

--- inc/inc_auth_db.php
+++ inc/inc_auth_db.php
@@ -1,12 +1,11 @@
 <?php
 
-//
 // Execute this file only once
 if (defined('_INC_AUTH_DB')) return;
 define('_INC_AUTH_DB', '1');
 
 class Auth_db {
-	var $nom, $login, $email, $md5pass, $md5next, $alea_futur, $statut;
+	var $nom, $username, $md5pass, $md5next, $alea_futur, $statut;
 
 	function init() {
 		return true;
@@ -26,9 +25,8 @@ class Auth_db {
 
 		if ($row = lcm_fetch_array($result)) {
 			$this->nom = $row['nom'];
-			$this->login = $row['username'];
-			$this->email = $row['email']; // FIXME
-			$this->statut = $row['status'];
+			$this->username = $row['username'];
+			$this->status = $row['status'];
 			$this->md5pass = $current_mdpass;
 			$this->md5next = $future_mdpass;
 			return true;
@@ -38,16 +36,16 @@ class Auth_db {
 	}
 
 	// Check the non-encrypted password (no javascript support)
-	function verifier($login, $pass) {
+	function validate_pass_cleartext($username, $pass) {
 		// Do not allow empty passwords
 		if ($pass == '') return false;
 
-		$query = "SELECT alea_actuel, alea_futur FROM lcm_author WHERE username='".addslashes($login)."'";
+		$query = "SELECT alea_actuel, alea_futur FROM lcm_author WHERE username='".addslashes($username)."'";
 		$result = lcm_query($query);
 		if ($row = lcm_fetch_array($result)) {
 			$md5pass = md5($row['alea_actuel'] . $pass);
 			$md5next = md5($row['alea_futur'] . $pass);
-			return $this->validate_md5_challenge($login, $md5pass, $md5next);
+			return $this->validate_md5_challenge($username, $md5pass, $md5next);
 		}
 		return false;
 	}
@@ -57,10 +55,10 @@ class Auth_db {
 		return true;
 	}
 
+	// [ML] not used afaik
 	function activate() {
-		// not used afaik
 		if ($this->statut == 'nouveau') { // new author
-			lcm_query("UPDATE lcm_author SET status='normal' WHERE username='".addslashes($this->login)."'");
+			lcm_query("UPDATE lcm_author SET status='normal' WHERE username='".addslashes($this->username)."'");
 		}
 		if ($this->md5next) {
 			include_lcm('inc_session');
@@ -68,7 +66,7 @@ class Auth_db {
 			$nouvel_alea_futur = creer_uniqid();
 			$query = "UPDATE lcm_author SET alea_actuel = alea_futur, ".
 				"password = '".addslashes($this->md5next)."', alea_futur = '$nouvel_alea_futur' ".
-				"WHERE username='".$this->login."'";
+				"WHERE username='".$this->username."'";
 			@spip_query($query);
 		}
 	}
@@ -76,6 +74,34 @@ class Auth_db {
 	function activer() {
 		lcm_log("use of deprecated function: activer, use activate instead");
 		return $this->activate();
+	}
+
+	function is_newpass_allowed($username, $author_session) {
+		if ($author_session['username'] == $username)
+			return true;
+		else if ($author_session['status'] == 'admin')
+			return true;
+		else
+			return false;
+	}
+
+	function newpass($username, $pass, $author_session) {
+		if ($this->is_newpass_allowed() == false)
+			return false;
+	
+		$query = "UPDATE lcm_author
+					SET password = '" . md5($pass) . "'
+					WHERE username = '" . $username . "'";
+
+		lcm_query($query);
+		return true;
+	}
+
+	function is_newlogin_allowed($username, $author_session) {
+		if ($author_session['status'] == 'admin')
+			return true;
+		else
+			return false;
 	}
 }
 
