82f1c20b27b2794fef1d08a5d17caea488693e47
mlutfy <mlutfy>
2005-01-13 12:27:09 +0000

    allowed(): gave admins the right to do everything

--- inc/inc_acc.php
+++ inc/inc_acc.php
@@ -1,34 +1,55 @@
 <?php
 
-//
+/*
+	This file is part of the Legal Case Management System (LCM).
+	(C) 2004-2005 Free Software Foundation, Inc.
+
+	This program is free software; you can redistribute it and/or modify it
+	under the terms of the GNU General Public License as published by the
+	Free Software Foundation; either version 2 of the License, or (at your
+	option) any later version.
+
+	This program is distributed in the hope that it will be useful, but
+	WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+	or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+	for more details.
+
+	You should have received a copy of the GNU General Public License along
+	with this program; if not, write to the Free Software Foundation, Inc.,
+	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
+
+	$Id: inc_acc.php,v 1.5 2005/01/13 12:27:09 mlutfy Exp $
+*/
+
 // Execute this file only once
 if (defined('_INC_ACC')) return;
 define('_INC_ACC', '1');
 
-function allowed($case,$access) {
+function allowed($case, $access) {
 	// By default, do not allow access
 	$allow = false;
 
-	// Check if the case number is present
-	if ($case>0) {
+	// Admins can access everything
+	if ($GLOBALS['author_session']['status'] == 'admin')
+		return true;
 
-		// Prepare query
+	// Check if the case number is present
+	if ($case > 0) {
+
 		$q = "SELECT *
 				FROM lcm_case_author
-				WHERE (id_case=$case
-					AND id_author=" . $GLOBALS['author_session']['id_author'] . ")";
+				WHERE (id_case = " . intval($case) . "
+					AND id_author= " . $GLOBALS['author_session']['id_author'] . ")";
 
-		// Do the query
 		$result = lcm_query($q);
 
-		// Process the result, if any
 		if ($row = lcm_fetch_array($result)) {
 
 			// Set initial value to true, if $access parameter is set
 			$allow = (bool) $access;
 
 			// Walk each character in the required access rights list
-			for($i=0 ; $i<strlen($access) ; $i++) {
+			for($i = 0; $i < strlen($access); $i++) {
 				switch ($access{$i}) {
 					case "r":
 						$allow &= ($row['ac_read']);
@@ -49,6 +70,7 @@ function allowed($case,$access) {
 			}
 		}
 	}
+
 	return $allow;
 }
 
