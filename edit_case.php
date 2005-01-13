869f9cd8ca2e33a509a13e9cc144832c9a20d0b9
mlutfy <mlutfy>
2005-01-13 13:26:38 +0000

    changed some translation strings + cleaning

--- edit_case.php
+++ edit_case.php
@@ -18,7 +18,7 @@
 	with this program; if not, write to the Free Software Foundation, Inc.,
 	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
 
-	$Id: edit_case.php,v 1.43 2005/01/13 12:37:30 mlutfy Exp $
+	$Id: edit_case.php,v 1.44 2005/01/13 13:26:38 mlutfy Exp $
 */
 
 session_start();
@@ -50,6 +50,7 @@ if (empty($errors)) {
 	$existing = ($case > 0);
 
 	// Set author ID by session data
+	// [ML] XXX FIXME: absurd! this is written in the database, there may be many authors
 	$case_data['id_author'] = $GLOBALS['author_session']['id_author'];
 
 	if ($existing) {
@@ -82,41 +83,41 @@ if (empty($errors)) {
 	}
 }
 
-// Start the page with the proper title
-if ($existing) lcm_page_start(_T('edit_case_details'));
-else lcm_page_start(_T('new_case'));
+// Start page and title
+if ($existing) lcm_page_start(_T('title_case_edit'));
+else lcm_page_start(_T('title_case_new'));
 
-	echo "\n<form action=\"upd_case.php\" method=\"POST\">
-	<table class=\"tbl_usr_dtl\">
-		<input type=\"hidden\" name=\"id_author\" value=\"" . $case_data['id_author'] . "\">
-			<!-- caption>" . _T('case_details') . "</caption -->
-			<!-- tr><th>" . _T('parameter') . "</th><th>" . _T('value') .  "</th></tr -->\n";
-	if ($case_data['id_case']) {
-		echo "\t<tr><td>" . _T('case_id') . ":</td><td>" . $case_data['id_case']
-			. "<input type=\"hidden\" name=\"id_case\" value=\"" .  $case_data['id_case'] . "\"></td></tr>\n";
-	}
+// Start edit case form
+echo "\n<form action=\"upd_case.php\" method=\"post\">
+<table class=\"tbl_usr_dtl\">
+<input type=\"hidden\" name=\"id_author\" value=\"" . $case_data['id_author'] . "\">\n";
+
+if ($case_data['id_case']) {
+	echo "\t<tr><td>" . _T('case_input_id') . "</td><td>" . $case_data['id_case']
+		. "<input type=\"hidden\" name=\"id_case\" value=\"" .  $case_data['id_case'] . "\"></td></tr>\n";
+}
 
 //	echo "
-//		<tr><td>" . _T('author_id') . ":</td><td>" . $case_data['id_author'] . "
+//		<tr><td>" . _T('author_id') . "</td><td>" . $case_data['id_author'] . "
 //			<input type=\"hidden\" name=\"id_author\" value=\"" . $case_data['id_author'] . "\"></td></tr>";
 	echo "
-		<tr><td>" . _T('case_title') . ":</td>
+		<tr><td>" . _T('case_input_title') . "</td>
 			<td><input name=\"title\" value=\"" . clean_output($case_data['title']) . "\" class=\"search_form_txt\">";
 	echo f_err('title',$errors) . "</td></tr>
-		<tr><td>" . _T('court_archive_id') . ":</td>
+		<tr><td>" . _T('case_input_court_archive') . "</td>
 			<td><input name=\"id_court_archive\" value=\"" . clean_output($case_data['id_court_archive']) . "\" class=\"search_form_txt\"></td></tr>";
 // [AG] Creation date not shown upon ML request
-//		<tr><td>" . _T('creation_date') . ":</td>
+//		<tr><td>" . _T('case_input_date_creation') . "</td>
 //			<td>" . $case_data['date_creation'] . "</td></tr>
 // [AG] Assignment date is set only when adding user to the case
-//		<tr><td>" . _T('assignment_date') . ":</td>
+//		<tr><td>" . _T('case_input_date_assignment') . "</td>
 //			<td><input name=\"date_assignment\" value=\"" . clean_output($case_data['date_assignment']) . "\" class=\"search_form_txt\"></td></tr>
 	echo "
-		<tr><td>" . _T('legal_reason') . ":</td>
+		<tr><td>" . _T('case_input_legal_reason') . "</td>
 			<td><input name=\"legal_reason\" value=\"" . clean_output($case_data['legal_reason']) . "\" class=\"search_form_txt\"></td></tr>
-		<tr><td>" . _T('alledged_crime') . ":</td>
+		<tr><td>" . _T('case_input_alledged_crime') . "</td>
 			<td><input name=\"alledged_crime\" value=\"" .  clean_output($case_data['alledged_crime']) . "\" class=\"search_form_txt\"></td></tr>
-		<tr><td>" . _T('case_status') . ":</td>
+		<tr><td>" . _T('case_input_status') . "</td>
 			<td><input name=\"status\" value=\"" . clean_output($case_data['status']) . "\" class=\"search_form_txt\"></td></tr>
 	";
 
@@ -155,20 +156,22 @@ else lcm_page_start(_T('new_case'));
 	// Different buttons for edit existing and for new case
 	// TODO: add expert mode
 	if ($existing) {
-		echo '	<button name="submit" type="submit" value="submit" class="simple_form_btn">' . _T('button_validate') . "</button>\n";
+		echo '<button name="submit" type="submit" value="submit" class="simple_form_btn">' . _T('button_validate') . "</button>\n";
 	} else {
-		echo '	<button name="submit" type="submit" value="add" class="simple_form_btn">' . _T('button_validate') . '</button>
+		echo '<button name="submit" type="submit" value="add" class="simple_form_btn">' . _T('button_validate') . '</button>
 	<button name="submit" type="submit" value="addnew" class="simple_form_btn">' . _T('add_and_open_new') . '</button>
 	<button name="submit" type="submit" value="adddet" class="simple_form_btn">' . _T('add_and_go_to_details') . "</button>\n";
 	}
 
-	if ($existing)
-		echo '	<button name="reset" type="reset" class="simple_form_btn">' . _T('button_reset') . "</button>\n";
+	// [ML] if ($existing)
+	//	echo '<button name="reset" type="reset" class="simple_form_btn">' . _T('button_reset') . "</button>\n";
 
-	echo '	<input type="hidden" name="ref_edit_case" value="' . $case_data['ref_edit_case'];
+	echo '<input type="hidden" name="ref_edit_case" value="' . $case_data['ref_edit_case'];
 	echo '">
 </form>
 
 ';
+
 	lcm_page_end();
+	session_destroy();
 ?>
