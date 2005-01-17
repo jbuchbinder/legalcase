cca812ef3aaf68fee39aead6c75a667e6525347c
mlutfy <mlutfy>
2005-01-17 13:04:13 +0000

    replaced SQL by get_contacts() + show e-emails only if allowed by policy + table shown only if there are contacts to show

--- author_det.php
+++ author_det.php
@@ -18,52 +18,57 @@
 	with this program; if not, write to the Free Software Foundation, Inc.,
 	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
 
-	$Id: author_det.php,v 1.3 2004/12/21 18:16:03 makaveev Exp $
+	$Id: author_det.php,v 1.4 2005/01/17 13:04:13 mlutfy Exp $
 */
 
 include('inc/inc.php');
-include_lcm('inc_lang');
+include_lcm('inc_contacts');
 
 // Initialise variables
-$author = intval($_GET['author']);
+$author = intval($_REQUEST['author']);
 
-if ($author>0) {
+if ($author > 0) {
 	// Get author data
 	$q = "SELECT *
 			FROM lcm_author
-			WHERE id_author=$author";
+			WHERE id_author = $author";
 	$result = lcm_query($q);
+
 	if ($author_data = lcm_fetch_array($result)) {
 		// Start the page
 		$fullname = $author_data['name_first'];
 		$fullname .= ($author_data['name_middle'] ? ' ' . $author_data['name_middle'] : '');
 		$fullname .= ($author_data['name_last'] ? ' ' . $author_data['name_last'] : '');
+
 		lcm_page_start("Author details: $fullname");
 
-		if (($GLOBALS['author_session']['status'] == 'admin') ||
-			($author == $GLOBALS['author_session']['id_author']))
-				echo '<p class="normal_text"><a href="edit_author.php?author=' . $author . "\" class=\"edit_lnk\">Edit author data</a></p>\n";
+		//
+		// Show author contacts (if any)
+		//
+		$hide_emails = read_meta('hide_emails');
+		$contacts = get_contacts('author', $author);
 
-		// Show author contacts
-		$q = "SELECT *
-				FROM lcm_contact,lcm_keyword
-				WHERE ((lcm_contact.id_of_person=$author)
-					AND (lcm_contact.type_person='author')
-					AND (lcm_contact.type_contact=lcm_keyword.id_keyword))";
-		$result = lcm_query($q);
+		$html = '';
+		$html .= '<table border="0" align="center" class="tbl_usr_dtl" width="99%">' . "\n";
+		$html .= '<tr><th class="heading" colspan="2">' . "Contacts:" . '</th></tr>' . "\n";
 
-		// Show results in table
-		echo "<table border='0' align='center' class='tbl_usr_dtl' width='99%'><tr><th class='heading' colspan='2'>Contacts:</th></tr>\n";
 		$i = 0;
-		while ($row = lcm_fetch_array($result)) {
-			echo "\t<tr>";
-			echo "<td class='tbl_cont_" . ($i % 2 ? "dark" : "light") . "'>" . _T($row['title']) . "</td>";
-			echo "<td class='tbl_cont_" . ($i % 2 ? "dark" : "light") . "'>" . $row['value'] . "</td>";
-			echo "</tr>\n";
-			$i++;
+		foreach($contacts as $c) {
+			if (! ($hide_emails == 'yes' && $c['name'] == 'email_main' && $author_session['status'] != 'admin')) {
+				$html .= "\t<tr>";
+				$html .= "<td class='tbl_cont_" . ($i % 2 ? "dark" : "light") . "'>" . _T($c['title']) . "</td>";
+				$html .= "<td class='tbl_cont_" . ($i % 2 ? "dark" : "light") . "'>" . $c['value'] . "</td>";
+				$html .= "</tr>\n";
+				$i++;
+			}
 		}
-		echo "</table><br />\n";
 
+		$html .= "</table><br />\n";
+
+		if ($i > 0)
+			echo $html;
+
+		/* [ML] 2005-01-17: Redundant with edit-author
 		if (($GLOBALS['author_session']['status'] == 'admin') ||
 			($author == $GLOBALS['author_session']['id_author'])) {
 			// Show "add contact" form
@@ -80,12 +85,18 @@ if ($author>0) {
 				echo "\t\t<option>" . _T($row['title']) . "</option>\n";
 			}
 			echo "\t</select>\n";
-			echo "\t<input type='text' size='40' style='style: 99%' name='value' class='search_form_txt' />\n";
+			echo "\t<input type='text' size='40' style='style: 99%' name='value' class='search_form_txt' />";
 			echo "\t<input name='submit' type='submit' class='search_form_btn' id='submit' value='Add contact' />\n";
 			echo "</form>\n";
-		}
+		} */
 
-		// End the page
+		//
+		// Show 'edit author' button, if allowed
+		//
+		if (($GLOBALS['author_session']['status'] == 'admin') ||
+			($author == $GLOBALS['author_session']['id_author']))
+				echo '<p class="normal_text"><a href="edit_author.php?author=' . $author . "\" class=\"edit_lnk\">Edit author data</a></p>\n";
+
 		lcm_page_end();
 	} else {
 		die("There's no such author!");
