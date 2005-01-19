dcede4741384a3cc264899376611ab4e1c5a9252
antzi <antzi>
2005-01-19 00:25:07 +0000

    Removed local session usage

--- upd_case.php
+++ upd_case.php
@@ -18,7 +18,7 @@
 	with this program; if not, write to the Free Software Foundation, Inc.,
     59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
 
-	$Id: upd_case.php,v 1.26 2004/12/17 11:27:52 antzi Exp $
+	$Id: upd_case.php,v 1.27 2005/01/19 00:25:07 antzi Exp $
 */
 
 include('inc/inc.php');
@@ -26,51 +26,51 @@ include_lcm('inc_acc');
 include_lcm('inc_filters');
 
 // Start session
-session_start();
+//session_start();
 
 // Register $errors array - just in case
-if (!session_is_registered("errors"))
-    session_register("errors");
+//if (!session_is_registered("errors"))
+//    session_register("errors");
 
 // Clear all previous errors
-$errors=array();
+$_SESSION['errors']=array();
 
 // Register form data in the session
-if(!session_is_registered("case_data"))
-    session_register("case_data");
+//if(!session_is_registered("case_data"))
+//    session_register("case_data");
 
 // Get form data from POST fields
 foreach($_POST as $key => $value)
-    $case_data[$key]=$value;
+    $_SESSION['case_data'][$key]=$value;
 
 // Check case data for validity
-if (!$case_data['title']) $errors['title'] = _T('error_no_case_name');
+if (!$_SESSION['case_data']['title']) $_SESSION['errors']['title'] = _T('error_no_case_name');
 
-if (count($errors)) {
+if (count($_SESSION['errors'])) {
     header("Location: $HTTP_REFERER");
     exit;
 } else {
 	//$cl = '(id_case,title,id_court_archive,date_creation,date_assignment,legal_reason,alledged_crime,status)';
 	//$vl = "($id_case,'$title','$id_court_archive','$date_creation',";
 	//$vl .= "'$date_assignment','$legal_reason','$alledged_crime','$status')";
-	$fl = "title='" . clean_input($case_data['title']) . "',
-			id_court_archive='" . clean_input($case_data['id_court_archive']) . "',";
-//			date_creation='" . $case_data['date_creation'] . "',
+	$fl = "title='" . clean_input($_SESSION['case_data']['title']) . "',
+			id_court_archive='" . clean_input($_SESSION['case_data']['id_court_archive']) . "',";
+//			date_creation='" . $_SESSION['case_data']['date_creation'] . "',
 // [AG] Creation date derived from MySQL server to prevent user manipulation
-//			date_assignment='" . clean_input($case_data['date_assignment']) . "',
+//			date_assignment='" . clean_input($_SESSION['case_data']['date_assignment']) . "',
 // [AG] Assignment date is set only when creating case or adding new user to it
 	$fl .= "
-			legal_reason='" . clean_input($case_data['legal_reason']) . "',
-			alledged_crime='" . clean_input($case_data['alledged_crime']) . "'";
+			legal_reason='" . clean_input($_SESSION['case_data']['legal_reason']) . "',
+			alledged_crime='" . clean_input($_SESSION['case_data']['alledged_crime']) . "'";
 
 // Put public access rights settings in a separate string
 	$public_access_rights = '';
-	if ($case_data['public'] || read_meta('case_read_always'))
+	if ($_SESSION['case_data']['public'] || read_meta('case_read_always'))
 		$public_access_rights .= "public=1";
 	else
 		$public_access_rights .= "public=0";
 
-	if ($case_data['pub_write'] || read_meta('case_write_always'))
+	if ($_SESSION['case_data']['pub_write'] || read_meta('case_write_always'))
 		$public_access_rights .= ", pub_write=1";
 	else
 		$public_access_rights .= ", pub_write=0";
@@ -82,7 +82,7 @@ if (count($errors)) {
 		if (!allowed($id_case,'e')) die("You don't have permission to change this case's information!");
 
 		// If modifying existing case, add status change to the list of fields
-		$fl .= ",status='" . clean_input($case_data['status']) . "'";
+		$fl .= ",status='" . clean_input($_SESSION['case_data']['status']) . "'";
 
 		// If admin access is allowed, set all fields
 		if (allowed($id_case,'a'))
@@ -136,7 +136,7 @@ if (count($errors)) {
 	$result = lcm_query($q);
 
     // Clear the session
-    session_destroy();
+//    session_destroy();
 
 	// [ML] I don't understand why: header("Location: $ref_edit_case");
 	// [AG] Because "edit_case" could be invoked from diferent places i.e. edit existing case or add new or other.
@@ -148,13 +148,13 @@ if (count($errors)) {
 	// Proceed accoring to the button type
 	switch ($submit) {
 		case 'addnew':
-			header("Location: edit_case.php?case=0&ref=" . $case_data['ref_edit_case']);
+			header("Location: edit_case.php?case=0&ref=" . $_SESSION['case_data']['ref_edit_case']);
 			break;
 		case 'adddet':
 			header("Location: case_det.php?case=$id_case");
 			break;
 		default :
-			header("Location: " . $case_data['ref_edit_case']);
+			header("Location: " . $_SESSION['case_data']['ref_edit_case']);
 	}
 }
 ?>
