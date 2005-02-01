a334fe9379277d3f0901464310b9404df2744a63
mlutfy <mlutfy>
2005-02-01 13:19:17 +0000

    added Case-ID, removed 'edit case', + minor cleanup

--- listcases.php
+++ listcases.php
@@ -18,17 +18,17 @@
 	with this program; if not, write to the Free Software Foundation, Inc.,
     59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
 
-	$Id: listcases.php,v 1.36 2004/12/16 15:12:13 makaveev Exp $
+	$Id: listcases.php,v 1.37 2005/02/01 13:19:17 mlutfy Exp $
 */
 
 include('inc/inc.php');
 include_lcm('inc_acc');
 include_lcm('inc_filters');
 
-$q = "SELECT lcm_case.id_case,title,status,public,pub_write
-		FROM lcm_case,lcm_case_author
-		WHERE (lcm_case.id_case=lcm_case_author.id_case
-			AND lcm_case_author.id_author=" . $GLOBALS['author_session']['id_author'];
+$q = "SELECT c.id_case, title, status, public, pub_write
+		FROM lcm_case as c, lcm_case_author as a
+		WHERE (c.id_case = a.id_case
+			AND a.id_author = " . $GLOBALS['author_session']['id_author'];
 
 // Add search criteria if any
 $find_case_string = '';
@@ -37,10 +37,10 @@ if (isset($_REQUEST['find_case_string']))
 	$find_case_string = $_REQUEST['find_case_string'];
 	
 if (strlen($find_case_string)>1) {
-	$q .= " AND (lcm_case.title LIKE '%$find_case_string%')";
+	$q .= " AND (c.title LIKE '%$find_case_string%')";
 	lcm_page_start("Cases, containing '$find_case_string':");
 } else {
-	lcm_page_start("List of cases");
+	lcm_page_start(_T('title_my_cases'));
 }
 
 $q .= ")";
@@ -70,24 +70,44 @@ echo "<!-- Page rows:" . $prefs['page_rows'] . "-->\n";
 
 <!-- [ML:FIXME] I'm not sure about the CSS classes -->
 <table border='0' align='center' class='tbl_usr_dtl' width='99%'>
-	<tr><th class='heading'>Description</th>
-		<th class='heading'>Status</th>
-		<th colspan="2" class='heading'>Actions</th>
+	<tr>
+		<th class="heading">#</th>
+		<th class="heading">Title</th>
+		<th colspan="3" class="heading">Status</th>
 	</tr>
 <?php
 // Process the output of the query
 for ($i = 0 ; (($i<$prefs['page_rows']) && ($row = lcm_fetch_array($result))) ; $i++) {
-	// Show case title
-	echo "<tr><td class='tbl_cont_" . ($i % 2 ? "dark" : "light") . "'>";
+	$ac_read = allowed($row['id_case'],'r');
+	$ac_edit = allowed($row['id_case'], 'e');
 
-	if (allowed($row['id_case'],'r')) echo '<a href="case_det.php?case=' . $row['id_case'] . '" class="content_link">';
+	echo "<tr>";
+
+	// Case ID
+	echo "<td class='tbl_cont_" . ($i % 2 ? "dark" : "light") . "'>";
+	if ($ac_read) echo '<a href="case_det.php?case=' . $row['id_case'] . '" class="content_link">';
+	echo $row['id_case'];
+	if ($ac_read) echo '</a>';
+	echo "</td>\n";
+
+	// Title
+	echo "<td class='tbl_cont_" . ($i % 2 ? "dark" : "light") . "'>";
+	if ($ac_read) echo '<a href="case_det.php?case=' . $row['id_case'] . '" class="content_link">';
 	echo highlight_matches(clean_output($row['title']),$find_case_string);
 	if (allowed($row['id_case'],'r')) echo '</a>';
-	echo "</td>\n<td class='tbl_cont_" . ($i % 2 ? "dark" : "light") . "'>" . $row['status'];
-	echo "</td>\n<td class='tbl_cont_" . ($i % 2 ? "dark" : "light") . "'>";
+	echo "</td>\n";
+	
+	// Status
+	echo "<td class='tbl_cont_" . ($i % 2 ? "dark" : "light") . "'>" . $row['status'] . "</td>\n";
+	
+	/*
+	echo "<td class='tbl_cont_" . ($i % 2 ? "dark" : "light") . "'>";
 	if (allowed($row['id_case'],'e'))
 		echo '<a href="edit_case.php?case=' . $row['id_case'] . '" class="content_link">Edit case</a>';
-	echo "</td>\n<td class='tbl_cont_" . ($i % 2 ? "dark" : "light") . "'>";
+	echo "</td>\n";
+	*/
+
+	echo "<td class='tbl_cont_" . ($i % 2 ? "dark" : "light") . "'>";
 	if (allowed($row['id_case'],'w'))
 		echo '<a href="edit_fu.php?case=' . $row['id_case'] . '" class="content_link">Add followup</a>';
 	echo "</td></tr>\n";
