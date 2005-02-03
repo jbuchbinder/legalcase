efbccd98ae214c89ea9eac5661cc84adef2c51e1
mlutfy <mlutfy>
2005-02-03 11:44:35 +0000

    cleanup + added description field

--- edit_rep.php
+++ edit_rep.php
@@ -16,19 +16,16 @@
 
 	You should have received a copy of the GNU General Public License along
 	with this program; if not, write to the Free Software Foundation, Inc.,
-    59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
+	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
 
-	$Id: edit_rep.php,v 1.6 2005/01/25 22:27:45 antzi Exp $
+	$Id: edit_rep.php,v 1.7 2005/02/03 11:44:35 mlutfy Exp $
 */
 
 include('inc/inc.php');
 include_lcm('inc_acc');
 include_lcm('inc_filters');
 
-// Initiate session
-// [ML] now in inc-auth session_start();
-
-if (empty($errors)) {
+if (empty($_SESSION['errors'])) {
 
     // Clear form data
     $rep_data = array();
@@ -70,9 +67,6 @@ if (empty($errors)) {
 		// Set default values for the new report
 		$rep_data['id_author'] = $GLOBALS['author_session']['id_author'];
 
-		// [ML] Not used, and should be format_date($date, 'short')
-		// $rep_data['date_creation'] = date(_T('date_format')); // was: date('Y-m-d H:i:s');
-
 		//$rep_data['public'] = read_meta('case_default_read');
 		//$rep_data['pub_write'] = read_meta('case_default_write');
 
@@ -82,22 +76,33 @@ if (empty($errors)) {
 }
 
 // Start the page with the proper title
-if ($existing) lcm_page_start(_T('edit_rep_details'));
-else lcm_page_start(_T('new_report'));
+if ($existing) 
+	lcm_page_start(_T('edit_rep_details'));
+else 
+	lcm_page_start(_T('new_report'));
 
-	echo "\n<form action='upd_rep.php' method='POST'>
-		<table class='tbl_usr_dtl'>\n";
-	if ($rep_data['id_report']) {
-		echo "\t<tr><td>" . _T('report_id') . ":</td><td>" . $rep_data['id_report'] . "
-			<input type=\"hidden\" name=\"id_report\" value=\"" .  $rep_data['id_report'] . "\"></td></tr>\n";
-	}
+echo "\n<form action='upd_rep.php' method='post'>
+	<table class='tbl_usr_dtl' width='99%'>\n";
 
-	echo "
-		<tr><td>" . _T('author_id') . ":</td><td>" . $rep_data['id_author'] . "
-			<input type=\"hidden\" name=\"id_author\" value=\"" . $rep_data['id_author'] . "\"></td></tr>
-		<tr><td>" . _T('rep_title') . ":</td>
-			<td><input name=\"title\" value=\"" . clean_output($rep_data['title']) . "\" class=\"search_form_txt\">";
-	echo f_err('title',$errors) . "</td></tr>";
+if ($rep_data['id_report']) {
+	echo "\t<tr><td>" . _T('report_id') . ":</td><td>" . $rep_data['id_report'] . "
+		<input type=\"hidden\" name=\"id_report\" value=\"" .  $rep_data['id_report'] . "\"></td></tr>\n";
+}
+
+echo "
+	<tr><td>" . _T('author_id') . ":</td><td>" . $rep_data['id_author'] . "
+		<input type=\"hidden\" name=\"id_author\" value=\"" . $rep_data['id_author'] . "\"></td></tr>
+	<tr><td>" . f_err_star('title', $_SESSION['errors']) . _T('rep_title') . ":</td>
+		<td><input name=\"title\" value=\"" . clean_output($rep_data['title']) . "\" class=\"search_form_txt\"></td>
+	</tr>\n";
+
+echo "<tr>\n";
+
+// Description
+echo '<td colspan="2">' . "<p>Description:</p>\n";
+echo '<textarea name="description" rows="5" cols="40" class="frm_tarea">';
+echo $rep_data['description'];
+echo "</textarea>\n";
 
 //	if ($admin || !read_meta('case_read_always') || !read_meta('case_write_always')) {
 //		echo "\t<tr><td>" . _T('public') . "</td>
@@ -129,29 +134,21 @@ else lcm_page_start(_T('new_report'));
 //<?php
 //	}
 
-	echo "</table>\n";
+echo "</table>\n";
 
-	// Different buttons for edit existing and for new case
-	// TODO: add normal/export mode
-	if ($existing) {
-		echo '	<button name="submit" type="submit" value="submit" class="simple_form_btn">' . _T('button_validate') . "</button>\n";
-	} else {
-		// More buttons for 'extended' mode
-		if ($prefs['mode'] == 'extended') {
-			echo '<button name="submit" type="submit" value="add" class="simple_form_btn">' . _T('button_validate') . "</button>\n";
-			echo '<button name="submit" type="submit" value="addnew" class="simple_form_btn">' . _T('add_and_open_new') . "</button>\n";
-			echo '<button name="submit" type="submit" value="adddet" class="simple_form_btn">' . _T('add_and_go_to_details') . "</button>\n"; }
-		else	// Less buttons in simple mode
-			echo '<button name="submit" type="submit" value="adddet" class="simple_form_btn">' . _T('button_validate') . "</button>\n";
-	}
+// Validation buttons
+echo '	<button name="submit" type="submit" value="submit" class="simple_form_btn">' . _T('button_validate') . "</button>\n";
 
-//	if ($existing)
-		echo '	<button name="reset" type="reset" class="simple_form_btn">' . _T('button_reset') . "</button>\n";
+// More buttons for 'extended' mode
+if ($prefs['mode'] == 'extended') {
+	echo '<button name="submit" type="submit" value="addnew" class="simple_form_btn">' . _T('add_and_open_new') . "</button>\n";
+	echo '<button name="submit" type="submit" value="adddet" class="simple_form_btn">' . _T('add_and_go_to_details') . "</button>\n";
+	echo '	<button name="reset" type="reset" class="simple_form_btn">' . _T('button_reset') . "</button>\n";
+}
 
-	echo '	<input type="hidden" name="ref_edit_rep" value="' . $rep_data['ref_edit_rep'];
-	echo '">
-</form>
+echo '<input type="hidden" name="ref_edit_rep" value="' . $rep_data['ref_edit_rep'] . '">' . "\n";
+echo '</form>' . "\n";
 
-';
-	lcm_page_end();
+lcm_page_end();
+
 ?>
