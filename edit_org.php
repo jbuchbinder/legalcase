47a7c5a0c941b046deb44e66e683d9bdb834d584
makaveev <makaveev>
2005-02-04 09:12:21 +0000

    removed tables from form, basic formating added instead

--- edit_org.php
+++ edit_org.php
@@ -18,7 +18,7 @@
 	with this program; if not, write to the Free Software Foundation, Inc.,
 	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
 
-	$Id: edit_org.php,v 1.17 2005/01/21 16:41:54 mlutfy Exp $
+	$Id: edit_org.php,v 1.18 2005/02/04 09:12:21 makaveev Exp $
 */
 
 include('inc/inc.php');
@@ -68,32 +68,30 @@ else
 ?>
 
 <form action="upd_org.php" method="POST">
-	<table class="tbl_usr_dtl" width="99%">
-		<!--tr><td>Organisation ID:</td>
-			<td><?php echo $org_data['id_org']; ?> -->
-			<input type="hidden" name="id_org" value="<?php echo $org_data['id_org']; ?>"><!--/td></tr-->
-		<tr><td><?php echo _T('org_input_name'); ?></td>
-			<td><input name="name" value="<?php echo clean_output($org_data['name']); ?>" class="search_form_txt"></td></tr>
-		<!--
-		<tr><td>Created on:</td>
-			<td><input name="date_creation" value="<?php echo clean_output($org_data['date_creation']); ?>" class="search_form_txt">
-			<?php echo f_err('date_creation',$errors); ?></td></tr>
-		<tr><td>Updated on:</td>
-			<td><input name="date_update" value="<?php echo clean_output($org_data['date_update']); ?>" class="search_form_txt">
-			<?php echo f_err('date_update',$errors); ?></td></tr>
-		-->
-		<tr><td>Address:</td>
-			<td><textarea name="address" cols="50" rows="3" class="frm_tarea"><?php
-			echo clean_output($org_data['address']); ?></textarea></td></tr>
-	</table>
-
+<fieldset class="info_box">
+	<!-- strong>Organisation ID:</strong><br />
+	<?php echo $org_data['id_org']; ?>
+	<input type="hidden" name="id_org" value="<?php echo $org_data['id_org']; ?>"><br /><br / -->
+	
+	<strong><?php echo _T('org_input_name'); ?></strong><br />
+	<input name="name" value="<?php echo clean_output($org_data['name']); ?>" class="search_form_txt"><br /><br />
+	
+	<!-- strong>Created on:</strong><br />
+	<input name="date_creation" value="<?php echo clean_output($org_data['date_creation']); ?>" class="search_form_txt">
+	<?php echo f_err('date_creation',$errors); ?><br /><br / -->
+	
+	<!-- strong>Updated on:</strong><br />
+	<input name="date_update" value="<?php echo clean_output($org_data['date_update']); ?>" class="search_form_txt">
+	<?php echo f_err('date_update',$errors); ?><br /><br / -->
+	<strong>Address:</strong><br />
+	<textarea name="address" cols="50" rows="3" class="frm_tarea"><?php echo clean_output($org_data['address']); ?></textarea><br /><br />
 <?php
 	echo '<input type="hidden" name="ref_edit_org" value="' . $GLOBALS['HTTP_REFERER'] . '">' . "\n";
 	echo '<button name="submit" type="submit" value="submit" class="simple_form_btn">' . _T('button_validate') . '</button>' . "\n";
 	if ($org && $prefs['mode'] == 'extended')
 		echo '<button name="reset" type="reset" class="simple_form_btn">' . _T('button_reset') . '</button>' . "\n";
 ?>
-
+</fieldset>
 </form>
 
 <?php
