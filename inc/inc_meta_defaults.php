373a04688e2e358cef5c93a1274881c4cee20ff5
mlutfy <mlutfy>
2005-02-07 08:16:20 +0000

    esthetic corrections

--- inc/inc_meta_defaults.php
+++ inc/inc_meta_defaults.php
@@ -1,5 +1,26 @@
 <?php
 
+/*
+	This file is part of the Legal Case Management System (LCM).
+	(C) 2004-2005 Free Software Foundation, Inc.
+
+	This program is free software; you can redistribute it and/or modify it
+	under the terms of the GNU General Public License as published by the
+	Free Software Foundation; either version 2 of the License, or (at your
+	option) any later version.
+
+	This program is distributed in the hope that it will be useful, but
+	WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+	or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+	for more details.
+
+	You should have received a copy of the GNU General Public License along
+	with this program; if not, write to the Free Software Foundation, Inc.,
+	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
+
+	$Id: inc_meta_defaults.php,v 1.9 2005/02/07 08:16:20 mlutfy Exp $
+*/
+
 // Execute this file only once
 if (defined('_INC_DEFAULTS')) return;
 define('_INC_DEFAULTS', '1');
@@ -17,7 +38,7 @@ function init_default_config() {
 		$lang = 'en';
 
 	// c.f. http://www.lcm.ngo-bg.org/article28.html
-	$liste_meta = array(
+	$list_meta = array(
 		// Default language of the site 
 		// (users can also personalise it individually).
 		'default_language' => $lang,
@@ -63,9 +84,9 @@ function init_default_config() {
 		'charset' => 'UTF-8'
 	);
 
-	while (list($nom, $valeur) = each($liste_meta)) {
-		if (!read_meta($nom)) {
-			write_meta($nom, $valeur);
+	while (list($key, $value) = each($list_meta)) {
+		if (!read_meta($key)) {
+			write_meta($key, $value);
 			$modifs = true;
 		}
 	}
