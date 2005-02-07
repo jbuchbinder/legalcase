a85c47a90a37609b53571ba380d42e1858b513f0
mlutfy <mlutfy>
2005-02-07 12:47:46 +0000

    added 'description' field + cleanup

--- upd_rep.php
+++ upd_rep.php
@@ -16,24 +16,17 @@
 
 	You should have received a copy of the GNU General Public License along
 	with this program; if not, write to the Free Software Foundation, Inc.,
-    59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
+	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
 
-	$Id: upd_rep.php,v 1.4 2005/01/25 22:29:57 antzi Exp $
+	$Id: upd_rep.php,v 1.5 2005/02/07 12:47:46 mlutfy Exp $
 */
 
 include('inc/inc.php');
 include_lcm('inc_acc');
 include_lcm('inc_filters');
 
-// Start session
-// [ML] inc_auth session_start();
-
-// Register $errors array - just in case
-if (!session_is_registered("errors"))
-    session_register("errors");
-
 // Clear all previous errors
-$errors=array();
+$_SESSION['errors'] = array();
 
 // Register form data in the session
 if(!session_is_registered("rep_data"))
@@ -48,13 +41,22 @@ $rep_data['id_report'] = intval($rep_data['id_report']);
 $rep_data['id_author'] = intval($rep_data['id_author']);
 
 // Check report data for validity
-if (!$rep_data['title']) $errors['title'] = _T('error_no_rep_name');
+if (!$rep_data['title']) 
+	$_SESSION['errors']['title'] = _T('error_no_rep_name');
 
-if (count($errors)) {
+if (count($_SESSION['errors'])) {
     header("Location: " . $GLOBALS['HTTP_REFERER']);
     exit;
-} else {
-	$fl = "title='" . clean_input($rep_data['title']) . "',id_author=" . $rep_data['id_author'] . ",date_update=NOW()";
+}
+
+//
+// Proceed with update of report info
+//
+
+$fl = "title = '" . clean_input($rep_data['title']) . "', "
+	. "id_author = " . $rep_data['id_author'] . ", "
+	. "description = '" . clean_input($rep_data['description']) . "', "
+	. "date_update = NOW()";
 
 // Put public access rights settings in a separate string
 //	$public_access_rights = '';
@@ -68,49 +70,50 @@ if (count($errors)) {
 //	else
 //		$public_access_rights .= ", pub_write=0";
 
-	if ($id_report > 0) {
-		// Check access rights
-		//if (!allowed($id_report,'e')) die("You don't have permission to change this case's information!");
-		// If admin access is allowed, set all fields
-		if (true) $q = "UPDATE lcm_report SET $fl WHERE id_report=$id_report";
-		else $q = "UPDATE lcm_report SET $fl WHERE id_report=$id_report";
-	} else {
-		$q = "INSERT INTO lcm_report SET id_report=0,date_creation=NOW(),$fl";
-		$result = lcm_query($q);
-		$id_report = lcm_insert_id();
 
-		// Insert new case_author relation
-		//$q = "INSERT INTO lcm_case_author SET
-		//		id_case=$id_case,
-		//		id_author=$id_author,
-		//		ac_read=1,
-		//		ac_write=1,
-		//		ac_admin=1";
-	}
+if ($id_report > 0) {
+	// Check access rights
+	// if (!allowed($id_report,'e')) die("You don't have permission to change this case's information!");
+	// If admin access is allowed, set all fields
 
-	// Some advanced ideas for future use
-	//$q="INSERT INTO lcm_case SET id_case=$id_case,$fl ON DUPLICATE KEY UPDATE $fl";
-	//$q="INSERT INTO lcm_case $cl VALUES $vl ON DUPLICATE KEY UPDATE $fl";
-
+	if (true)
+		$q = "UPDATE lcm_report SET $fl WHERE id_report = $id_report";
+	else 
+		$q = "UPDATE lcm_report SET $fl WHERE id_report = $id_report";
+	
+	lcm_query($q);
+} else {
+	$q = "INSERT INTO lcm_report SET id_report=0,date_creation=NOW(),$fl";
 	$result = lcm_query($q);
+	$id_report = lcm_insert_id();
 
-    // Clear the session
-	// [ML] errors must be shown first
-	// session_destroy();
-
-	//header("Location: rep_det.php?rep=$id_report");
-	$ref_edit_rep = ($rep_data['ref_edit_rep'] ? $rep_data['ref_edit_rep'] : "rep_det.php?rep=$id_report");
-
-	// Proceed accoring to the button type
-	switch ($submit) {
-		case 'addnew':
-			header("Location: edit_rep.php?rep=0&ref=$ref_edit_rep");
-			break;
-		case 'adddet':
-			header("Location: rep_det.php?rep=$id_report");
-			break;
-		default :
-			header("Location: $ref_edit_rep");
-	}
+	// Insert new case_author relation
+	//$q = "INSERT INTO lcm_case_author SET
+	//		id_case=$id_case,
+	//		id_author=$id_author,
+	//		ac_read=1,
+	//		ac_write=1,
+	//		ac_admin=1";
 }
+
+// Some advanced ideas for future use
+//$q="INSERT INTO lcm_case SET id_case=$id_case,$fl ON DUPLICATE KEY UPDATE $fl";
+//$q="INSERT INTO lcm_case $cl VALUES $vl ON DUPLICATE KEY UPDATE $fl";
+// $result = lcm_query($q);
+
+//header("Location: rep_det.php?rep=$id_report");
+$ref_edit_rep = ($rep_data['ref_edit_rep'] ? $rep_data['ref_edit_rep'] : "rep_det.php?rep=$id_report");
+
+// Proceed accoring to the button type
+switch ($submit) {
+	case 'addnew':
+		header("Location: edit_rep.php?rep=0&ref=$ref_edit_rep");
+		break;
+	case 'adddet':
+		header("Location: rep_det.php?rep=$id_report");
+		break;
+	default :
+		header("Location: $ref_edit_rep");
+}
+
 ?>
