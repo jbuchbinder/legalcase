4bb6508ceacee05d0ff98776ba77afd7f94d7327
antzi <antzi>
2005-02-17 15:08:59 +0000

    Fixed GPL formatting

--- import_db.php
+++ import_db.php
@@ -16,9 +16,9 @@
 
 	You should have received a copy of the GNU General Public License along
 	with this program; if not, write to the Free Software Foundation, Inc.,
-    59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
+	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
 
-	$Id: import_db.php,v 1.7 2005/02/17 09:45:40 antzi Exp $
+	$Id: import_db.php,v 1.8 2005/02/17 15:09:26 antzi Exp $
 */
 
 include('inc/inc.php');
