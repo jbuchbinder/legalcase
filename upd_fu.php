eae14c221aaf18c2322064d5418c7e33fddd96c6
antzi <antzi>
2005-03-09 14:52:50 +0000

    Added setting relation to the appointment when creating as a result if it.

--- upd_fu.php
+++ upd_fu.php
@@ -18,7 +18,7 @@
 	with this program; if not, write to the Free Software Foundation, Inc.,
 	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
 
-	$Id: upd_fu.php,v 1.33 2005/03/09 03:51:56 antzi Exp $
+	$Id: upd_fu.php,v 1.34 2005/03/09 14:52:50 antzi Exp $
 */
 
 include('inc/inc.php');
@@ -162,6 +162,14 @@ if (count($_SESSION['errors'])) {
 			lcm_panic("$q<br>\nError ".lcm_errno().": ".lcm_error());
 
 		$id_followup = lcm_insert_id();
+
+		// Set relation to the parent appointment, if any
+		if (! empty($_SESSION['fu_data']['id_app'])) {
+			$q = "INSERT INTO lcm_app_fu SET id_app=" . $_SESSION['fu_data']['id_app']
+				. ", id_followup=$id_followup, relation='parent'";
+			if (!($result = lcm_query($q))) 
+				lcm_panic("$q<br>\nError ".lcm_errno().": ".lcm_error());
+		}
 	}
 
 	// Send user back to add/edit page's referer or (default) to followup detail page
