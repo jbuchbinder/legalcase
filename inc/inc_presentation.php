d2c0d518782221300e8dcb7a0d5906e79e5ec9f9
antzi <antzi>
2005-03-18 04:54:09 +0000

    Added current month calendar in the left navbar

--- inc/inc_presentation.php
+++ inc/inc_presentation.php
@@ -18,7 +18,7 @@
 	with this program; if not, write to the Free Software Foundation, Inc.,
 	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
 
-	$Id: inc_presentation.php,v 1.166 2005/03/17 15:22:59 mlutfy Exp $
+	$Id: inc_presentation.php,v 1.167 2005/03/18 04:54:09 antzi Exp $
 */
 
 //
@@ -223,7 +223,28 @@ function lcm_page_start($title = "", $css_files = "", $meta = '') {
 						<div class=\"nav_column_menu_head\">
 							<div class=\"mm_calendar\">" . _T('menu_calendar') . "</div>
 						</div>
-						<p class=\"nav_column_text\">". format_date() ."</p>
+						<p class=\"nav_column_text\">";
+//	echo format_date();
+	// Show calendar
+	include_lcm('inc_calendar');
+/*	$q = "SELECT lcm_app.*
+			FROM lcm_app, lcm_author_app as a
+			WHERE (a.id_author=" . $GLOBALS['author_session']['id_author'] . "
+				AND lcm_app.id_app=a.id_app)";
+
+	$result = lcm_query($q);
+
+	if (lcm_num_rows($result) > 0) {
+		$events = array();
+		while ($row=lcm_fetch_array($result)) {
+			$events[] = $row;
+		}
+		echo lcm_http_calendrier_ics($events,'20050222');
+	} */
+	$now = date('Y-m-d');
+	echo http_calendrier_agenda(mois($now), annee($now), jour($now), mois($now), annee($now));
+	echo "</p>";
+	echo "
 					</div>\n";
 	
 	// Start agenda box
