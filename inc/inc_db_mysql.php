4a7392f7bf45e171c8cb6d9de69bf3305f7e1336
mlutfy <mlutfy>
2005-03-18 16:02:35 +0000

    added GPL + XHTML mini fix

--- inc/inc_db_mysql.php
+++ inc/inc_db_mysql.php
@@ -1,5 +1,26 @@
 <?php
 
+/*
+	This file is part of the Legal Case Management System (LCM).
+	(C) 2004-2005 Free Software Foundation, Inc.
+
+	This program is free software; you can redistribute it and/or modify it
+	under the terms of the GNU General Public License as published by the
+	Free Software Foundation; either version 2 of the License, or (at your
+	option) any later version.
+
+	This program is distributed in the hope that it will be useful, but
+	WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+	or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+	for more details.
+
+	You should have received a copy of the GNU General Public License along
+	with this program; if not, write to the Free Software Foundation, Inc.,
+	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
+
+	$Id: inc_db_mysql.php,v 1.18 2005/03/18 16:02:35 mlutfy Exp $
+*/
+
 if (defined('_INC_DB_MYSQL')) return;
 define('_INC_DB_MYSQL', '1');
 
@@ -37,7 +58,7 @@ function lcm_query_db($query) {
 	if ($my_debug AND lcm_sql_errno()) {
 		$s = lcm_sql_error();
 		$error = _T('warning_sql_query_failed') . "<br />\n" . htmlentities($query) . "<br />\n";
-		$error .= "&laquo; " . htmlentities($s) . " &raquo;<p>";
+		$error .= "&laquo; " . htmlentities($s) . " &raquo;<br />";
 		echo $error;
 		echo lcm_getbacktrace();
 	}
