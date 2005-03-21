dd5aff9008d66ecbcf3b6e6d24d9453ac7243de8
makaveev <makaveev>
2005-03-21 14:23:12 +0000

    added link on the Balance logo to the summary.php

--- inc/inc_presentation.php
+++ inc/inc_presentation.php
@@ -18,7 +18,7 @@
 	with this program; if not, write to the Free Software Foundation, Inc.,
 	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
 
-	$Id: inc_presentation.php,v 1.173 2005/03/21 08:30:54 makaveev Exp $
+	$Id: inc_presentation.php,v 1.174 2005/03/21 14:23:12 makaveev Exp $
 */
 
 //
@@ -156,6 +156,7 @@ function lcm_page_start($title = "", $css_files = "", $meta = '') {
 	//
 	
 	echo "<div id='header'>
+		<a href='summary.php' class='balance_link'>&nbsp;</a>
 		<h1 class='lcm_main_head'><a href='summary.php' class='head_ttl_link'>" . $site_name . "</a></h1>
 		<div class='lcm_slogan'><a href='summary.php' class='head_subttl_link'>" . $site_desc . "</a></div>
 	</div>";
