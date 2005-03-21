4417dced7a41398b62af0d556020dca9aa815a1e
mlutfy <mlutfy>
2005-03-21 12:01:43 +0000

    put Go To Page stuff in show_list_end() + added headers sorting by name_first

--- listauthors.php
+++ listauthors.php
@@ -18,12 +18,14 @@
 	with this program; if not, write to the Free Software Foundation, Inc.,
 	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
 
-	$Id: listauthors.php,v 1.23 2005/03/02 17:30:21 antzi Exp $
+	$Id: listauthors.php,v 1.24 2005/03/21 12:01:43 mlutfy Exp $
 */
 
 include('inc/inc.php');
 include_lcm('inc_acc');
 include_lcm('inc_filters');
+
+lcm_page_start("List of authors"); // TRAD
 
 $q = "SELECT id_author,name_first,name_middle,name_last,status
 		FROM lcm_author
@@ -34,17 +36,21 @@ if (strlen($find_author_string)>1) {
 	$q .= " AND ((name_first LIKE '%$find_author_string%')"
 		. " OR (name_middle LIKE '%$find_author_string%')"
 		. " OR (name_last LIKE '%$find_author_string%'))";
-	lcm_page_start("Authors, containing '$find_author_string':");
-} else {
-	lcm_page_start("List of authors");
 }
 
 $q .= ")";
 
-// Do the query
-$result = lcm_query($q);
+// Sort clients by first name
+// [ML] I know, problably more logical by last name, but we do not split the columns
+// later we can sort by any column if we need to
+$order_name_first = 'ASC';
+if (isset($_REQUEST['order_name_first']))
+	if ($_REQUEST['order_name_first'] == 'ASC' || $_REQUEST['order_name_first'] == 'DESC')
+		$order_name_first = $_REQUEST['order_name_first'];
 
-// Get the number of rows in the result
+$q .= " ORDER BY name_first " . $order_name_first;
+
+$result = lcm_query($q);
 $number_of_rows = lcm_num_rows($result);
 
 // Check for correct start position of the list
@@ -56,85 +62,48 @@ if ($list_pos>0)
 		die("Error seeking position $list_pos in the result");
 
 // Search form
-?>
-<form name="frm_find_author" class="search_form" action="listauthors.php" method="post">
-	Find author:&nbsp;<input type="text" name="find_author_string" size="10" class="search_form_txt"<?php
+show_find_box('author', $find_author_string);
 
-//	if (isset($find_author_string)) echo " value='$find_author_string'";
-	echo " value='$find_author_string'";
+$headers = array();
+$headers[0]['title'] = 'Name'; // TRAD
+$headers[0]['order'] = 'order_name_first';
+$headers[0]['default'] = 'asc';
+$headers[1]['title'] = 'Status'; // TRAD
+$headers[1]['order'] = 'no_order';
 
-?> />&nbsp;<input type="submit" name="submit" value="Search" class="search_form_btn" />
-</form>
-
-<table border='0' width='99%' align='center' class='tbl_usr_dtl'>
-<tr><th class='heading'>Name</th><th class='heading'>Status</th><th class='heading'>Action</th></tr>
-<?php
+show_list_start($headers);
 
 // Process the output of the query
 for ($i = 0 ; (($i<$prefs['page_rows']) && ($row = lcm_fetch_array($result))) ; $i++) {
 	// Show author name
-	echo "\t<tr><td class='tbl_cont_" . ($i % 2 ? "dark" : "light") . "'>";
+	echo "<tr>\n";
+	echo "<td class='tbl_cont_" . ($i % 2 ? "dark" : "light") . "'>";
 	echo '<a href="author_det.php?author=' . $row['id_author'] . '" class="content_link">';
-	echo highlight_matches(clean_output($row['name_first'] . ' ' . $row['name_middle'] . ' '
-		. $row['name_last']),$find_author_string);
+	echo highlight_matches(get_person_name($row), $find_author_string);
 	echo "</a></td>\n";
+
 	// Show author status
-	echo "\t\t<td class='tbl_cont_" . ($i % 2 ? "dark" : "light") . "'>";
+	echo "<td class='tbl_cont_" . ($i % 2 ? "dark" : "light") . "'>";
 	echo clean_output($row['status']);
 	echo "</td>\n";
+
 	// Show author action(s)
+	/* [ML]
 	echo "\t\t<td class='tbl_cont_" . ($i % 2 ? "dark" : "light") . "'>";
 	if (($GLOBALS['author_session']['status'] == 'admin') ||
 		($row['id_author'] == $GLOBALS['author_session']['id_author']))
 			echo '<a href="edit_author.php?author=' . $row['id_author'] . '" class="content_link">Edit</a>';
-	echo "</td>\n\t</tr>\n";
+	echo "</td>\n";
+	*/
+
+	echo "</tr>\n";
 }
 
-echo "</table>\n";
-
-?>
-<table border='0' align='center' width='99%' class='page_numbers'>
-	<tr><td align="left" width="15%">
-<?php
-// Show link to previous page
-if ($list_pos>0) {
-	echo '<a href="listauthors.php';
-	if ($list_pos>$prefs['page_rows']) echo '?list_pos=' . ($list_pos - $prefs['page_rows']);
-	if (strlen($find_author_string)>1) echo "&amp;find_author_string=" . rawurlencode($find_author_string);
-	echo '" class="content_link">< Prev</a> ';
-}
-
-echo "</td>\n\t\t<td align='center' width='70%'>";
-
-// Show page numbers with direct links
-$list_pages = ceil($number_of_rows / $prefs['page_rows']);
-if ($list_pages>1) {
-	echo 'Go to page: ';
-	for ($i=0 ; $i<$list_pages ; $i++) {
-		if ($i==floor($list_pos / $prefs['page_rows'])) echo '['. ($i+1) . '] ';
-		else {
-			echo '<a href="listauthors.php?list_pos=' . ($i*$prefs['page_rows']);
-			if (strlen($find_author_string)>1) echo "&amp;find_author_string=" . rawurlencode($find_author_string);
-			echo '" class="content_link">' . ($i+1) . '</a> ';
-		}
-	}
-}
-
-echo "</td>\n\t\t<td align='right' width='15%'>";
-
-// Show link to next page
-$next_pos = $list_pos + $prefs['page_rows'];
-if ($next_pos<$number_of_rows) {
-	echo "<a href=\"listauthors.php?list_pos=$next_pos";
-	if (strlen($find_author_string)>1) echo "&amp;find_author_string=" . rawurlencode($find_author_string);
-	echo '" class="content_link">Next ></a>';
-}
-
-echo "</td>\n\t</tr>\n</table>\n";
+show_list_end($list_pos, $number_of_rows);
 
 // Show add auhor button
 if ($GLOBALS['author_session']['status'] == 'admin')
-	echo '<a href="edit_author.php?author=0" class="create_new_lnk">Add author</a><br /><br />';
+	echo '<p><a href="edit_author.php?author=0" class="create_new_lnk">Add author</a></p>'; // TRAD
 
 lcm_page_end();
 ?>
