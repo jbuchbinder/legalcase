50badf05940224c3570275d7c58916aa0baece18
mlutfy <mlutfy>
2005-03-22 13:07:10 +0000

    fixed how the script dies (sorry for reformatting) + added 'search' box

--- sel_auth.php
+++ sel_auth.php
@@ -18,7 +18,7 @@
 	with this program; if not, write to the Free Software Foundation, Inc.,
 	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
 
-	$Id: sel_auth.php,v 1.8 2005/03/19 15:42:12 mlutfy Exp $
+	$Id: sel_auth.php,v 1.9 2005/03/22 13:07:10 mlutfy Exp $
 */
 
 include('inc/inc.php');
@@ -26,87 +26,104 @@ include_lcm('inc_acc');
 
 global $prefs;
 
-if ($case > 0) {
-	$destination = "case_det.php?id_case=" . $case;
+$case = intval($_REQUEST['case']);
 
-	if (isset($GLOBALS['HTTP_REFERER']))
-		$destination = $GLOBALS['HTTP_REFERER'];
+if (! ($case > 0))
+	die("Which case?");
+
+$destination = "case_det.php?id_case=" . $case;
+
+if (isset($GLOBALS['HTTP_REFERER']))
+	$destination = $GLOBALS['HTTP_REFERER'];
 	
-	$dest_link = new Link($destination);
+$dest_link = new Link($destination);
 
-	if (allowed($case,'a')) {
-		// Prepare query
-		$q = "SELECT *
-			FROM lcm_case_author
-			WHERE id_case=$case";
+if (! allowed($case,'a'))
+	die("You don't have permission to add users to this case!");
 
-		$result = lcm_query($q);
+$q = "SELECT *
+		FROM lcm_case_author
+		WHERE id_case=$case";
 
-		// Prepare list query
-		$q = "SELECT id_author,name_first,name_middle,name_last
-			FROM lcm_author
-			WHERE id_author NOT IN (0";
+$result = lcm_query($q);
 
-		// Add clients to NOT IN list
-		while ($row = lcm_fetch_array($result)) {
-			$q .= ',' . $row['id_author'];
-		}
-		$q .= ')';
+$q = "SELECT id_author,name_first,name_middle,name_last
+		FROM lcm_author
+		WHERE id_author NOT IN (0";
 
-		// Sort by name_first
-		$order_name = 'ASC';
-		if (isset($_REQUEST['order_name']))
-			if ($_REQUEST['order_name'] == 'ASC' || $_REQUEST['order_name'] == 'DESC')
-				$order_name = $_REQUEST['order_name'];
+// Add clients to NOT IN list
+while ($row = lcm_fetch_array($result)) {
+	$q .= ',' . $row['id_author'];
+}
+
+$q .= ')';
+
+// Add search criteria if any
+if (strlen($_REQUEST['find_author_string']) > 1) {
+	$find_author_string = $_REQUEST['find_author_string'];
+
+	$q .= " AND ((name_first LIKE '%$find_author_string%')"
+		. " OR (name_middle LIKE '%$find_author_string%')"
+		. " OR (name_last LIKE '%$find_author_string%'))";
+}
+
+// Sort by name_first
+$order_name = 'ASC';
+if (isset($_REQUEST['order_name']))
+	if ($_REQUEST['order_name'] == 'ASC' || $_REQUEST['order_name'] == 'DESC')
+		$order_name = $_REQUEST['order_name'];
 		
-		$q .= " ORDER BY name_first " . $order_name;
+$q .= " ORDER BY name_first " . $order_name;
 
-		$result = lcm_query($q);
-		$number_of_rows = lcm_num_rows($result);
+$result = lcm_query($q);
+$number_of_rows = lcm_num_rows($result);
 
-		// Check if any author(s) available for selection
-		if (lcm_num_rows($result) > 0)
-			lcm_page_start("Select users(s)"); // TRAD
-		else {
-			header('Location: ' . $dest_link->getUrlForHeader());
-			exit;
-		}
+// Check if any author(s) available for selection
+if (lcm_num_rows($result) > 0)
+	lcm_page_start("Select users(s)"); // TRAD
+else {
+	header('Location: ' . $dest_link->getUrlForHeader());
+	exit;
+}
 
-		show_context_start();
-		show_context_case_title($case);
-		show_context_case_involving($case);
-		show_context_end();
+show_context_start();
+show_context_case_title($case);
+show_context_case_involving($case);
+show_context_end();
 
-		echo '<form action="add_auth.php" method="post">' . "\n";
+show_find_box('author', $find_author_string, '__self__');
 
-		$headers = array();
-		$headers[0]['title'] = '';
-		$headers[0]['order'] = 'no_order';
-		$headers[1]['title'] = 'Name'; // TRAD
-		$headers[1]['order'] = 'order_name';
+echo '<form action="add_auth.php" method="post">' . "\n";
 
-		$list_pos = get_list_pos($result);
-		show_list_start($headers);
+$headers = array();
+$headers[0]['title'] = '';
+$headers[0]['order'] = 'no_order';
+$headers[1]['title'] = _Th('person_input_name');
+$headers[1]['order'] = 'order_name';
 
-		for ($i = 0 ; (($i < $prefs['page_rows']) && ($row = lcm_fetch_array($result))) ; $i++) {
-			echo "<tr>\n";
-			echo '<td class="tbl_cont_' . ($i % 2 ? "dark" : "light") . '">';
-			echo '<input type="checkbox" name="authors[]" value="' . $row['id_author'] . '" />';
-			echo "</td>\n";
-			echo '<td class="tbl_cont_' . ($i % 2 ? "dark" : "light") . '">' . get_person_name($row) . "</td>\n";
-			echo "</tr>\n";
-		}
+$list_pos = get_list_pos($result);
+show_list_start($headers);
 
-		show_list_end($list_pos, $number_of_rows);
-	?>
+for ($i = 0 ; (($i < $prefs['page_rows']) && ($row = lcm_fetch_array($result))) ; $i++) {
+	echo "<tr>\n";
+	echo '<td class="tbl_cont_' . ($i % 2 ? "dark" : "light") . '">';
+	echo '<input type="checkbox" name="authors[]" value="' . $row['id_author'] . '" />';
+	echo "</td>\n";
+	echo '<td class="tbl_cont_' . ($i % 2 ? "dark" : "light") . '">' . get_person_name($row) . "</td>\n";
+	echo "</tr>\n";
+}
 
-		<input type="hidden" name="case" value="<?php echo $case; ?>" />
-		<input type="hidden" name="ref_sel_auth" value="<?php echo $dest_link->getUrl(); ?>" />
-		<button name="submit" type="submit" value="submit" class="simple_form_btn"><?php echo _T('button_validate'); ?></button>
-	</form>
-	<?php
-	} else die("You don't have permission to add users to this case!");
-} else die("Which case?");
+show_list_end($list_pos, $number_of_rows);
+
+?>
+
+<input type="hidden" name="case" value="<?php echo $case; ?>" />
+<input type="hidden" name="ref_sel_auth" value="<?php echo $dest_link->getUrl(); ?>" />
+<button name="submit" type="submit" value="submit" class="simple_form_btn"><?php echo _T('button_validate'); ?></button>
+</form>
+
+<?php
 
 lcm_page_end();
+
 ?>
