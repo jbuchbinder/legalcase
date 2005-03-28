741481c9db6b9fa781f18786112e44c2b0f367ba
mlutfy <mlutfy>
2005-03-28 11:09:37 +0000

    minor fix for keyword name

--- edit_case.php
+++ edit_case.php
@@ -18,7 +18,7 @@
 	with this program; if not, write to the Free Software Foundation, Inc.,
 	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
 
-	$Id: edit_case.php,v 1.68 2005/03/28 11:02:06 mlutfy Exp $
+	$Id: edit_case.php,v 1.69 2005/03/28 11:09:37 mlutfy Exp $
 */
 
 include('inc/inc.php');
@@ -220,8 +220,8 @@ if ($_SESSION['case_data']['id_case']) {
 
 	foreach ($kwg_for_case as $kwg) {
 		echo "<tr>\n";
-		echo '<td>' . f_err_star('keyword_' . $cpt_kw) . $kwg['title'] 
-			. " (" . _T('keywords_input_policy_' . $kwg['policy']) . ")</td>\n";
+		echo '<td>' . f_err_star('keyword_' . $cpt_kw) . _Ti($kwg['title']) 
+			. "<br />(" . _T('keywords_input_policy_' . $kwg['policy']) . ")</td>\n";
 
 		$kw_for_kwg = get_keywords_in_group_id($kwg['id_group']);
 		if (count($kw_for_kwg)) {
