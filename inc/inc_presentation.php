606fe8dfac7d0fe497242cf5cd7ed50897a7446c
mlutfy <mlutfy>
2005-04-04 14:30:02 +0000

    html_start: removed reference to layer.js, not used

--- inc/inc_presentation.php
+++ inc/inc_presentation.php
@@ -18,7 +18,7 @@
 	with this program; if not, write to the Free Software Foundation, Inc.,
 	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
 
-	$Id: inc_presentation.php,v 1.200 2005/04/04 14:14:11 mlutfy Exp $
+	$Id: inc_presentation.php,v 1.201 2005/04/04 14:30:02 mlutfy Exp $
 */
 
 //
@@ -69,7 +69,6 @@ function lcm_html_start($title = "AUTO", $css_files = "", $meta = '') {
 
 	// The 'antifocus' is used to erase default titles such as "New appointment"
 	// other functions are used in calendar functions (taken from Spip's presentation.js)
-	echo '<script type="text/javascript" src="inc/layer.js"></script>' . "\n";
 	echo "<script type='text/javascript'><!--
 		var title_antifocus = false;
 
