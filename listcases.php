a306d245adf6574480d7178ee710237f4dacb135
mlutfy <mlutfy>
2005-04-07 16:17:19 +0000

    initial test of 'show latest follow-ups in cases concerning the user (BHC request)

--- listcases.php
+++ listcases.php
@@ -18,7 +18,7 @@
 	with this program; if not, write to the Free Software Foundation, Inc.,
 	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
 
-	$Id: listcases.php,v 1.53 2005/04/07 10:22:19 mlutfy Exp $
+	$Id: listcases.php,v 1.54 2005/04/07 16:17:19 mlutfy Exp $
 */
 
 include('inc/inc.php');
@@ -160,16 +160,164 @@ if ($list_pos > 0)
 		lcm_panic("Error seeking position $list_pos in the result");
 
 // Process the output of the query
+echo '<p class="normal_text">' . "\n";
 show_listcase_start();
 
 for ($i = 0 ; (($i<$prefs['page_rows']) && ($row = lcm_fetch_array($result))); $i++)
 	show_listcase_item($row, $i);
 
 show_listcase_end($list_pos, $number_of_rows);
+echo "</p>\n";
 
 echo '<p><a href="edit_case.php?case=0" class="create_new_lnk">' . _T('case_button_new') . "</a></p>\n";
 echo '<p><a href="edit_client.php" class="create_new_lnk">' . _T('client_button_new') . "</a></p>\n";
-echo "<br /><br />\n";
+
+//
+// List of recent follow-ups
+//
+
+echo '<a name="fu"></a>' . "\n";
+echo "<div class='prefs_column_menu_head'>" . 'Recent follow-ups' . "</div>\n";
+
+$headers[0]['title'] = "#";
+$headers[0]['order'] = 'no_order';
+$headers[1]['title'] = _Th('time_input_date_start');
+$headers[1]['order'] = 'fu_order';
+$headers[1]['default'] = 'ASC';
+$headers[2]['title'] = (($prefs['time_intervals'] == 'absolute') ? _Th('time_input_date_end') : _Th('time_input_length'));
+$headers[2]['order'] = 'no_order';
+$headers[3]['title'] = _Th('case_input_author');
+$headers[3]['order'] = 'no_order';
+$headers[4]['title'] = _Th('fu_input_type');
+$headers[4]['order'] = 'no_order';
+$headers[5]['title'] = _Th('fu_input_description');
+$headers[5]['order'] = 'no_order';
+
+echo '<p class="normal_text">' . "\n";
+			
+show_list_start($headers);
+
+$q = "SELECT fu.id_case, fu.id_followup, fu.date_start, fu.date_end, fu.type, fu.description,
+			a.name_first, a.name_middle, a.name_last,
+			c.title ";
+			
+// Author of the follow-up
+if ($case_owner == 'all' && $author_session['status'] == 'admin') {
+
+	// all will be selected (admin only)
+	$q .= " FROM lcm_followup as fu, lcm_author as a
+			WHERE fu.id_author = a.id_author ";
+
+} else {
+
+	// Get list of cases on which author is assigned
+	$q_temp = "SELECT c.id_case
+				FROM lcm_case_author as ca, lcm_case as c
+				WHERE ca.id_case = c.id_case
+				  AND ca.id_author = " . $author_session['id_author'];
+
+	if ($case_period < 1900) // since X days
+		$q_temp .= " AND TO_DAYS(NOW()) - TO_DAYS(c.date_creation) < " . $case_period;
+	else // for year X
+		$q_temp .= " AND YEAR(date_creation) = " . $case_period;
+			 
+	$r_temp = lcm_query($q_temp);
+	$list_cases = array();
+
+	while ($row = lcm_fetch_array($r_temp))
+		$list_cases[] = $row['id_case'];
+
+	// Cases on which I am working (include colleagues)
+	$q .= " FROM lcm_followup as fu, lcm_author as a, lcm_case as c
+			WHERE fu.id_author = a.id_author 
+			  AND c.id_case = fu.id_case AND (";
+
+	if ($case_owner == 'public')
+		$q .= " c.public = 1 OR ";
+
+	$q .= " fu.id_case IN (" . implode(",", $list_cases) . "))";
+}
+
+// Period (date_creation) to show
+if ($case_period < 1900) // since X days
+	$q .= " AND TO_DAYS(NOW()) - TO_DAYS(date_start) < " . $case_period;
+else // for year X
+	$q .= " AND YEAR(date_start) = " . $case_period;
+
+
+// Add ordering
+if ($fu_order)
+	$q .= " ORDER BY date_start $fu_order, id_followup $fu_order";
+			
+$result = lcm_query($q);
+
+// Check for correct start position of the list
+$number_of_rows = lcm_num_rows($result);
+$fu_list_pos = 0;
+				
+if (isset($_REQUEST['fu_list_pos']))
+	$fu_list_pos = $_REQUEST['fu_list_pos'];
+				
+if ($fu_list_pos >= $number_of_rows)
+	$fu_list_pos = 0;
+				
+// Position to the page info start
+if ($fu_list_pos > 0)
+	if (!lcm_data_seek($result,$fu_list_pos))
+		lcm_panic("Error seeking position $fu_list_pos in the result");
+			
+// Set the length of short followup title
+$title_length = (($prefs['screen'] == "wide") ? 48 : 115);
+			
+// Process the output of the query
+for ($i = 0 ; (($i<$prefs['page_rows']) && ($row = lcm_fetch_array($result))); $i++) {
+	echo "<tr>\n";
+
+	// Id case
+	echo '<td><abbrev title="' . $row['title'] . '">' . $row['id_case'] . '</abbrev></td>';
+					
+	// Start date
+	echo '<td>' . format_date($row['date_start'], 'short') . '</td>';
+					
+	// Time
+	echo '<td>';
+	$fu_date_end = vider_date($row['date_end']);
+	if ($prefs['time_intervals'] == 'absolute') {
+		if ($fu_date_end) echo format_date($row['date_end'],'short');
+	} else {
+		$fu_time = ($fu_date_end ? strtotime($row['date_end']) - strtotime($row['date_start']) : 0);
+		echo format_time_interval($fu_time,($prefs['time_intervals_notation'] == 'hours_only'));
+	}
+	echo '</td>';
+
+	// Author initials
+	echo '<td>' . get_person_initials($row) . '</td>';
+
+	// Type
+	echo '<td>' . _T('kw_followups_' . $row['type'] . '_title') . '</td>';
+
+	// Description
+	if ($row['type'] == 'assignment' && is_numeric($row['description'])) {
+		$res1 = lcm_query("SELECT * FROM lcm_author WHERE id_author = " . $row['description']);
+		$author1 = lcm_fetch_array($res1);
+		$short_description = _T('case_info_author_assigned', array('name' => get_person_name($author1)));
+	} else{
+		if (strlen(lcm_utf8_decode($row['description'])) < $title_length) 
+			$short_description = $row['description'];
+		else
+			$short_description = substr($row['description'],0,$title_length) . '...';
+	}
+
+	echo '<td>';
+	echo '<a href="fu_det.php?followup=' . $row['id_followup'] . '" class="content_link">' . clean_output($short_description) . '</a>';
+	echo '</td>';
+
+	echo "</tr>\n";
+}
+
+show_list_end($fu_list_pos, $number_of_rows, false, 'fu');
+
+echo "</p>\n";
 
 lcm_page_end();
 
