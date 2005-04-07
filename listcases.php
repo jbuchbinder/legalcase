c581d9d333d19c3b26621ec57eb61a0d387347c4
mlutfy <mlutfy>
2005-04-07 10:16:04 +0000

    testing 'list-case filters', a bit functional

--- listcases.php
+++ listcases.php
@@ -18,17 +18,21 @@
 	with this program; if not, write to the Free Software Foundation, Inc.,
 	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
 
-	$Id: listcases.php,v 1.51 2005/03/31 14:44:18 mlutfy Exp $
+	$Id: listcases.php,v 1.52 2005/04/07 10:16:04 mlutfy Exp $
 */
 
 include('inc/inc.php');
 include_lcm('inc_acc');
 include_lcm('inc_filters');
 
+global $author_session;
+
 lcm_page_start(_T('title_my_cases'));
 lcm_bubble('case_list');
 
+//
 // For "find case"
+//
 $find_case_string = '';
 
 if (isset($_REQUEST['find_case_string'])) {
@@ -36,11 +40,73 @@ if (isset($_REQUEST['find_case_string'])) {
 	show_find_box('case', $find_case_string);
 }
 
+//
+// For "Filter case owner"
+//
+$case_owner = 'my';
+if (isset($_REQUEST['case_owner']))
+	$case_owner = $_REQUEST['case_owner'];
+
+// always include 'my' cases [ML] $q_owner is re-used below
+$q_owner .= " (a.id_author = " . $author_session['id_author'];
+
+if ($case_owner == 'public')
+	$q_owner .= " OR c.public = 1";
+
+$q_owner .= " ) ";
+
+//
+// For "Filter case date_creation"
+//
+$case_period = '365';
+if (isset($_REQUEST['case_period']))
+	$case_period = $_REQUEST['case_period'];
+
+//
+// Show filters form
+//
+$types_owner = array('my', 'public');
+$types_period = array('m1' => 30, 'm3' => 91, 'm6' => 182, 'y1' => 365); // 30 days, 3 months, 6 months, 1 year
+
+echo '<form action="listcases.php" method="get">' . "\n";
+echo "<p class=\"normal_text\">\n";
+echo "Filter: "; // TRAD
+echo '<select name="case_owner">';
+
+foreach ($types_owner as $t) {
+	$sel = ($case_owner == $t ? ' selected="selected" ' : '');
+	echo '<option value="' . $t . '"' . $sel . '>' . _T('case_filter_owner_' . $t) . "</option>\n";
+}
+
+echo "</select>\n";
+
+echo "Period: "; // TRAD
+echo '<select name="case_period">';
+
+foreach ($types_period as $key => $val) {
+	$sel = ($case_period == $val ? ' selected="selected" ' : '');
+	echo '<option value="' . $val . '"' . $sel . '>' . _T('case_filter_period_' . $key) . "</option>\n";
+}
+
+$q_dates = "SELECT DISTINCT YEAR(date_creation) as year
+			FROM lcm_case as c, lcm_case_author as a
+			WHERE c.id_case = a.id_case AND " . $q_owner;
+
+$result = lcm_query($q_dates);
+
+while($row = lcm_fetch_array($result))
+	echo '<option value="' . $row['year'] . '">' . $row['year'] . "</option>\n";
+
+echo "</select>\n";
+
+echo ' <button name="submit" type="submit" value="submit" class="simple_form_btn">' . _T('button_validate') . "</button>\n";
+echo "</p>\n";
+echo "</form>\n";
+
 // Select cases of which the current user is author
 $q = "SELECT c.id_case, title, id_court_archive, status, public, pub_write, date_creation
 		FROM lcm_case as c, lcm_case_author as a
-		WHERE (c.id_case = a.id_case
-			AND a.id_author = " . $GLOBALS['author_session']['id_author'];
+		WHERE (c.id_case = a.id_case ";
 
 if (strlen($find_case_string) > 0) {
 	$q .= " AND ( (c.id_case LIKE '%$find_case_string%')
@@ -49,6 +115,19 @@ if (strlen($find_case_string) > 0) {
 }
 
 $q .= ")";
+
+//
+// Apply filters to SQL
+//
+
+// Case owner
+$q .= " AND " . $q_owner;
+
+// Period (date_creation) to show
+if ($case_period < 1900) // since X days
+	$q .= " AND TO_DAYS(NOW()) - TO_DAYS(date_creation) < " . $case_period;
+else // for year X
+	$q .= " AND YEAR(date_creation) = " . $case_period;
 
 // Sort cases by creation date
 $case_order = 'DESC';
@@ -74,7 +153,6 @@ if ($list_pos >= $number_of_rows)
 if ($list_pos > 0)
 	if (!lcm_data_seek($result,$list_pos))
 		lcm_panic("Error seeking position $list_pos in the result");
-
 
 // Process the output of the query
 show_listcase_start();
