bbf760d9ec5b72f5a4f54ddf43c9397f177de8c6
mlutfy <mlutfy>
2005-04-07 16:53:15 +0000

    cleanup + trad

--- edit_auth.php
+++ edit_auth.php
@@ -18,7 +18,7 @@
 	with this program; if not, write to the Free Software Foundation, Inc.,
 	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
 
-	$Id: edit_auth.php,v 1.11 2004/12/10 10:52:04 antzi Exp $
+	$Id: edit_auth.php,v 1.12 2005/04/07 16:53:15 mlutfy Exp $
 */
 
 include('inc/inc.php');
@@ -28,22 +28,30 @@ include_lcm('inc_filters');
 // Initialise variables
 $case = intval($_GET['case']);
 
-if ($case > 0) {
-	if (allowed($case,'a')) {
-		$q = "SELECT *
-			FROM lcm_case_author,lcm_author
-			WHERE (id_case=$case
-				AND lcm_case_author.id_author=lcm_author.id_author";
-		if ($author > 0)
-			$q .= " AND lcm_author.id_author=$author";
-		$q .= ')';
+if (! ($case > 0))
+	die("Which case?");
 
-		$result = lcm_query($q);
+if (! allowed($case,'a'))
+	die("You don't have permission to edit this case's access rights.");
 
-		lcm_page_start("Edit author's rights on case $case");
-	?>
-	<form action="upd_auth.php" method="POST">
-	<h3>Access rights</h3>
+$q = "SELECT *
+	FROM lcm_case_author,lcm_author
+	WHERE (id_case=$case
+	  AND lcm_case_author.id_author=lcm_author.id_author";
+
+if ($author > 0)
+	$q .= " AND lcm_author.id_author=$author";
+
+$q .= ')';
+
+$result = lcm_query($q);
+
+lcm_page_start("Edit author's rights on case $case"); // TRAD
+
+echo '<form action="upd_auth.php" method="post">' . "\n";
+
+// TRAD TRAD TRAD ...
+?>
 		<table border="0" class="tbl_usr_dtl" width="99%">
 			<tr><th align="center" class="heading">User</th>
 				<th align="center" class="heading">Read</th>
@@ -51,40 +59,45 @@ if ($case > 0) {
 				<th align="center" class="heading">Edit</th>
 				<th align="center" class="heading">Admin</th>
 			</tr>
-	<?php
+<?php
 
-		// Process the output of the query
 		while ($row = lcm_fetch_array($result)) {
-			echo '		<tr><td align="left">';
-			echo clean_output($row['name_first'] . ' ' . $row['name_middle'] . ' ' . $row['name_last']) . "</td>\n";
-			echo '			<td align="center">';
+			echo "<tr>\n";
+
+			echo '<td align="left">';
+			echo get_person_name($row) . "</td>\n";
+
+			echo '<td align="center">';
 			echo '<input type="checkbox" name="auth[' . $row['id_author'] . "][ac_read]" . '" value="1"';
 			if ($row['ac_read']) echo ' checked';
 			echo "></td>\n";
-			echo '			<td align="center">';
+
+			echo '<td align="center">';
 			echo '<input type="checkbox" name="auth[' . $row['id_author'] . "][ac_write]" . '" value="1"';
 			if ($row['ac_write']) echo ' checked';
 			echo "></td>\n";
-			echo '			<td align="center">';
+
+			echo '<td align="center">';
 			echo '<input type="checkbox" name="auth[' . $row['id_author'] . "][ac_edit]" . '" value="1"';
 			if ($row['ac_edit']) echo ' checked';
 			echo "></td>\n";
-			echo '			<td align="center">';
+
+			echo '<td align="center">';
 			echo '<input type="checkbox" name="auth[' . $row['id_author'] . "][ac_admin]" . '" value="1"';
 			if ($row['ac_admin']) echo ' checked';
 			echo "></td>\n";
 		}
 	?>
+
 			</tr>
 		</table>
-		<button name="submit" type="submit" value="submit" class="simple_form_btn">Save</button>
-		<button name="reset" type="reset" class="simple_form_btn">Reset</button>
+
+		<p><button name="submit" type="submit" value="submit" class="simple_form_btn"><?php echo _T('button_validate'); ?></button></p>
 		<input type="hidden" name="case" value="<?php echo $case; ?>">
 		<input type="hidden" name="ref_edit_auth" value="<?php echo $HTTP_REFERER; ?>">
-	</form>
-	<?php
-		lcm_page_end();
-	} else die("You don't have permission to edit this case's access rights!");
-} else die("Which case?");
 
+	</form>
+
+<?php
+	lcm_page_end();
 ?>
