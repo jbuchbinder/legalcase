b32bedea50b481ab33d79bde159311782d6524f4
makaveev <makaveev>
2005-04-08 11:19:57 +0000

    fixed style class to Enter a new activity link

--- inc/inc_calendar.php
+++ inc/inc_calendar.php
@@ -21,7 +21,7 @@
 	with this program; if not, write to the Free Software Foundation, Inc.,
 	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
 
-	$Id: inc_calendar.php,v 1.21 2005/04/08 11:16:50 makaveev Exp $
+	$Id: inc_calendar.php,v 1.22 2005/04/08 11:19:57 makaveev Exp $
 */
 
 
@@ -772,7 +772,10 @@ function http_calendrier_clics($year, $month, $day, $clic, $script)
 	//  global $bleu, $jaune, $vert;
 
 	return http_href("$script?type=jour&jour=$day&mois=$month&annee=$year", $clic)
-		. '&nbsp;<a href="edit_app.php?time=' . rawurlencode("$year-$month-$day") . '" class="edit_lnk" title="' . _T('app_button_new') . '">'. _T('app_button_new') . '</a>';
+		. '&nbsp;<a href="edit_app.php?time=' . rawurlencode("$year-$month-$day") . '" class="content_lnk">'
+		. '<img src="images/jimmac/stock_edit-16.png" border="0" width="16" '
+		. 'height="16" alt="' . _T('app_button_new') . '" title="' . _T('app_button_new') . '" />'
+		. '</a>';
 }
 
 // Shows events of a week
