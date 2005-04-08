2308fccdbdf2835e9b92d6a08fd7a2b7f3b34d97
mlutfy <mlutfy>
2005-04-08 05:59:41 +0000

    minor cleanups + trad

--- set_case_stage.php
+++ set_case_stage.php
@@ -18,7 +18,7 @@
 	with this program; if not, write to the Free Software Foundation, Inc.,
 	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
 
-	$Id: set_case_stage.php,v 1.1 2005/03/06 14:38:48 antzi Exp $
+	$Id: set_case_stage.php,v 1.2 2005/04/08 05:59:41 mlutfy Exp $
 */
 
 include('inc/inc.php');
@@ -50,7 +50,8 @@ $q = "SELECT * FROM lcm_case WHERE id_case=$case";
 $result = lcm_query($q);
 
 // Check if the case exists
-if (!($row=lcm_fetch_array($result))) die("There is no such case!");
+if (!($row=lcm_fetch_array($result)))
+	die("There is no such case.");
 
 // Check if the stage is changed
 if ($row['stage'] == $stage) {
@@ -59,7 +60,7 @@ if ($row['stage'] == $stage) {
 }
 
 // Start the page
-lcm_page_start('Changing case stage: ' . $row['title']);
+lcm_page_start('Changing case stage: ' . $row['title']); // TRAD
 
 // Show the errors (if any)
 echo show_all_errors($_SESSION['errors']);
@@ -67,24 +68,27 @@ echo show_all_errors($_SESSION['errors']);
 // Write form
 echo "<form action='upd_fu.php' method='post'>
 	<table class='tbl_usr_dtl' width='99%'>
-		<tr><td>Start:</td>
+		<tr><td>" . _Ti('time_input_date_start') . "</td>
 			<td>";
+
 $date_start = date('Y-m-d H:i:s');
-echo _T('calendar_info_date') . ' ';
 echo get_date_inputs('start', $date_start, false);
-echo ' ' . _T('calendar_info_time') . ' ';
+echo ' ' . _T('time_input_time_at') . ' ';
 echo get_time_inputs('start', $date_start);
 echo ' ' . f_err_star('date_start',$errors);
-echo "</td>
+
+echo "</td>\n";
+echo "</tr><tr>\n";
+echo "<td>" . "Type:" . /* TRAD */ "</td>
+			<td><input type='hidden' name='type' value='stage_change'>" . _T('kw_followups_stage_change_title') . "</td>
 		</tr>
-		<tr><td>Type:</td>
-			<td><input type='hidden' name='type' value='stage_change'>Stage change</td>
-		</tr>
-		<tr><td valign='top'>Description:</td>
+		<tr><td valign='top'>" . f_err_star('description') . "Description:" . /* TRAD */ "</td>
 			<td><textarea name='description' rows='15' cols='40' class='frm_tarea'>" . $_SESSION['fu_data']['description'] . "</textarea></td>
 		</tr>\n";
 echo "\t</table>
-	<button name='submit' type='submit' value='submit' class='simple_form_btn'>" . _T('button_validate') . "</button>
+
+	<p><button name='submit' type='submit' value='submit' class='simple_form_btn'>" . _T('button_validate') . "</button></p>
+
 	<input type='hidden' name='id_case' value='$case'>
 	<input type='hidden' name='new_stage' value='$stage'>
 	<input type='hidden' name='ref_edit_fu' value='" . $GLOBALS['HTTP_REFERER'] . "'>
