d3e705313b418daaaeb4d8ca973b0d29e0612364
mlutfy <mlutfy>
2005-04-11 13:50:30 +0000

    allowed: replaced SQL union by left-join so that users not on case can fallback on public + case-specific access rights bypass public, so even if 'public=1' but lcm_case_author.ac_read=0, then user does not have the right (**testing required**)

--- inc/inc_acc.php
+++ inc/inc_acc.php
@@ -18,13 +18,15 @@
 	with this program; if not, write to the Free Software Foundation, Inc.,
 	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
 
-	$Id: inc_acc.php,v 1.8 2005/04/11 13:06:53 mlutfy Exp $
+	$Id: inc_acc.php,v 1.9 2005/04/11 13:50:30 mlutfy Exp $
 */
 
 // Execute this file only once
 if (defined('_INC_ACC')) return;
 define('_INC_ACC', '1');
 
+// c.f. http://www.lcm.ngo-bg.org/ecrire/articles.php3?id_article=76
+// or http://www.lcm.ngo-bg.org/article76.html
 function allowed($case, $access) {
 	// By default, do not allow access
 	$allow = false;
@@ -36,11 +38,14 @@ function allowed($case, $access) {
 	// Check if the case number is present
 	if ($case > 0) {
 
-		$q = "SELECT ca.*, c.status
-				FROM lcm_case_author as ca, lcm_case as c
-				WHERE (ca.id_case = " . intval($case) . "
-					AND ca.id_author= " . $GLOBALS['author_session']['id_author'] . ")
-					AND ca.id_case = c.id_case";
+		// Left join is used to fallback on 'public' values if the user is not
+		// assigned to the case.
+		$q = "SELECT ca.*, c.status, c.public, c.pub_write
+				FROM lcm_case as c
+				LEFT JOIN lcm_case_author as ca 
+					ON (ca.id_case = c.id_case 
+						AND id_author = " . $GLOBALS['author_session']['id_author'] . ")
+				WHERE c.id_case = " . intval($case);
 
 		$result = lcm_query($q);
 
@@ -57,10 +62,10 @@ function allowed($case, $access) {
 			for($i = 0; $i < strlen($access); $i++) {
 				switch ($access{$i}) {
 					case "r":
-						$allow &= ($row['ac_read']);
+						$allow &= ($row['ac_read'] || ($row['ac_read'] != '0' && $row['public']));
 						break;
 					case "w":
-						$allow &= ($row['ac_write'] && $open);
+						$allow &= (($row['ac_write'] || ($row['ac_write'] != '0' && $row['pub_write'])) && $open);
 						break;
 					case "e":
 						$allow &= ($row['ac_edit'] && $open);
