ba4645ba15bc3e9bde0f3f931a889c258537fd30
antzi <antzi>
2005-04-12 20:52:24 +0000

    Fixed default settings for public access of new cases, set from site policy (boolean -> yes/no string) Added disabling of public access settings in the form, when user has no 'admin' access control to the case.

--- edit_case.php
+++ edit_case.php
@@ -18,7 +18,7 @@
 	with this program; if not, write to the Free Software Foundation, Inc.,
 	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
 
-	$Id: edit_case.php,v 1.73 2005/04/12 20:33:12 antzi Exp $
+	$Id: edit_case.php,v 1.74 2005/04/12 20:52:24 antzi Exp $
 */
 
 include('inc/inc.php');
@@ -77,8 +77,8 @@ if (empty($_SESSION['errors'])) {
 
 	} else {
 		// Set default values for the new case
-		$_SESSION['case_data']['public'] = read_meta('case_default_read');
-		$_SESSION['case_data']['pub_write'] = read_meta('case_default_write');
+		$_SESSION['case_data']['public'] = (int) (read_meta('case_default_read') == 'yes');
+		$_SESSION['case_data']['pub_write'] = (int) (read_meta('case_default_write') == 'yes');
 		$_SESSION['case_data']['status'] = 'draft';
 
 		$_SESSION['case_data']['admin'] = true;
@@ -258,6 +258,7 @@ if ($_SESSION['case_data']['id_case']) {
 
 	// Public access rights
 	if ( $_SESSION['case_data']['admin'] || (read_meta('case_read_always') != 'yes') || (read_meta('case_write_always') != 'yes') ) {
+		$dis = ( allowed($case,'a') ? '' : ' disabled="disabled"');
 		echo '<tr><td colspan="2">' . _T('case_input_collaboration') .  ' <br />
 				<ul>';
 
@@ -268,7 +269,7 @@ if ($_SESSION['case_data']['id_case']) {
 			if ($_SESSION['case_data']['public'])
 				echo ' checked="checked"';
 
-			echo " />";
+			echo "$dis />";
 			echo '<label for="case_public_read">' . _T('case_input_collaboration_read') . "</label></li>\n";
 		}
 
@@ -279,7 +280,7 @@ if ($_SESSION['case_data']['id_case']) {
 			if ($_SESSION['case_data']['pub_write'])
 				echo ' checked="checked"';
 
-			echo " />";
+			echo "$dis />";
 			echo '<label for="case_public_write">' . _T('case_input_collaboration_write') . "</label></li>\n";
 		}
 
