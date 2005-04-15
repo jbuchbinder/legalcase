c9767662c0231472c2f44211161c06cc55b717cd
mlutfy <mlutfy>
2005-04-15 09:19:58 +0000

    added get_fu_description()

--- inc/inc_filters.php
+++ inc/inc_filters.php
@@ -18,7 +18,7 @@
 	with this program; if not, write to the Free Software Foundation, Inc.,
 	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
 
-	$Id: inc_filters.php,v 1.64 2005/04/15 06:34:35 mlutfy Exp $
+	$Id: inc_filters.php,v 1.65 2005/04/15 09:19:58 mlutfy Exp $
 */
 
 // Execute this file only once
@@ -256,6 +256,32 @@ function get_person_initials($item, $with_abbver = true) {
 		$ret = '<abbrev title="' . get_person_name($item) . '">' . $ret . '</abbrev>';
 
 	return $ret;
+}
+
+function get_fu_description($item) {
+	if (! is_array($item)) {
+		lcm_debug("get_fu_description: parameter is not an array.");
+		return '';
+	}
+
+	$short_description = '';
+
+	if ($item['type'] == 'assignment' && is_numeric($item['description'])) {
+		$res1 = lcm_query("SELECT * FROM lcm_author WHERE id_author = " . $item['description']);
+		$author1 = lcm_fetch_array($res1);
+		$short_description = _T('case_info_author_assigned', array('name' => get_person_name($author1)));
+	} else{
+		if ($item['description']) {
+			if (strlen(lcm_utf8_decode($item['description'])) < $title_length) 
+				$short_description = $item['description'];
+			else
+				$short_description = substr($item['description'], 0, $title_length) . '...';
+		} else {
+			$short_description = _T('fu_info_emptydesc');
+		}
+	}
+
+	return $short_description;
 }
 
 // Dirty hack: utf8_decode is mainly used for strlen(),
