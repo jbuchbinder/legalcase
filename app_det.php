966191dd97aea5e1a9c0bf7eaad1655c06f572d0
mlutfy <mlutfy>
2005-06-01 12:19:38 +0000

    another AC fix with 'create a new f-u for this app'

--- app_det.php
+++ app_det.php
@@ -18,7 +18,7 @@
 	with this program; if not, write to the Free Software Foundation, Inc.,
 	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
 
-	$Id: app_det.php,v 1.19 2005/06/01 12:18:18 mlutfy Exp $
+	$Id: app_det.php,v 1.20 2005/06/01 12:19:38 mlutfy Exp $
 */
 
 include('inc/inc.php');
@@ -166,9 +166,11 @@ echo '<p class="normal_text">' . "\n";
 				$short_description = substr($fu['description'],0,$title_length) . '...';
 			echo '<br />Resulting followup:' . ' <a href="fu_det.php?followup=' . $fu['id_followup'] . '">' . $short_description; // TRAD
 		} else {
-			// Show create followup from appointment
-			echo '<br /><a href="edit_fu.php?case=' . $row['id_case'] . '&amp;app=' . $row['id_app']
-				. '" class="create_new_lnk">Create new followup from this appointment';	// TRAD
+			if ($ac['w']) {
+				// Show create followup from appointment
+				echo '<br /><a href="edit_fu.php?case=' . $row['id_case'] . '&amp;app=' . $row['id_app']
+					. '" class="create_new_lnk">Create new followup from this appointment';	// TRAD
+			}
 		}
 
 		echo "</a><br />\n";
