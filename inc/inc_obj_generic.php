c1541de6baeb0af8b3a2fe4ce8ba056a5984ca12
mlutfy <mlutfy>
2006-03-02 20:48:40 +0000

    initial commit

---
+++ inc/inc_obj_generic.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+	This file is part of the Legal Case Management System (LCM).
+	(C) 2004-2005 Free Software Foundation, Inc.
+
+	This program is free software; you can redistribute it and/or modify it
+	under the terms of the GNU General Public License as published by the 
+	Free Software Foundation; either version 2 of the License, or (at your 
+	option) any later version.
+
+	This program is distributed in the hope that it will be useful, but 
+	WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+	or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+	for more details.
+
+	You should have received a copy of the GNU General Public License along 
+	with this program; if not, write to the Free Software Foundation, Inc.,
+	59 Temple Place, Suite 330, Boston, MA  02111-1307, USA
+
+	$Id: inc_obj_generic.php,v 1.1 2006/03/02 20:48:40 mlutfy Exp $
+*/
+
+// Execute this file only once
+if (defined('_INC_OBJ_GENERIC')) return;
+define('_INC_OBJ_GENERIC', '1');
+
+class LcmObject {
+	var $data; 
+
+	function LcmObject() {
+		$this->data = array();
+	}
+
+	function getDataInt($field, $default = 0) {
+		if (isset($this->data[$field]))
+			return $this->data[$field];
+
+		if (is_string($default) && $default == '__ASSERT__')
+			lcm_panic("Value does not exist.");
+
+		return $default;
+	}
+
+	function getDataString($field, $default = '') {
+		if (isset($this->data[$field]))
+			return $this->data[$field];
+
+		if ($default == '__ASSERT__')
+			lcm_panic("Value does not exist.");
+
+		return $default;
+	}
+}
+
+?>
